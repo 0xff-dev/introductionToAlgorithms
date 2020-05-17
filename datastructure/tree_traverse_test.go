3d41b692cc170a5230ba9faec3ad108ddb59297b
shuang zhang <stevenshuang521@gmail.com>
2020-05-17 17:53:06 +0800

    other tree algorithm test

--- datastructure/tree_traverse_test.go
+++ datastructure/tree_traverse_test.go
@@ -7,10 +7,10 @@ import (
 
 func TestTreeNodeTraverse(t *testing.T) {
 	tree := &treeNode{
-		Data:  10,
-		Left:  &treeNode{
-			Data:  8,
-			Left:  nil,
+		Data: 10,
+		Left: &treeNode{
+			Data: 8,
+			Left: nil,
 			Right: &treeNode{
 				Data:  6,
 				Left:  nil,
@@ -18,8 +18,8 @@ func TestTreeNodeTraverse(t *testing.T) {
 			},
 		},
 		Right: &treeNode{
-			Data:  12,
-			Left:  &treeNode{
+			Data: 12,
+			Left: &treeNode{
 				Data:  5,
 				Left:  nil,
 				Right: nil,
@@ -45,4 +45,68 @@ func TestTreeNodeTraverse(t *testing.T) {
 	tree.PostOrder()
 	fmt.Println("post order ------")
 	tree.PostOrder1()
+
+	fmt.Println("floor traverse ----")
+	tree.Floor()
+}
+
+func TestFindPostOrder(t *testing.T) {
+	pre, in := []int{1, 2, 4, 7, 3, 5, 8, 9, 6}, []int{4, 7, 2, 1, 8, 5, 9, 3, 6}
+	FindPostOrder(pre, in)
+}
+
+func TestTreeNode_Predecessor(t *testing.T) {
+	tree := &treeNode{
+		Data: 12,
+		Left: &treeNode{
+			Data: 10,
+			Left: &treeNode{
+				Data: 8,
+				Left: nil,
+				Right: &treeNode{
+					Data:  9,
+					Left:  nil,
+					Right: nil,
+				},
+			},
+			Right: &treeNode{
+				Data:  11,
+				Left:  nil,
+				Right: nil,
+			},
+		},
+		Right: &treeNode{
+			Data: 15,
+			Left: &treeNode{
+				Data:  14,
+				Left:  nil,
+				Right: nil,
+			},
+			Right: &treeNode{
+				Data:  16,
+				Left:  nil,
+				Right: nil,
+			},
+		},
+	}
+	fmt.Println("14 没有左子树切是父节点的左子树 aim is : 12")
+	node := tree.Predecessor(14)
+	fmt.Println(node.Data)
+	fmt.Println("10 有自己的左子树: 9")
+	node = tree.Predecessor(10)
+	fmt.Println(node.Data)
+	fmt.Println("11 没有左子树，是父节点的右子树 : 10")
+	node = tree.Predecessor(11)
+	fmt.Println(node.Data)
+
+	// 后继
+	fmt.Println("11 没有右子树切是父节点的右子树 : 12")
+	node = tree.Successor(11)
+	fmt.Println(node.Data)
+	fmt.Println("8有自己右子树9")
+	node = tree.Successor(8)
+	fmt.Println(node.Data)
+	fmt.Println("14是父节点的左子树: 15")
+	node = tree.Successor(14)
+	fmt.Println(node.Data)
 }
