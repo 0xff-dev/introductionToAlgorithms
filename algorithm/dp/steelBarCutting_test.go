f0fb294906d7c1aae2c2fe74a9c7da4070abefc6
shuang zhang <stevenshuang521@gmail.com>
2020-05-25 23:07:15 +0800

    steel bar cutting test

---
+++ algorithm/dp/steelBarCutting_test.go
@@ -0,0 +1,12 @@
+package dp
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMaxPrice(t *testing.T) {
+	for index := 0; index <= 10; index ++ {
+		fmt.Println("res: ", index, " = ", MaxPrice(index))
+	}
+}
