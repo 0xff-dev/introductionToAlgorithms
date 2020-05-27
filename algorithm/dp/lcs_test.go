a7a45bf299aa6a44a106c339e9ca1c60265a003c
shuang zhang <stevenshuang521@gmail.com>
2020-05-27 23:15:14 +0800

    scroll array test

--- algorithm/dp/lcs_test.go
+++ algorithm/dp/lcs_test.go
@@ -8,6 +8,7 @@ import (
 func TestLCS(t *testing.T) {
 	s1, s2 := "BDCABA", "ABCBDAB"
 	fmt.Println(LCS(s2, s1))
+	fmt.Println("scroll array: ", ScrollArrayLCS(s2, s1))
 	s1, s2 = "", "aaaaaaaa"
 	fmt.Println(LCS(s2, s1))
 }
