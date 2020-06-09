687273f4fba6f6499802d6df066cb37028704cf1
shuang zhang <stevenshuang521@gmail.com>
2020-06-09 23:21:48 +0800

    delete node in rb tree test

--- datastructure/rb_tree_test.go
+++ datastructure/rb_tree_test.go
@@ -1,11 +1,24 @@
 package datastructure
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestNewRBTree(t *testing.T) {
 	tree := NewRBTree()
 	tree.Insert(10)
 	tree.Insert(12)
 	tree.Insert(11)
+	tree.Insert(15)
+	tree.Insert(2)
 	tree.Root.Floor()
+
+	self := tree.Root.Search(11)
+	if self == nil {
+		fmt.Println("11 not found")
+	} else {
+		tree.Delete(self)
+		tree.Root.Floor()
+	}
 }
