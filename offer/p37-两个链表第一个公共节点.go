a7a7e7d423e89ae4a8e588939c01babe087f1a6b
shuang zhang <stevenshuang521@gmail.com>
2020-08-15 14:49:33 +0800

    两个链表的公共节点

---
+++ offer/p37-两个链表第一个公共节点.go
@@ -0,0 +1,35 @@
+package offer
+
+func getListLength(list *ListNode) int {
+	walker := list
+	length := 0
+	for ; walker != nil; walker = walker.Next{
+		length++
+	}
+	return length
+}
+
+
+func CommonNode(list1, list2 *ListNode) *ListNode {
+	if list1 == nil || list2 == nil {
+		return nil
+	}
+	l1, l2 := getListLength(list1), getListLength(list2)
+	walker1, walker2 := list1, list2
+	if l1 > l2 {
+		for step := l1-l2; step > 0; step-- {
+			walker1 = walker1.Next
+		}
+	} else {
+		for step := l2-l1; step > 0; step-- {
+			walker2 = walker2.Next
+		}
+	}
+	for walker1 != nil && walker2 != nil && walker1 != walker2 {
+		walker1, walker2 = walker1.Next, walker2.Next
+	}
+	if walker1 == nil || walker2 == nil {
+		return nil
+	}
+	return walker2
+}
---
+++ offer/p37_test.go
@@ -0,0 +1,30 @@
+package offer
+
+import "testing"
+
+func TestCommonNode(t *testing.T) {
+	commList := &ListNode{
+		Val:  3,
+		Next: &ListNode{
+			Val:  4,
+			Next: nil,
+		},
+	}
+	list1 := &ListNode{
+		Val:  8,
+		Next: &ListNode{
+			Val:  10,
+			Next: commList,
+		},
+	}
+	commonNode := CommonNode(list1, commList)
+	if commonNode == nil || commonNode.Val != 3 {
+		t.Fatalf("not common node")
+	}
+	t.Logf("successfully found common node: %d", commonNode.Val)
+	commonNode = CommonNode(commonNode, commonNode)
+	if commonNode == nil || commonNode.Val != 3 {
+		t.Fatalf("expect commond node val: %d", 3)
+	}
+	t.Logf("successfully found common node: %d", commonNode.Val)
+}
