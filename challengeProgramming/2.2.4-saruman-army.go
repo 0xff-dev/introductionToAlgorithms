fffda4cc70398b50da7a710191270fdbfb7ef3ee
shuang zhang <stevenshuang521@gmail.com>
2020-09-20 23:00:46 +0800

    优先队列，贪心

--- challengeProgramming/2.2.4-saruman-army.go
+++ challengeProgramming/2.2.4-saruman-army.go
@@ -34,3 +34,36 @@ func MarkNode(nodes []int, r int) {
 	}
 	fmt.Println("mark nodes: ", markNodes)
 }
+
+/*
+	将一个模板切割成N块，每次切割的长度需要花销长度的能量，例如长度为21的要切割为5， 8， 8，切割为13和8的时候花销为21
+	13切割为5，8的花销为13，所以总的花销为34
+
+	反向思维，去最小的合成最终的长度。可以利用优先队列完成，小根堆
+*/
+
+func TotalCount(boards []int) int {
+	length := len(boards)
+	result := 0
+	for length > 1 {
+		l, r := 0, 1
+		if boards[l] > boards[r] {
+			l, r = r, l
+		}
+		for index := 2; index < length; index++ {
+			if boards[l] > boards[index] {
+				r, l = l, index
+			} else if boards[r] > boards[index] {
+				r = index
+			}
+		}
+		tmp := boards[l] + boards[r]
+		result += tmp
+		if l == length-1 {
+			l, r = r, l
+		}
+		boards[l], boards[r] = tmp, boards[length-1]
+		length--
+	}
+	return result
+}
--- challengeProgramming/2.2.4_test.go
+++ challengeProgramming/2.2.4_test.go
@@ -9,3 +9,18 @@ func TestMarkNode(t *testing.T) {
 	MarkNode(input, 3)
 	MarkNode(input, 10)
 }
+
+func TestTotalCount(t *testing.T) {
+	input := []int{8, 5, 8}
+	if res := TotalCount(input); res != 34 {
+		t.Fatalf("expect 34 get %d", res)
+	}
+	input = []int{}
+	if res := TotalCount(input); res != 0 {
+		t.Fatalf("expect 0 get %d", res)
+	}
+	input = []int{1}
+	if res := TotalCount(input); res != 0 {
+		t.Fatalf("expect 0 get %d", res)
+	}
+}
