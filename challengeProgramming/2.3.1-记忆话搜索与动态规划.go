1d5d90d5574fe308cdc01907c286f96447887e2a
shuang zhang <stevenshuang521@gmail.com>
2020-09-27 01:29:46 +0800

    最长公共子序列

--- challengeProgramming/2.3.1-记忆话搜索与动态规划.go
+++ challengeProgramming/2.3.1-记忆话搜索与动态规划.go
@@ -1,5 +1,7 @@
 package challengeProgramming
 
+import "fmt"
+
 /*
 01背包问题
 */
@@ -17,4 +19,52 @@ func Backpack(weights, values []int, capacity int) int {
 		}
 	}
 	return dp[capacity]
+}
+
+/*
+最长公共子序列问题
+*/
+func LongestCommonSubsequence(s1, s2 string) int {
+	lenS1, lenS2 := len(s1), len(s2)
+	if lenS1 == 0 || lenS2 == 0 {
+		return 0
+	}
+	dp := make([][]int, lenS2+1)
+	for i := 0; i < lenS2+1; i++ {
+		dp[i] = make([]int, lenS1+1)
+	}
+	for col := 1; col < lenS1+1; col++ {
+		for row := 1; row < lenS2+1; row++ {
+			if s1[col-1] == s2[row-1] {
+				dp[row][col] = dp[row-1][col-1] + 1
+			} else {
+				dp[row][col] = max(dp[row-1][col], dp[row][col-1])
+			}
+		}
+	}
+	findPath(dp, s2, s1, lenS2, lenS1, "")
+	return dp[lenS2][lenS1]
+}
+
+func findPath(dp [][]int, s1, s2 string, x, y int, ans string) {
+	if x == 0 || y == 0 {
+		fmt.Println("ans: ", ans)
+		return
+	}
+	cx, cy := s1[x-1], s2[y-1]
+	if cx == cy {
+		findPath(dp, s1, s2, x-1, y-1, string(cx)+ans)
+		return
+	}
+	if dp[x][y] == dp[x-1][y] {
+		findPath(dp, s1, s2, x-1, y, ans)
+	}
+	if dp[x][y] == dp[x][y-1] {
+		findPath(dp, s1, s2, x, y-1, ans)
+	}
+}
+
+// 明天实现
+func LCSbyScrollingArray(s1, s2 string) string {
+	return ""
 }
--- challengeProgramming/2.3.1_test.go
+++ challengeProgramming/2.3.1_test.go
@@ -8,3 +8,23 @@ func TestBackpack(t *testing.T) {
 		t.Fatalf("expect 7 get %d", res)
 	}
 }
+
+type subsequence struct {
+	S1, S2 string
+	Ans    int
+}
+
+func TestLongestCommonSubsequence(t *testing.T) {
+	for _, ts := range []subsequence{
+		{"abcd", "becd", 3},
+		{"aaaaa", "aaaa", 4},
+		{"abcdef", "a", 1},
+		{"aaaa", "", 0},
+		{"abcd", "tyui", 0},
+		{"BDCABA", "ABCBDAB", 4},
+	} {
+		if res := LongestCommonSubsequence(ts.S1, ts.S2); res != ts.Ans {
+			t.Fatalf("s1: %s, s2: %s =====> expcet %d get %d", ts.S1, ts.S2, ts.Ans, res)
+		}
+	}
+}
