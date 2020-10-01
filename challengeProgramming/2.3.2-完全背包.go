4ad3e7bdaa9cdcde24f2b7083f336b264310d644
shuang zhang <stevenshuang521@gmail.com>
2020-10-01 22:00:19 +0800

    母函数

--- challengeProgramming/2.3.2-完全背包.go
+++ challengeProgramming/2.3.2-完全背包.go
@@ -21,3 +21,40 @@ func CompleteBackpack(weights, values []int, capacity int) int {
 	}
 	return dp[capacity]
 }
+
+/*
+   有n个不同大小的数字，每个有m个，求选择他们中的若干是否可以是他们的和为K
+   这道题可以考虑dp， 母函数
+*/
+
+// 母函数思想，可以这么理解，选择一个的时候可以得到那些值，在选择第二个，可以得到另一波的值。这样
+// 根据系数即可得到目标的有多少中组合
+func GeneratingFunction(values, nums []int, k int) int {
+	// 一个基本的生成式
+	if len(values) == 0 {
+		return 0
+	}
+	// coefficient就是系数表达式, tmp 辅助计算
+	// 0+0*x^1+0*x^2+......
+	coefficient, tmp := make([]int, k+1), make([]int, k+1)
+	for c, v := 0, 0; c <= nums[0] && v <= k; c, v = c+1, v+values[0] {
+		coefficient[v] = 1
+	}
+
+	for index := 1; index < len(values); index++ {
+		for idx := 0; idx <= k; idx++ {
+			for c, v := 0, 0; c <= nums[index] && v+idx <= k; c, v = c+1, v+values[index] {
+				tmp[v+idx] += coefficient[idx]
+			}
+		}
+		for i := 0; i <= k; i++ {
+			coefficient[i], tmp[i] = tmp[i], 0
+		}
+	}
+
+	for index := 0; index < k+1; index++ {
+		fmt.Printf("%dx^%d+ ", coefficient[index], index)
+	}
+	fmt.Println()
+	return coefficient[k]
+}
--- challengeProgramming/2.3.2-完全背包_test.go
+++ challengeProgramming/2.3.2-完全背包_test.go
@@ -1,10 +1,21 @@
 package challengeProgramming
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestCompleteBakcpack(t *testing.T) {
 	weights, values := []int{3, 4, 2}, []int{4, 5, 3}
 	if res := CompleteBackpack(weights, values, 7); res != 10 {
 		t.Fatalf("expect 10 get %d ", res)
+	}
+}
+
+func TestGeneratingFunction(t *testing.T) {
+	values, nums := []int{3, 5, 8}, []int{3, 2, 2}
+	for _, aim := range []int{3, 6, 9, 5, 8, 12, 17, 19} {
+		fmt.Println("aim: ", aim)
+		GeneratingFunction(values, nums, aim)
 	}
 }
