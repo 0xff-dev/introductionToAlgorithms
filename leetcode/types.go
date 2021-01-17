9120710feac418017bf906da56c10e3d61bbf302
stevenshuang <stevenshuang521@gmail.com>
2021-01-17 16:03:25 +0800

    add Dis function for structure listNode

--- leetcode/types.go
+++ leetcode/types.go
@@ -1,6 +1,14 @@
 package leetcode
 
+import "log"
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
+}
+
+func (l *ListNode) Dis() {
+	for walker := l; walker != nil; walker = walker.Next {
+		log.Printf("val is [%d]", walker.Val)
+	}
 }
