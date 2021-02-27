8962dc981772128bafc3310325c6d826f2c0888c
stevenshuang <stevenshuang521@gmail.com>
2021-02-27 10:12:29 +0800

    add unique algorithm

--- leetcode/46Permutations.go
+++ leetcode/46Permutations.go
@@ -1,21 +1,37 @@
 package leetcode
 
+import (
+	"fmt"
+	"strings"
+)
+
 func permute(nums []int) [][]int {
 	r := make([][]int, 0)
-	permuteAux(nums, 0, &r)
+	check := make(map[string]struct{})
+	template := strings.Repeat("%d", len(nums))
+	permuteAux(nums, 0, &r, check, template)
 	return r
 }
 
-func permuteAux(nums []int, index int, r *[][]int) {
+func permuteAux(nums []int, index int, r *[][]int, exist map[string]struct{}, template string) {
 	if index == len(nums) {
-		dest := make([]int, len(nums))
-		copy(dest, nums)
+		dest := make([]int, 0)
+		interfaceDest := make([]interface{}, 0)
+		for _, n := range nums {
+			dest = append(dest, n)
+			interfaceDest = append(interfaceDest, n)
+		}
+		key := fmt.Sprintf(template, interfaceDest...)
+		if _, ok := exist[key]; ok {
+			return
+		}
+		exist[key] = struct{}{}
 		*r = append(*r, dest)
 		return
 	}
 	for idx := index; idx < len(nums); idx++ {
 		nums[idx], nums[index] = nums[index], nums[idx]
-		permuteAux(nums, index+1, r)
+		permuteAux(nums, index+1, r, exist, template)
 		nums[idx], nums[index] = nums[index], nums[idx]
 	}
 }
--- leetcode/46Permutations_test.go
+++ leetcode/46Permutations_test.go
@@ -14,4 +14,8 @@ func TestPermute(t *testing.T) {
 	input = []int{1, 0}
 	r = permute(input)
 	t.Log(r)
+
+	input = []int{1, 1, 2}
+	r = permute(input)
+	t.Log(r)
 }
