f6c9e9fb7e4d43f7ed173825d527fbe9340759b4
stevenshuang <stevenshuang521@gmail.com>
2021-04-03 16:45:05 +0800

    update

--- leetcode/746MinCostClimbingStairs.go
+++ leetcode/746MinCostClimbingStairs.go
@@ -3,12 +3,12 @@ package leetcode
 func minCostClimbingStairs(cost []int) int {
 	oneStep, twoStep := cost[0], cost[1]
 	for idx := 2; idx < len(cost); idx++ {
-		_min := oneStep + cost[idx]
-		if _min > twoStep+cost[idx] {
-			_min = twoStep + cost[idx]
+		_min := oneStep
+		if _min > twoStep {
+			_min = twoStep
 		}
 		oneStep = twoStep
-		twoStep = _min
+		twoStep = _min + cost[idx]
 	}
 	if oneStep > twoStep {
 		return twoStep
