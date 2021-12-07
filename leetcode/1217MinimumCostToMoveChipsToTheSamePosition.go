55930bccffe0da2959bf4bddb53563e1bb0a1858
0xff-dev <stevenshuang521@gmail.com>
2021-12-07 09:04:19 +0800

    minimum cost to move chips to the same position2

--- leetcode/1217MinimumCostToMoveChipsToTheSamePosition.go
+++ leetcode/1217MinimumCostToMoveChipsToTheSamePosition.go
@@ -19,3 +19,18 @@ func minCostToMoveChips(position []int) int {
 	}
 	return minCost
 }
+
+func minCostToMoveChips2(position []int) int {
+	odd, even := 0, 0
+	for _, pos := range position {
+		if pos&1 == 0 {
+			even++
+			continue
+		}
+		odd++
+	}
+	if even > odd {
+		return odd
+	}
+	return even
+}
--- leetcode/1217MinimumCostToMoveChipsToTheSamePosition_test.go
+++ leetcode/1217MinimumCostToMoveChipsToTheSamePosition_test.go
@@ -7,14 +7,23 @@ func TestMinCostToMoveChips(t *testing.T) {
 	if minCost := minCostToMoveChips(position); minCost != 1 {
 		t.Fatalf("expect 1 get %d", minCost)
 	}
+	if minCost := minCostToMoveChips2(position); minCost != 1 {
+		t.Fatalf("expect 1 get %d", minCost)
+	}
 
 	position = []int{2, 2, 2, 3, 3}
 	if minCost := minCostToMoveChips(position); minCost != 2 {
 		t.Fatalf("expect 2 get %d", minCost)
 	}
+	if minCost := minCostToMoveChips2(position); minCost != 2 {
+		t.Fatalf("expect 2 get %d", minCost)
+	}
 
 	position = []int{1, 1000000000}
 	if minCost := minCostToMoveChips(position); minCost != 1 {
+		t.Fatalf("expect 1 get %d", minCost)
+	}
+	if minCost := minCostToMoveChips2(position); minCost != 1 {
 		t.Fatalf("expect 1 get %d", minCost)
 	}
 }
