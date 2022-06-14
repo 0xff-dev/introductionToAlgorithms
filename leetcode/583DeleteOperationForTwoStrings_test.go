e369cd561a75fd4e2160ac69c220e059572c9257
0xff-dev <stevenshuang521@gmail.com>
2022-06-14 22:38:44 +0800

    add solution for 'delete operation for two strings'

--- leetcode/583DeleteOperationForTwoStrings_test.go
+++ leetcode/583DeleteOperationForTwoStrings_test.go
@@ -7,14 +7,23 @@ func TestMinDistance1(t *testing.T) {
 	if r := minDistance1(w1, w2); r != 2 {
 		t.Fatalf("expect 2 get %d", r)
 	}
+	if r := Solution(w1, w2); r != 2 {
+		t.Fatalf("expect 2 get %d", r)
+	}
 
 	w1, w2 = "aa", "aa"
 	if r := minDistance1(w1, w2); r != 0 {
 		t.Fatalf("expect 0 get %d", r)
 	}
+	if r := Solution(w1, w2); r != 0 {
+		t.Fatalf("expect 0 get %d", r)
+	}
 
 	w1, w2 = "leetcode", "etco"
 	if r := minDistance1(w1, w2); r != 4 {
+		t.Fatalf("expect 4 get %d", r)
+	}
+	if r := Solution(w1, w2); r != 4 {
 		t.Fatalf("expect 4 get %d", r)
 	}
 }
