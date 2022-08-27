e069bf65b295445fbfa0715b98f222190b8f8a1b
0xff-dev <stevenshuang521@gmail.com>
2022-08-27 16:12:31 +0800

    add solution and test cases

--- leetcode/1758MinimumChangesToMakeAlternatingBinaryString.go
+++ leetcode/1758MinimumChangesToMakeAlternatingBinaryString.go
@@ -23,3 +23,25 @@ func minOperation1758(s string) int {
 	}
 	return pz
 }
+
+func minOperations1758_2(s string) int {
+	case1, b1 := 0, uint8(0) // 0101.....
+	case2, b2 := 0, uint8(1) // 1010
+	for _, is := range s {
+		if uint8(is-'0') != b1 {
+			case1++
+		}
+		b1 = 1 - b1
+	}
+	for _, is := range s {
+		if uint8(is-'0') != b2 {
+			case2++
+		}
+		b2 = 1 - b2
+	}
+	if case1 > case2 {
+		return case2
+	}
+	return case1
+
+}
--- leetcode/1758MinimumChangesToMakeAlternatingBinaryString_test.go
+++ leetcode/1758MinimumChangesToMakeAlternatingBinaryString_test.go
@@ -3,23 +3,35 @@ package leetcode
 import "testing"
 
 func TestMinOperations1758(t *testing.T) {
-	//a := "0100"
-	//if r := minOperation1758(a); r != 1 {
-	//	t.Fatalf("expect 1 get %d", 1)
-	//}
-	//
-	//a = "0"
-	//if r := minOperation1758(a); r != 0 {
-	//	t.Fatalf("expect 0 get %d", r)
-	//}
-	//
-	//a = "10"
-	//if r := minOperation1758(a); r != 0 {
-	//	t.Fatalf("expect 0 get %d", r)
-	//}
+	a := "0100"
+	if r := minOperation1758(a); r != 1 {
+		t.Fatalf("expect 1 get %d", 1)
+	}
+	if r := minOperations1758_2(a); r != 1 {
+		t.Fatalf("expect 1 get %d", r)
+	}
 
-	a := "1111"
+	a = "0"
+	if r := minOperation1758(a); r != 0 {
+		t.Fatalf("expect 0 get %d", r)
+	}
+	if r := minOperations1758_2(a); r != 0 {
+		t.Fatalf("expect 0 get %d", r)
+	}
+
+	a = "10"
+	if r := minOperation1758(a); r != 0 {
+		t.Fatalf("expect 0 get %d", r)
+	}
+	if r := minOperations1758_2(a); r != 0 {
+		t.Fatalf("expect 0 get %d", r)
+	}
+
+	a = "1111"
 	if r := minOperation1758(a); r != 2 {
 		t.Fatalf("expect 2 get %d", r)
+	}
+	if r := minOperations1758_2(a); r != 2 {
+		t.Fatalf("expect 0 get %d", r)
 	}
 }
