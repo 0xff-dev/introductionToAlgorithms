7bb16d802fef1b9750beff4a1af19b3f187343e2
0xff-dev <stevenshuang521@gmail.com>
2022-12-13 00:00:46 +0800

    update logic

--- leetcode/2148CountElementsWithStrictlySmallerAndGreaterElements.go
+++ leetcode/2148CountElementsWithStrictlySmallerAndGreaterElements.go
@@ -1,39 +1,19 @@
 package leetcode
 
-import "sort"
-
 func countElements(nums []int) int {
-	ans := 0
-	sort.Ints(nums)
+	max, min := nums[0], nums[0]
 	check := make(map[int]int)
 	for _, n := range nums {
+		if n > max {
+			max = n
+		}
+		if n < min {
+			min = n
+		}
 		check[n]++
 	}
 	if len(check) > 2 {
-		return len(nums) - check[nums[0]] - check[nums[len(nums)-1]]
-	}
-	// 1, 2,2,2,,3,3,3,4
-	// 1,,1,1,1,1,1,
-	// 1,2,2,2,2
-	/// ,1,1,1,2
-	// 2,7,11,15
-
-	// -3,3,3,90
-	// -6 -6 -6 -5 5 5
-	diff := 1
-	for idx := 1; idx < len(nums); idx++ {
-		if nums[idx] != nums[idx-1] {
-			diff++
-		}
-		if diff > 1 && idx != len(nums)-1 {
-			ans++
-		}
-	}
-	if diff > 2 {
-		if nums[len(nums)-1] == nums[len(nums)-2] {
-			ans--
-		}
-		return ans
+		return len(nums)-check[max]-check[min]
 	}
 	return 0
 }
