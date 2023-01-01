06beefba3cd719c3a6f48121f2c6f1915275ec4c
0xff-dev <stevenshuang521@gmail.com>
2023-01-01 16:42:31 +0800

    word pattern, add solution

--- leetcode/290WordPattern.go
+++ leetcode/290WordPattern.go
@@ -32,3 +32,33 @@ func wordPattern(pattern string, s string) bool {
 	}
 	return true
 }
+
+func wordPattern290(pattern string, s string) bool {
+	words := strings.Split(s, " ")
+	if len(words) != len(pattern) {
+		return false
+	}
+	b2w := make(map[byte]string)
+	w2b := make(map[string]byte)
+	b, w := pattern[0], words[0]
+	b2w[b] = w
+	w2b[w] = b
+	// abba, dog cat cat dog
+	// abba dog, dog, dog, dog
+	for i := 1; i < len(pattern); i++ {
+		if pattern[i] == b {
+			if words[i] != w {
+				return false
+			}
+			continue
+		}
+		b, w = pattern[i], words[i]
+		v, ok := b2w[pattern[i]]
+		v1, ok1 := w2b[words[i]]
+		if (ok && words[i] != v) || (ok1 && pattern[i] != v1) {
+			return false
+		}
+		b2w[b] = w
+	}
+	return true
+}
--- leetcode/290WordPattern_test.go
+++ leetcode/290WordPattern_test.go
@@ -7,19 +7,31 @@ func TestWordPattern(t *testing.T) {
 	if !wordPattern(pattern, s) {
 		t.Fatalf("expect true get false")
 	}
+	if !wordPattern290(pattern, s) {
+		t.Fatalf("290 expect true get false")
+	}
 
 	pattern, s = "abba", "dog cat cat fish"
 	if wordPattern(pattern, s) {
 		t.Fatalf("expect false get true")
+	}
+	if wordPattern290(pattern, s) {
+		t.Fatalf("290 expect false get true")
 	}
 
 	pattern, s = "aaaa", "dog cat cat dog"
 	if wordPattern(pattern, s) {
 		t.Fatalf("expect false get true")
 	}
+	if wordPattern290(pattern, s) {
+		t.Fatalf("290 expect false get true")
+	}
 
 	pattern, s = "abba", "dog dog dog dog"
 	if wordPattern(pattern, s) {
 		t.Fatalf("expect flase get true")
+	}
+	if wordPattern290(pattern, s) {
+		t.Fatalf("290 expect false gettrue")
 	}
 }
