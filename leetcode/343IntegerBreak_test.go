6466e476a47491510c9fdb1c25764cb52a27b459
0xff-dev <stevenshuang521@gmail.com>
2023-02-14 22:04:40 +0800

    integer break

---
+++ leetcode/343IntegerBreak_test.go
@@ -0,0 +1,15 @@
+package leetcode
+
+import "testing"
+
+func TestIntegerBreak(t *testing.T) {
+	n := 2
+	if r := integerBreak(n); r != 1 {
+		t.Fatalf("expect 1 get %d", r)
+	}
+
+	n = 10
+	if r := integerBreak(n); r != 36 {
+		t.Fatalf("expect 36 get %d", r)
+	}
+}
