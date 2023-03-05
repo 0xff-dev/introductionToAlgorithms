47ab48f9199da9fbecefb74b99abcde4aadade01
0xff-dev <stevenshuang521@gmail.com>
2023-03-05 16:41:09 +0800

    Surrounded Regions

---
+++ leetcode/130SurrounededRegions.go
@@ -0,0 +1,37 @@
+package leetcode
+
+// 把边上的给填满了
+func solve(board [][]byte) {
+	rows, cols := len(board), len(board[0])
+	for row := 0; row < rows; row++ {
+		for col := 0; col < cols; col++ {
+			if row == 0 || row == rows-1 || col == 0 || col == cols-1 {
+				fillEdges(board, row, col)
+			}
+		}
+	}
+	for row := 0; row < rows; row++ {
+		for col := 0; col < cols; col++ {
+			if board[row][col] == '1' {
+				board[row][col] = 'O'
+				continue
+			}
+			if board[row][col] == 'O' {
+				board[row][col] = 'X'
+			}
+		}
+	}
+}
+
+// 边上填充‘1’
+func fillEdges(board [][]byte, x, y int) {
+	if x < 0 || x >= len(board) || y < 0 || y >= len(board[0]) || board[x][y] != 'O' {
+		return
+	}
+
+	board[x][y] = '1'
+	fillEdges(board, x+1, y)
+	fillEdges(board, x-1, y)
+	fillEdges(board, x, y-1)
+	fillEdges(board, x, y+1)
+}
---
+++ leetcode/130SurrounededRegions_test.go
@@ -0,0 +1,25 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSolove(t *testing.T) {
+	board := [][]byte{
+		{'X', 'X', 'X', 'X'},
+		{'X', 'O', 'O', 'X'},
+		{'X', 'X', 'O', 'X'},
+		{'X', 'O', 'X', 'X'},
+	}
+	exp := [][]byte{
+		{'X', 'X', 'X', 'X'},
+		{'X', 'X', 'X', 'X'},
+		{'X', 'X', 'X', 'X'},
+		{'X', 'O', 'X', 'X'},
+	}
+	solve(board)
+	if !reflect.DeepEqual(exp, board) {
+		t.Fatalf("expect %v get %v", exp, board)
+	}
+}
