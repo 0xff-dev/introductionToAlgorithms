8c4c160a0825259e4efa61d2a69a2ce59a1bb5ad
0xff-dev <stevenshuang521@gmail.com>
2023-06-14 22:14:14 +0800

    Minimum Aboluste Difference in BST

---
+++ leetcode/530MinimumAbsoluteDifferenceInBST.go
@@ -0,0 +1,27 @@
+package leetcode
+
+func getMinimumDifference(root *TreeNode) int {
+	target := -1
+	ans := -1
+	dfs530(root, &target, &ans)
+	return ans
+}
+
+func dfs530(root *TreeNode, target, ans *int) {
+	if root == nil {
+		return
+	}
+	if root.Left != nil {
+		dfs530(root.Left, target, ans)
+	}
+	if *target != -1 {
+		diff := root.Val - *target
+		if *ans == -1 || diff < *ans {
+			*ans = diff
+		}
+	}
+	*target = root.Val
+	if root.Right != nil {
+		dfs530(root.Right, target, ans)
+	}
+}
