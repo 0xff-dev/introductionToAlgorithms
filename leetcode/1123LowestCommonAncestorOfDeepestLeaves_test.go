5d39759e7b5b54a29c8aede1999cd7833d3d5f73
0xff-dev <stevenshuang521@gmail.com>
2023-08-12 22:36:06 +0800

    Lowest Common Ancestor Of Deepest Leaves

---
+++ leetcode/1123LowestCommonAncestorOfDeepestLeaves_test.go
@@ -0,0 +1,48 @@
+package leetcode
+
+import "testing"
+
+func TestLcaDeepestLeaves(t *testing.T) {
+	tree := &TreeNode{
+		Val: 3,
+		Left: &TreeNode{
+			Val: 5,
+			Left: &TreeNode{
+				Val: 6,
+			},
+			Right: &TreeNode{
+				Val:   2,
+				Left:  &TreeNode{Val: 7},
+				Right: &TreeNode{Val: 4},
+			},
+		},
+		Right: &TreeNode{
+			Val:   1,
+			Left:  &TreeNode{Val: 0},
+			Right: &TreeNode{Val: 8},
+		},
+	}
+	if r := lcaDeepestLeaves(tree); r.Val != 2 {
+		t.Fatalf("expect 2 get %d", r.Val)
+	}
+
+	tree = &TreeNode{
+		Val: 1,
+	}
+	if r := lcaDeepestLeaves(tree); r.Val != 1 {
+		t.Fatalf("expect 2 get %d", r.Val)
+	}
+
+	tree = &TreeNode{
+		Val: 0,
+		Left: &TreeNode{
+			Val:   1,
+			Right: &TreeNode{Val: 2},
+		},
+		Right: &TreeNode{Val: 3},
+	}
+
+	if r := lcaDeepestLeaves(tree); r.Val != 2 {
+		t.Fatalf("expect 2 get %d", r.Val)
+	}
+}
