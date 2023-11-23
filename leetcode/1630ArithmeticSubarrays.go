5e9db737f58237b7af7177e8685d46fdcc66b576
0xff-dev <stevenshuang521@gmail.com>
2023-11-23 23:24:48 +0800

    Arithmethic Subarrays

---
+++ leetcode/1630ArithmeticSubarrays.go
@@ -0,0 +1,24 @@
+package leetcode
+
+import "sort"
+
+func checkArithmeticSubarrays(nums []int, l []int, r []int) []bool {
+	ans := make([]bool, len(l))
+	for i := 0; i < len(l); i++ {
+		s, e := l[i], r[i]
+		c := make([]int, e-s+1)
+		copy(c, nums[s:e+1])
+		sort.Ints(c)
+		ok := true
+		diff := -1
+		for i := 1; i < len(c); i++ {
+			if diff != -1 && c[i]-c[i-1] != diff {
+				ok = false
+				break
+			}
+			diff = c[i] - c[i-1]
+		}
+		ans[i] = ok
+	}
+	return ans
+}
