094cfe7085c108f78c20d6027e1bbc3aa2cdcceb
0xff-dev <stevenshuang521@gmail.com>
2023-12-10 15:37:29 +0800

    Reach A Number

---
+++ leetcode/754ReachANumber.go
@@ -0,0 +1,16 @@
+package leetcode
+
+func reachNumber(target int) int {
+	if target < 0 {
+		target = -target
+	}
+	n := 0
+	for target > 0 {
+		n++
+		target -= n
+	}
+	if target&1 == 0 {
+		return n
+	}
+	return n + 1 + n&1
+}
