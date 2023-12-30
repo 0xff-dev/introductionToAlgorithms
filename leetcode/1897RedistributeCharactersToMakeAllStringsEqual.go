d4a574b6040dff4ca9cad72a3a7cdceae8e529f9
0xff-dev <stevenshuang521@gmail.com>
2023-12-30 12:30:47 +0800

    Redistribute Characters to Make all Strings Equal

---
+++ leetcode/1897RedistributeCharactersToMakeAllStringsEqual.go
@@ -0,0 +1,17 @@
+package leetcode
+
+func makeEqual(words []string) bool {
+	letters := [26]int{}
+	for _, word := range words {
+		for _, b := range word {
+			letters[b-'a']++
+		}
+	}
+	l := len(words)
+	for i := 0; i < 26; i++ {
+		if letters[i]%l != 0 {
+			return false
+		}
+	}
+	return true
+}
