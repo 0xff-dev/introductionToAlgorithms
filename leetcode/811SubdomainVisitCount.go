0d41f73e382e3c75354c68d6aebf348a8b5ec4bc
0xff-dev <stevenshuang521@gmail.com>
2024-01-09 22:29:42 +0800

    Subdomain Visit Count

---
+++ leetcode/811SubdomainVisitCount.go
@@ -0,0 +1,26 @@
+package leetcode
+
+import "fmt"
+
+func subdomainVisits(cpdomains []string) []string {
+	domains := make(map[string]int)
+	var (
+		rep    int
+		domain string
+	)
+	for _, cnt := range cpdomains {
+		fmt.Sscanf(cnt, "%d %s", &rep, &domain)
+		domains[domain] += rep
+		for idx := len(domain) - 1; idx >= 0; idx-- {
+			if domain[idx] == '.' {
+				x := domain[idx+1:]
+				domains[x] += rep
+			}
+		}
+	}
+	ans := make([]string, 0)
+	for domain, cnt := range domains {
+		ans = append(ans, fmt.Sprintf("%d %s", cnt, domain))
+	}
+	return ans
+}
