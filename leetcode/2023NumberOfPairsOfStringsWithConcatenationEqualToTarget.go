6caba5611143efe414ffaca6aa34da9c3499fa17
0xff-dev <stevenshuang521@gmail.com>
2024-01-26 22:09:13 +0800

    Number Of Pairs Of Strings With Concatenation Equal to Target

---
+++ leetcode/2023NumberOfPairsOfStringsWithConcatenationEqualToTarget.go
@@ -0,0 +1,18 @@
+package leetcode
+
+func numOfPairs(nums []string, target string) int {
+	count := make(map[string]int)
+	for _, s := range nums {
+		count[s]++
+	}
+	ans := 0
+	for i := 1; i < len(target); i++ {
+		a, b := target[:i], target[i:]
+		if a != b {
+			ans += count[a] * count[b]
+			continue
+		}
+		ans += count[a] * (count[a] - 1)
+	}
+	return ans
+}
