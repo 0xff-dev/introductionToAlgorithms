b3f3768b86197aba87f2d448b59db05892c890ac
0xff-dev <stevenshuang521@gmail.com>
2024-02-06 10:19:14 +0800

    optimized code

--- leetcode/1233RemoveSubFoldersFromTheFileSystem.go
+++ leetcode/1233RemoveSubFoldersFromTheFileSystem.go
@@ -1,9 +1,11 @@
 package leetcode
 
 import (
+	"sort"
 	"strings"
 )
 
+/*
 type filePathNode struct {
 	end  bool
 	data map[string]*filePathNode
@@ -20,6 +22,9 @@ func insertFolder(root *filePathNode, path string) {
 		}
 		if i == len(paths)-1 {
 			walker.data[p].end = true
+			continue
+		}
+		if walker.data[p].end {
 			continue
 		}
 		walker = walker.data[p]
@@ -42,4 +47,46 @@ func removeSubfolders(folder []string) []string {
 		insertFolder(root, str)
 	}
 	return searchFolder(root, "")
+}
+*/
+
+type filePathNode struct {
+	end  bool
+	data map[string]*filePathNode
+}
+
+func insertFolder(root *filePathNode, path string) bool {
+	walker := root
+	paths := strings.Split(path, "/")
+	for i := 1; i < len(paths); i++ {
+		p := paths[i]
+		_, ok := walker.data[p]
+		if !ok {
+			walker.data[p] = &filePathNode{end: false, data: make(map[string]*filePathNode)}
+		}
+		if i == len(paths)-1 {
+			walker.data[p].end = true
+			continue
+		}
+		if walker.data[p].end {
+			return false
+		}
+		walker = walker.data[p]
+	}
+	return true
+}
+
+func removeSubfolders(folder []string) []string {
+	sort.Slice(folder, func(i, j int) bool {
+		return len(folder[i]) < len(folder[j])
+	})
+
+	root := &filePathNode{end: false, data: make(map[string]*filePathNode)}
+	ans := make([]string, 0)
+	for _, str := range folder {
+		if insertFolder(root, str) {
+			ans = append(ans, str)
+		}
+	}
+	return ans
 }
