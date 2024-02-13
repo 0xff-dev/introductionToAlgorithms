8f5b20a5a33303d819807c864e1ac49b73891318
0xff-dev <stevenshuang521@gmail.com>
2024-02-13 15:23:00 +0800

    Find First Palindromic String in the Array

---
+++ leetcode/2108FindFirstPalindromicStringInTheArray.go
@@ -0,0 +1,13 @@
+package leetcode
+
+func firstPalindrome(words []string) string {
+	for _, str := range words {
+		s, e := 0, len(str)-1
+		for ; s < e && str[s] == str[e]; s, e = s+1, e-1 {
+		}
+		if s >= e {
+			return str
+		}
+	}
+	return ""
+}
