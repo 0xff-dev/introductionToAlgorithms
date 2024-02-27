a9b99deebc77efea1edceca17188a8e492334de3
0xff-dev <stevenshuang521@gmail.com>
2024-02-27 23:01:40 +0800

    Average Of Levels In Binary Tree

---
+++ leetcode/637AverageOfLevelsInBinaryTree.go
@@ -0,0 +1,22 @@
+package leetcode
+
+func averageOfLevels(root *TreeNode) []float64 {
+	queue := []*TreeNode{root}
+	ans := []float64{}
+	for len(queue) > 0 {
+		nq := []*TreeNode{}
+		sum := 0
+		for _, n := range queue {
+			sum += n.Val
+			if n.Left != nil {
+				nq = append(nq, n.Left)
+			}
+			if n.Right != nil {
+				nq = append(nq, n.Right)
+			}
+		}
+		ans = append(ans, float64(sum)/float64(len(queue)))
+		queue = nq
+	}
+	return ans
+}
