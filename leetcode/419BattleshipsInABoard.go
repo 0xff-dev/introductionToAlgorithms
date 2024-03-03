57f3775257f5810e9420e17870a87860947a3911
0xff-dev <stevenshuang521@gmail.com>
2024-03-03 14:49:49 +0800

    Battleships In a Board

---
+++ leetcode/419BattleshipsInABoard.go
@@ -0,0 +1,20 @@
+package leetcode
+
+func countBattleships(board [][]byte) int {
+	rows, cols := len(board), len(board[0])
+	ans := 0
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			if board[i][j] == 'X' {
+				if i > 0 && board[i-1][j] == 'X' {
+					continue
+				}
+				if j > 0 && board[i][j-1] == 'X' {
+					continue
+				}
+				ans++
+			}
+		}
+	}
+	return ans
+}
