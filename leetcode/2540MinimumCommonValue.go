b9c897785c75dbcd699ba6c779b596fb62e19dae
0xff-dev <stevenshuang521@gmail.com>
2024-03-09 10:02:14 +0800

    add antoher solution

--- leetcode/2540MinimumCommonValue.go
+++ leetcode/2540MinimumCommonValue.go
@@ -1,5 +1,7 @@
 package leetcode
 
+import "sort"
+
 func getCommon(nums1 []int, nums2 []int) int {
 	i, j := 0, 0
 	for i < len(nums1) && j < len(nums2) {
@@ -11,6 +13,18 @@ func getCommon(nums1 []int, nums2 []int) int {
 			continue
 		}
 		j++
+	}
+	return -1
+}
+
+func getCommon1(nums1 []int, nums2 []int) int {
+	l2 := len(nums2)
+	for _, n := range nums1 {
+		if idx := sort.Search(l2, func(i int) bool {
+			return nums2[i] >= n
+		}); idx != l2 && nums2[idx] == n {
+			return n
+		}
 	}
 	return -1
 }
