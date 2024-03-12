d8239e2d61b47411921265d426ecd6f5c55f12ee
0xff-dev <stevenshuang521@gmail.com>
2024-03-12 22:24:24 +0800

    Water And Jug Problem

---
+++ leetcode/365WaterAndJugProblem.go
@@ -0,0 +1,15 @@
+package leetcode
+
+func gcd365(x, y int) int {
+	for y != 0 {
+		x, y = y, x%y
+	}
+	return x
+}
+func canMeasureWater(x int, y int, target int) bool {
+	if x+y < target {
+		return false
+	}
+	g := gcd365(x, y)
+	return target%g == 0
+}
