35359d590952732e5f70b2a58eb60b489c003d4b
0xff-dev <stevenshuang521@gmail.com>
2024-04-01 20:53:35 +0800

    Corporate Flight Bookings

---
+++ leetcode/1109CorporateFlightBookings.go
@@ -0,0 +1,11 @@
+package leetcode
+
+func corpFlightBookings(bookings [][]int, n int) []int {
+	ans := make([]int, n)
+	for _, booking := range bookings {
+		for i := booking[0]; i <= booking[1]; i++ {
+			ans[i-1] += booking[2]
+		}
+	}
+	return ans
+}
