ff99d4a223dd297165e4ba12e45a44c6d08dea95
0xff-dev <stevenshuang521@gmail.com>
2024-04-15 20:58:10 +0800

    Minimum Number of Operations To Move All Balls To Each Box

---
+++ leetcode/1769MinimumNumberOfOperationsToMoveAllBallsToEachBox.go
@@ -0,0 +1,21 @@
+package leetcode
+
+func minOperations1769(boxes string) []int {
+	ans := make([]int, len(boxes))
+	pos := make([]int, 0)
+	for i := 0; i < len(boxes); i++ {
+		if boxes[i] == '1' {
+			pos = append(pos, i)
+		}
+	}
+	for i := 0; i < len(boxes); i++ {
+		for _, p := range pos {
+			diff := p - i
+			if diff < 0 {
+				diff = -diff
+			}
+			ans[i] += diff
+		}
+	}
+	return ans
+}
