fd1eaa3f3db84fd3bbed3c09aaca82b8e3bb6d5d
0xff-dev <stevenshuang521@gmail.com>
2024-04-16 22:11:38 +0800

    Longest harmonious Subsequence

---
+++ leetcode/594LongestHarmoniousSubsequence.go
@@ -0,0 +1,22 @@
+package leetcode
+
+import "sort"
+
+func findLHS(nums []int) int {
+	keys := make(map[int]int)
+	a := make([]int, 0)
+	for _, n := range nums {
+		if _, ok := keys[n]; !ok {
+			a = append(a, n)
+		}
+		keys[n]++
+	}
+	sort.Ints(a)
+	ans := 0
+	for i := 1; i < len(a); i++ {
+		if a[i]-a[i-1] == 1 && keys[a[i]]+keys[a[i-1]] > ans {
+			ans = keys[a[i]] + keys[a[i-1]]
+		}
+	}
+	return ans
+}
