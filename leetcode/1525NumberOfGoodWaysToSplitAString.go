35bfb2dd2d30311c06474f7a91bb228d89ea7e8a
0xff-dev <stevenshuang521@gmail.com>
2024-04-20 21:40:09 +0800

    Number Of Good Ways To Split a String

---
+++ leetcode/1525NumberOfGoodWaysToSplitAString.go
@@ -0,0 +1,26 @@
+package leetcode
+
+func numSplits(s string) int {
+	ans := 0
+	total := [26]int{}
+	for _, b := range s {
+		total[b-'a']++
+	}
+	left := [26]int{}
+	for end := 0; end < len(s)-1; end++ {
+		left[s[end]-'a']++
+		a, b := 0, 0
+		for i := 0; i < 26; i++ {
+			if left[i] > 0 {
+				a++
+			}
+			if total[i] > left[i] {
+				b++
+			}
+		}
+		if a == b {
+			ans++
+		}
+	}
+	return ans
+}
