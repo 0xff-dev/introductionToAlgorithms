c0cb521ef44f6a4c95f8152883ff96ec4a2ece8d
0xff-dev <stevenshuang521@gmail.com>
2024-05-02 11:59:43 +0800

    Largest Positive Integer That Exists With Its Negative

---
+++ leetcode/2441LargestPositiveIntegerThatExistsWithItsNetagive.go
@@ -0,0 +1,17 @@
+package leetcode
+
+func findMaxK(nums []int) int {
+	e := make(map[int]struct{})
+	ans := -1
+	for _, n := range nums {
+		if _, ok := e[-n]; ok {
+			if n < 0 {
+				n = -n
+			}
+			ans = max(ans, n)
+			continue
+		}
+		e[n] = struct{}{}
+	}
+	return ans
+}
