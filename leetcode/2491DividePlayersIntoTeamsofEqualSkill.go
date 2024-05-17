22ba9951a6cc660d247d125196f585819e271d22
0xff-dev <stevenshuang521@gmail.com>
2024-05-17 09:02:28 +0800

    divide Players Into Teams of Equal skill

---
+++ leetcode/2491DividePlayersIntoTeamsofEqualSkill.go
@@ -0,0 +1,21 @@
+package leetcode
+
+import "sort"
+
+func dividePlayers(skill []int) int64 {
+	sort.Ints(skill)
+	cur := -1
+	ans := int64(0)
+	for s, e := 0, len(skill)-1; s < e; s, e = s+1, e-1 {
+		if cur == -1 {
+			cur = skill[s] + skill[e]
+			ans += int64(skill[s]) * int64(skill[e])
+			continue
+		}
+		if skill[s]+skill[e] != cur {
+			return -1
+		}
+		ans += int64(skill[s]) * int64(skill[e])
+	}
+	return ans
+}
