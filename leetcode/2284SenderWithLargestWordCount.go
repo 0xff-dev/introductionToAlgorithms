9bc9145437e6c6250f01935296f4b16f64d5daaa
0xff-dev <stevenshuang521@gmail.com>
2024-05-30 08:49:41 +0800

    Sneder With Largest Word Count

---
+++ leetcode/2284SenderWithLargestWordCount.go
@@ -0,0 +1,20 @@
+package leetcode
+
+import "strings"
+
+func largestWordCount(messages []string, senders []string) string {
+	count := make(map[string]int)
+	mc, ma := 0, ""
+	for idx, msg := range messages {
+		count[senders[idx]] += len(strings.Split(msg, " "))
+		if count[senders[idx]] > mc {
+			mc = count[senders[idx]]
+			ma = senders[idx]
+			continue
+		}
+		if count[senders[idx]] == mc && senders[idx] > ma {
+			ma = senders[idx]
+		}
+	}
+	return ma
+}
