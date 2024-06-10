d7aabc93bd444d4bd0317682e517220ec86842b5
0xff-dev <stevenshuang521@gmail.com>
2024-06-10 09:31:24 +0800

    Height Checker

---
+++ leetcode/1051HeightChecker.go
@@ -0,0 +1,16 @@
+package leetcode
+
+import "sort"
+
+func heightChecker(heights []int) int {
+	dst := make([]int, len(heights))
+	copy(dst, heights)
+	sort.Ints(dst)
+	ans := 0
+	for i := 0; i < len(dst); i++ {
+		if dst[i] != heights[i] {
+			ans++
+		}
+	}
+	return ans
+}
