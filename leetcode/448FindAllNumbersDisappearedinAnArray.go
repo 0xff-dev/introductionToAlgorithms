d94aefac30c770a83a83e9aec14d67019448236c
0xff-dev <stevenshuang521@gmail.com>
2024-06-13 09:02:39 +0800

    Find All Number Disappeared in An Array

---
+++ leetcode/448FindAllNumbersDisappearedinAnArray.go
@@ -0,0 +1,20 @@
+package leetcode
+
+func findDisappearedNumbers(nums []int) []int {
+	for _, n := range nums {
+		cur := n
+		if cur < 0 {
+			cur = -cur
+		}
+		if nums[cur-1] > 0 {
+			nums[cur-1] = -nums[cur-1]
+		}
+	}
+	missing := make([]int, 0)
+	for i := 0; i < len(nums); i++ {
+		if nums[i] > 0 {
+			missing = append(missing, i+1)
+		}
+	}
+	return missing
+}
