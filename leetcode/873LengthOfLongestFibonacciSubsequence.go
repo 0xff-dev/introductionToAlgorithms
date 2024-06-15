589c80b887df3a24fcc742ebf8e086f61bcec8db
0xff-dev <stevenshuang521@gmail.com>
2024-06-15 15:57:08 +0800

    Length Of Longest Fibonacci Subsequence

---
+++ leetcode/873LengthOfLongestFibonacciSubsequence.go
@@ -0,0 +1,28 @@
+package leetcode
+
+func lenLongestFibSubseq(arr []int) int {
+	nums := make(map[int]struct{})
+	for _, n := range arr {
+		nums[n] = struct{}{}
+	}
+	var try func(int, int, *int)
+	try = func(a, b int, c *int) {
+		_, ok := nums[a+b]
+		if !ok {
+			return
+		}
+		*c++
+		try(b, a+b, c)
+	}
+	ans := 0
+	for i := 0; i < len(arr)-1; i++ {
+		for j := i + 1; j < len(arr); j++ {
+			c := 0
+			try(arr[i], arr[j], &c)
+			if c != 0 {
+				ans = max(ans, c+2)
+			}
+		}
+	}
+	return ans
+}
