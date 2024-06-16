79aa5d55ce69d8927be8dd18c1e91ce3dd2c0c0c
0xff-dev <stevenshuang521@gmail.com>
2024-06-16 09:48:11 +0800

    Pathing Array

---
+++ leetcode/330PatchingArray.go
@@ -0,0 +1,19 @@
+package leetcode
+
+func minPatches(nums []int, n int) int {
+	patches := 0
+	maxReach := 0
+	i := 0
+
+	for maxReach < n {
+		if i < len(nums) && nums[i] <= maxReach+1 {
+			maxReach += nums[i]
+			i++
+		} else {
+			maxReach = 2*maxReach + 1
+			patches++
+		}
+	}
+
+	return patches
+}
