89c30dcb8952059e58ca4ee7062a713c6ea324d3
0xff-dev <stevenshuang521@gmail.com>
2024-06-17 08:54:00 +0800

    Apply Operations To Make STring empty

---
+++ leetcode/3039ApplyOperationsToMakeStringEmpty.go
@@ -0,0 +1,28 @@
+package leetcode
+
+import (
+	"sort"
+	"strings"
+)
+
+func lastNonEmptyString(s string) string {
+	count := [26][2]int{}
+	mc := 0
+	for i, b := range s {
+		count[b-'a'][0]++
+		count[b-'a'][1] = i
+		mc = max(mc, count[b-'a'][0])
+	}
+	indies := []int{}
+	for i := 0; i < 26; i++ {
+		if count[i][0] == mc {
+			indies = append(indies, count[i][1])
+		}
+	}
+	sort.Ints(indies)
+	sb := strings.Builder{}
+	for _, i := range indies {
+		sb.WriteByte(s[i])
+	}
+	return sb.String()
+}
