ae49a31fce3e9ea38da74fc06bf130ff999b50a0
0xff-dev <stevenshuang521@gmail.com>
2024-06-18 23:24:07 +0800

    use seg tree

--- leetcode/826MostProfitAssigningWork.go
+++ leetcode/826MostProfitAssigningWork.go
@@ -1,8 +1,39 @@
 package leetcode
 
 import (
+	"math"
 	"sort"
 )
+
+type SegmentTreeNode struct {
+	Left, Right, Max      int
+	LeftChild, RightChild *SegmentTreeNode
+}
+
+func buildSegmentTree(nums []workProfit, left, right int) *SegmentTreeNode {
+	if left == right {
+		return &SegmentTreeNode{Left: left, Right: right, Max: nums[left].p}
+	}
+
+	mid := (left + right) / 2
+	leftNode := buildSegmentTree(nums, left, mid)
+	rightNode := buildSegmentTree(nums, mid+1, right)
+	max := max(leftNode.Max, rightNode.Max)
+
+	return &SegmentTreeNode{Left: left, Right: right, Max: max, LeftChild: leftNode, RightChild: rightNode}
+}
+
+func queryMax(root *SegmentTreeNode, left, right int) int {
+	if root.Left >= left && root.Right <= right {
+		return root.Max
+	}
+
+	if root.Right < left || root.Left > right {
+		return math.MinInt32
+	}
+
+	return max(queryMax(root.LeftChild, left, right), queryMax(root.RightChild, left, right))
+}
 
 type workProfit struct {
 	d, p int
@@ -20,6 +51,7 @@ func maxProfitAssignment(difficulty []int, profit []int, worker []int) int {
 		return dp[i].d < dp[j].d
 	})
 
+	tree := buildSegmentTree(dp, 0, l-1)
 	ans := 0
 	for _, n := range worker {
 		idx := sort.Search(l, func(i int) bool {
@@ -27,11 +59,9 @@ func maxProfitAssignment(difficulty []int, profit []int, worker []int) int {
 		})
 		// [0, idx)在个区间找到最大值
 		// 不行就上先段树
-		m := 0
-		for i := 0; i < idx; i++ {
-			m = max(m, dp[i].p)
+		if idx != 0 {
+			ans += queryMax(tree, 0, idx-1)
 		}
-		ans += m
 	}
 
 	return ans
