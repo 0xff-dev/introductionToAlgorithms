bedafe56e98fc45d8d11d42afd45b832a56a2cf0
0xff-dev <stevenshuang521@gmail.com>
2024-06-27 08:51:29 +0800

    Find Cneter Of Star Graph

---
+++ leetcode/1791FindCenterOfStarGraph.go
@@ -0,0 +1,12 @@
+package leetcode
+
+func findCenter(edges [][]int) int {
+	a, b := edges[0], edges[1]
+	if a[0] == b[1] || a[0] == b[0] {
+		return a[0]
+	}
+	if a[1] == b[1] || a[1] == b[0] {
+		return a[1]
+	}
+	return -1
+}
