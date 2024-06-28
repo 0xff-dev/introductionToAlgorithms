3954371515cfb06ea549f6f22ed9a805d7f41254
0xff-dev <stevenshuang521@gmail.com>
2024-06-28 08:56:52 +0800

    Maximum Total Importance Of Roads

---
+++ leetcode/2285MaximumTotalImportanceOfRoads.go
@@ -0,0 +1,20 @@
+package leetcode
+
+import "sort"
+
+func maximumImportance(n int, roads [][]int) int64 {
+	ans := int64(0)
+
+	// 检查入读和出度
+	in := make([]int, n)
+	for _, r := range roads {
+		in[r[0]]++
+		in[r[1]]++
+	}
+	sort.Ints(in)
+	v := int64(1)
+	for _, i := range in {
+		ans += v * int64(i)
+	}
+	return ans
+}
