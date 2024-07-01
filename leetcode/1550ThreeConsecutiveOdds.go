da93e0ebe797d94dd7dbb696cb39f3e0f897d851
0xff-dev <stevenshuang521@gmail.com>
2024-07-01 08:50:58 +0800

    Three Consecutive Odds

---
+++ leetcode/1550ThreeConsecutiveOdds.go
@@ -0,0 +1,10 @@
+package leetcode
+
+func threeConsecutiveOdds(arr []int) bool {
+	for i := 0; i < len(arr)-2; i++ {
+		if arr[i]&1 == 1 && arr[i+1]&1 == 1 && arr[i+2]&1 == 1 {
+			return true
+		}
+	}
+	return false
+}
