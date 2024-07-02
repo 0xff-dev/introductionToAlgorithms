e458916c1cbfce73b5e9a0ca2b8e911a87224973
0xff-dev <stevenshuang521@gmail.com>
2024-07-02 08:41:39 +0800

    Intersection Of Two Arrays II

---
+++ leetcode/350IntersectionofTwoArraysII.go
@@ -0,0 +1,16 @@
+package leetcode
+
+func intersect350(nums1 []int, nums2 []int) []int {
+	in := [1001]int{}
+	for _, n := range nums1 {
+		in[n]++
+	}
+	ans := make([]int, 0)
+	for _, n := range nums2 {
+		if in[n] > 0 {
+			ans = append(ans, n)
+			in[n]--
+		}
+	}
+	return ans
+}
