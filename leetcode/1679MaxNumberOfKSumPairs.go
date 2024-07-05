30cfef798ec035d335fb43cbc558611281566f4e
0xff-dev <stevenshuang521@gmail.com>
2024-07-05 08:50:24 +0800

    max Number Of K Sum Pairs

---
+++ leetcode/1679MaxNumberOfKSumPairs.go
@@ -0,0 +1,23 @@
+package leetcode
+
+import "sort"
+
+func maxOperations(nums []int, k int) int {
+	sort.Ints(nums)
+	s, e := 0, len(nums)-1
+	ans := 0
+	for s < e {
+		sum := nums[s] + nums[e]
+		if sum == k {
+			ans++
+			s, e = s+1, e-1
+			continue
+		}
+		if sum < k {
+			s++
+		} else {
+			e--
+		}
+	}
+	return ans
+}
