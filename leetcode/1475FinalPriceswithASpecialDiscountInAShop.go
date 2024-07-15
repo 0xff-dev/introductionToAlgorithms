682518585a3faa546139156f25523ccf6597bba2
0xff-dev <stevenshuang521@gmail.com>
2024-07-15 22:19:40 +0800

    Final Prices With a Special Discount In a Shop

---
+++ leetcode/1475FinalPriceswithASpecialDiscountInAShop.go
@@ -0,0 +1,16 @@
+package leetcode
+
+func finalPrices(prices []int) []int {
+	l := len(prices)
+	dst := make([]int, l)
+	copy(dst, prices)
+	for i := l - 2; i >= 0; i-- {
+		for j := i + 1; j < l; j++ {
+			if prices[j] <= prices[i] {
+				dst[i] -= prices[j]
+				break
+			}
+		}
+	}
+	return dst
+}
