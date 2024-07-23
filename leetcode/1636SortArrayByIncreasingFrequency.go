62861abcdb97ea8125fc5917612ca52913fae7fe
0xff-dev <stevenshuang521@gmail.com>
2024-07-23 08:49:11 +0800

    Sort Array By Increasing Frequency

---
+++ leetcode/1636SortArrayByIncreasingFrequency.go
@@ -0,0 +1,17 @@
+package leetcode
+
+import "sort"
+
+func frequencySort1636(nums []int) []int {
+	freq := make(map[int]int)
+	for _, n := range nums {
+		freq[n]++
+	}
+	sort.Slice(nums, func(i, j int) bool {
+		if freq[nums[i]] == freq[nums[j]] {
+			return nums[i] > nums[j]
+		}
+		return freq[nums[i]] < freq[nums[j]]
+	})
+	return nums
+}
