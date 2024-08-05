74b1ed3bf04a9a9b1167fe0067582be889325b76
0xff-dev <stevenshuang521@gmail.com>
2024-08-05 08:54:02 +0800

    Kth Distinct String In An Array

---
+++ leetcode/2053KthDistinctStringInAnArray.go
@@ -0,0 +1,19 @@
+package leetcode
+
+func kthDistinct(arr []string, k int) string {
+	count := make(map[string]int)
+	for _, s := range arr {
+		count[s]++
+	}
+
+	for i := range arr {
+		if count[arr[i]] > 1 {
+			continue
+		}
+		k--
+		if k == 0 {
+			return arr[i]
+		}
+	}
+	return ""
+}
