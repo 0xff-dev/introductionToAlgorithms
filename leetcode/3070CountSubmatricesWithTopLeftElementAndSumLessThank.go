2111079b1c6d36e4a4cb52cb9935d4b3e1377cf6
0xff-dev <stevenshuang521@gmail.com>
2024-08-07 09:07:04 +0800

    count Submatrices With TopLeft Element And Sum Less Than K

---
+++ leetcode/3070CountSubmatricesWithTopLeftElementAndSumLessThank.go
@@ -0,0 +1,27 @@
+package leetcode
+
+func countSubmatrices(grid [][]int, k int) int {
+	if grid[0][0] > k {
+		return 0
+	}
+	ans := 1
+	for i := 1; i < len(grid[0]); i++ {
+		grid[0][i] = grid[0][i-1] + grid[0][i]
+		if grid[0][i] <= k {
+			ans++
+		}
+
+	}
+	for r := 1; r < len(grid); r++ {
+		cur := 0
+		for c := 0; c < len(grid[0]); c++ {
+			cur += grid[r][c]
+			tmp := cur + grid[r-1][c]
+			if tmp <= k {
+				ans++
+			}
+			grid[r][c] = tmp
+		}
+	}
+	return ans
+}
