d2ea574d602030a686e97241a7b96ab269cb954b
0xff-dev <stevenshuang521@gmail.com>
2024-08-19 09:10:04 +0800

    Most Common Word

---
+++ leetcode/819MostCommonWord.go
@@ -0,0 +1,40 @@
+package leetcode
+
+import "strings"
+
+var skipSymbol = map[byte]struct{}{
+	'!': {}, '?': {}, '\'': {}, ',': {}, ';': {}, '.': {},
+}
+
+func mostCommonWord(paragraph string, banned []string) string {
+	words := strings.Split(paragraph, " ")
+	bm := make(map[string]struct{})
+	for _, b := range banned {
+		bm[b] = struct{}{}
+	}
+	count := make(map[string]int)
+	for _, word := range words {
+		lower := strings.ToLower(word)
+		i := 0
+		for ; i < len(word); i++ {
+			if _, ok := skipSymbol[word[i]]; ok {
+				break
+			}
+		}
+		w := lower[:i]
+		if _, ok := bm[w]; ok {
+			continue
+		}
+
+		count[w]++
+	}
+	ans := 0
+	ansKey := ""
+	for k, c := range count {
+		if c > ans {
+			ansKey = k
+			ans = c
+		}
+	}
+	return ansKey
+}
