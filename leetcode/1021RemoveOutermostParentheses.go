fc1df195dc8f6decb046c418f5d06d3a587d3717
0xff-dev <stevenshuang521@gmail.com>
2024-08-25 14:53:58 +0800

    Remove Outermost Parentheses

---
+++ leetcode/1021RemoveOutermostParentheses.go
@@ -0,0 +1,23 @@
+package leetcode
+
+import "strings"
+
+func removeOuterParentheses(s string) string {
+	left := 1
+	leftIndex := 0
+	ans := strings.Builder{}
+	for i := 1; i < len(s); i++ {
+		if s[i] == '(' {
+			left++
+			continue
+		}
+		left--
+		if left == 0 {
+			ans.WriteString(s[leftIndex+1 : i])
+			leftIndex = i + 1
+			left = 1
+			i++
+		}
+	}
+	return ans.String()
+}
