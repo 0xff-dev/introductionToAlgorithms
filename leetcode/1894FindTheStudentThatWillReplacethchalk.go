2df5ecdb0da32472afc731612796c51989fff632
0xff-dev <stevenshuang521@gmail.com>
2024-09-02 09:17:34 +0800

    optimize algorithm 1894

--- leetcode/1894FindTheStudentThatWillReplacethchalk.go
+++ leetcode/1894FindTheStudentThatWillReplacethchalk.go
@@ -4,27 +4,18 @@ import "sort"
 
 func chalkReplacer(chalk []int, k int) int {
 	l := len(chalk)
-	ans := 0
 	for i := 1; i < l; i++ {
 		chalk[i] += chalk[i-1]
 	}
-	if k > chalk[l-1] {
-		k %= chalk[l-1]
+	k %= chalk[l-1]
+	idx := sort.Search(l, func(i int) bool {
+		return chalk[i] >= k
+	})
+	if idx == l {
+		return 0
 	}
-
-	for k > 0 {
-		idx := sort.Search(l, func(i int) bool {
-			return chalk[i] >= k
-		})
-		if idx == l {
-			k -= chalk[l-1]
-			continue
-		}
-		ans = idx
-		if chalk[idx] == k {
-			ans = (idx + 1) % l
-		}
-		break
+	if chalk[idx] == k {
+		return (idx + 1) % l
 	}
-	return ans
+	return idx
 }
