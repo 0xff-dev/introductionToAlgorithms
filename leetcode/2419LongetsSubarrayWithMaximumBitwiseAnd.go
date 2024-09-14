5c6e781bde2df5de24e3e6c6636ba4f45f4d7dc8
0xff-dev <stevenshuang521@gmail.com>
2024-09-14 09:50:16 +0800

    Longest Subarray With Maximum Bitwise And

---
+++ leetcode/2419LongetsSubarrayWithMaximumBitwiseAnd.go
@@ -0,0 +1,22 @@
+package leetcode
+
+func longestSubarray2419(nums []int) int {
+	m := 0
+	l := 0
+	ans := 0
+	for _, n := range nums {
+		if n == m {
+			l++
+			ans = max(ans, l)
+			continue
+		}
+		if n > m {
+			m = n
+			l = 1
+			ans = 1
+			continue
+		}
+		l = 0
+	}
+	return ans
+}
