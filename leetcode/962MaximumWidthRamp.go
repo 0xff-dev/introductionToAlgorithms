685dbfe0540ec9ac7dcd0655fc77e532a56a2f61
0xff-dev <stevenshuang521@gmail.com>
2024-10-10 23:18:42 +0800

    Maximum Width Ramp

---
+++ leetcode/962MaximumWidthRamp.go
@@ -0,0 +1,25 @@
+package leetcode
+
+func maxWidthRamp(nums []int) int {
+	n := len(nums)
+	stack := make([]int, n)
+
+	index := -1
+	for i := 0; i < n; i++ {
+		if index == -1 || nums[stack[index]] > nums[i] {
+			index++
+			stack[index] = i
+		}
+	}
+
+	ans := 0
+
+	for j := n - 1; j >= 0; j-- {
+		for index != -1 && nums[stack[index]] <= nums[j] {
+			ans = max(ans, j-stack[index])
+			index--
+		}
+	}
+
+	return ans
+}
