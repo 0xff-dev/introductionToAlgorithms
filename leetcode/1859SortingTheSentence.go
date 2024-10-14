0a33fb4f578a205b5b771739505e1bb2a04b0661
0xff-dev <stevenshuang521@gmail.com>
2024-10-14 09:16:39 +0800

    Sorting The Senetence

---
+++ leetcode/1859SortingTheSentence.go
@@ -0,0 +1,23 @@
+package leetcode
+
+import (
+	"sort"
+	"strings"
+)
+
+func sortSentence(s string) string {
+	array := strings.Split(s, " ")
+	sort.Slice(array, func(i, j int) bool {
+		a, b := array[i], array[j]
+		la, lb := a[len(a)-1], b[len(b)-1]
+		return la < lb
+	})
+	buf := strings.Builder{}
+	for i, str := range array {
+		buf.WriteString(str[:len(str)-1])
+		if i != len(array)-1 {
+			buf.WriteString(" ")
+		}
+	}
+	return buf.String()
+}
