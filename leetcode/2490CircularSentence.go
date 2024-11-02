d45e1c91dc7bd1c7ef89c8dc511750de43c80dc1
0xff-dev <stevenshuang521@gmail.com>
2024-11-02 09:26:49 +0800

    Circular Sentence

---
+++ leetcode/2490CircularSentence.go
@@ -0,0 +1,16 @@
+package leetcode
+
+func isCircularSentence(sentence string) bool {
+	l := len(sentence)
+	if sentence[0] != sentence[l-1] {
+		return false
+	}
+	for i := range sentence {
+		if sentence[i] == ' ' {
+			if sentence[i-1] != sentence[i+1] {
+				return false
+			}
+		}
+	}
+	return true
+}
