e193c7f7fd189554b4ae0fdff5b995246c5445a2
0xff-dev <stevenshuang521@gmail.com>
2024-11-04 09:02:55 +0800

    String Compression III

---
+++ leetcode/3163StringCompressionIII.go
@@ -0,0 +1,33 @@
+package leetcode
+
+import "strings"
+
+func compressedString(word string) string {
+	buf := strings.Builder{}
+	count := uint8(1)
+	bs := []byte(word)
+	pre := bs[0]
+	for i := 1; i < len(bs); i++ {
+		b := bs[i]
+		if b == pre {
+			count++
+			if count == 9 {
+				buf.WriteByte(count + '0')
+				buf.WriteByte(b)
+				count = 0
+			}
+			continue
+		}
+		if count != 0 {
+			buf.WriteByte(count + '0')
+			buf.WriteByte(pre)
+		}
+		pre = b
+		count = 1
+	}
+	if count != 0 {
+		buf.WriteByte(count + '0')
+		buf.WriteByte(pre)
+	}
+	return buf.String()
+}
