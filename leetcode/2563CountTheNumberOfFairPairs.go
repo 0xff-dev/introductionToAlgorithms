f75ee76667da9de314274f0c63b731081049de3a
0xff-dev <stevenshuang521@gmail.com>
2024-11-13 09:14:57 +0800

    count The Number Of Fair Pairs

---
+++ leetcode/2563CountTheNumberOfFairPairs.go
@@ -0,0 +1,27 @@
+package leetcode
+
+import "sort"
+
+func countFairPairs(nums []int, lower, upper int) int64 {
+	sort.Ints(nums)
+	var ans int64 = 0
+	l := len(nums)
+	for i := 0; i < len(nums); i++ {
+		// 1, 2, 3, 4
+		tmpL := l - i - 1
+		leftIndex := sort.Search(tmpL, func(ii int) bool {
+			return nums[i]+nums[ii+1+i] >= lower
+		})
+		if leftIndex == tmpL {
+			continue
+		}
+		rightIndex := sort.Search(tmpL, func(ii int) bool {
+			return nums[i]+nums[ii+1+i] > upper
+		})
+		if rightIndex == tmpL && nums[i]+nums[l-1] > upper {
+			continue
+		}
+		ans += int64(rightIndex - leftIndex)
+	}
+	return ans
+}
