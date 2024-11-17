c2fbe280e38a527b7fc97b5632784b6fe8d2f73f
0xff-dev <stevenshuang521@gmail.com>
2024-11-17 12:46:54 +0800

    add solution for 862

--- leetcode/862ShortestSubarrayWithSumAtLeastK.go
+++ leetcode/862ShortestSubarrayWithSumAtLeastK.go
@@ -1,10 +1,10 @@
 package leetcode
 
 import (
-	"container/list"
-	"math"
+	"container/heap"
 )
 
+/*
 func shortestSubarray(nums []int, k int) int {
 	n := len(nums)
 	prefixSum := make([]int, n+1)
@@ -31,4 +31,50 @@ func shortestSubarray(nums []int, k int) int {
 		return -1
 	}
 	return minLength
+}
+*/
+
+type heap862 struct {
+	v, i int
+}
+type heap862list []heap862
+
+func (h *heap862list) Len() int {
+	return len(*h)
+}
+func (h *heap862list) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
+}
+
+func (h *heap862list) Less(i, j int) bool {
+	return (*h)[i].v < (*h)[j].v
+}
+func (h *heap862list) Push(x any) {
+	*h = append(*h, x.(heap862))
+}
+func (h *heap862list) Pop() any {
+	old := *h
+	l := len(old)
+	x := old[l-1]
+	*h = old[:l-1]
+	return x
+}
+
+func shortestSubarray(nums []int, k int) int {
+	h := heap862list{{0, -1}}
+	cur := 0
+	l := len(nums)
+	ans := -1
+	i := 0
+	for ; i < l; i++ {
+		cur += nums[i]
+		for len(h) > 0 && cur-h[0].v >= k {
+			top := heap.Pop(&h).(heap862)
+			if ans == -1 || ans > i-top.i {
+				ans = i - top.i
+			}
+		}
+		heap.Push(&h, heap862{cur, i})
+	}
+	return ans
 }
