05086291ba31840d6eedcb4709b4024c086ec8a9
0xff-dev <stevenshuang521@gmail.com>
2024-11-26 08:55:21 +0800

    Find Champion II

---
+++ leetcode/2924FindChampionII.go
@@ -0,0 +1,20 @@
+package leetcode
+
+func findChampion(n int, edges [][]int) int {
+	in := make(map[int]int)
+	for _, e := range edges {
+		in[e[1]]++
+	}
+	ans := -1
+	cur := 0
+	for i := range n {
+		if in[i] == 0 {
+			cur++
+			ans = i
+			if cur > 1 {
+				return -1
+			}
+		}
+	}
+	return ans
+}
