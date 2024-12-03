84cd5f376a02fd0baa4a94edb7c3d7c80d54c207
0xff-dev <stevenshuang521@gmail.com>
2024-12-03 09:09:58 +0800

    Print Words Vertically

---
+++ leetcode/1324PritWordsVertically.go
@@ -0,0 +1,33 @@
+package leetcode
+
+import (
+	"strings"
+)
+
+func printVertically(s string) []string {
+	words := strings.Split(s, " ")
+	ml := 0
+	for _, w := range words {
+		ml = max(ml, len(w))
+	}
+	ans := make([]string, 0)
+	buf := strings.Builder{}
+	for index := 0; index < ml; index++ {
+		tailSpaces := 0
+		for _, w := range words {
+			if index < len(w) {
+				buf.WriteByte(w[index])
+				tailSpaces = 0
+				continue
+			}
+			buf.WriteByte(' ')
+			tailSpaces++
+		}
+		cur := buf.String()
+		l := len(cur) - tailSpaces
+		cur = cur[:l]
+		ans = append(ans, cur)
+		buf.Reset()
+	}
+	return ans
+}
