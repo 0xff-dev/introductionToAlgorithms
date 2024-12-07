7e437cd7a12bfeb78211032de0a2755697ebe53e
0xff-dev <stevenshuang521@gmail.com>
2024-12-07 12:19:39 +0800

    Minimum Limit Of Balls In A Bag

---
+++ leetcode/1760MinimumLimitOfBallsInABag.go
@@ -0,0 +1,35 @@
+package leetcode
+
+func minimumSize(nums []int, maxOperations int) int {
+	var can func(int) bool
+	can = func(limit int) bool {
+		op := maxOperations
+		for _, n := range nums {
+			if n <= limit {
+				continue
+			}
+			// 9 /3
+			times := n / limit
+			if n%limit == 0 {
+				times--
+			}
+			if times > op {
+				return false
+			}
+			op -= times
+		}
+		return true
+	}
+	left, right := 1, 1000000001
+	ans := -1
+	for left < right {
+		mid := (left + right) / 2
+		if can(mid) {
+			right = mid
+			ans = mid
+			continue
+		}
+		left = mid + 1
+	}
+	return ans
+}
