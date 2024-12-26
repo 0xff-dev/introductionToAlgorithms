744e074f4238118458e0e89f77aff4acd5bfb553
0xff-dev <stevenshuang521@gmail.com>
2024-12-26 22:06:11 +0800

    interval List Intersections

---
+++ leetcode/986IntervalListIntersections.go
@@ -0,0 +1,29 @@
+package leetcode
+
+func intervalIntersection(firstList [][]int, secondList [][]int) [][]int {
+	la, lb := len(firstList), len(secondList)
+	if la == 0 || lb == 0 {
+		return nil
+	}
+	a, b := 0, 0
+	ans := make([][]int, 0)
+	for a < la && b < lb {
+		af, as := firstList[a][0], firstList[a][1]
+		bf, bs := secondList[b][0], secondList[b][1]
+		if af > bs {
+			b++
+			continue
+		}
+		if bf > as {
+			a++
+			continue
+		}
+		ans = append(ans, []int{max(af, bf), min(as, bs)})
+		if bs > as {
+			a++
+		} else {
+			b++
+		}
+	}
+	return ans
+}
