3e24afa1d20a03b95421150b13a4a91e657680bf
0xff-dev <stevenshuang521@gmail.com>
2025-01-01 21:04:35 +0800

    Partition Array According To Given Pivot

---
+++ leetcode/2161PartitionArrayAccordingToGivenPivot.go
@@ -0,0 +1,22 @@
+package leetcode
+
+func pivotArray(nums []int, pivot int) []int {
+	i, j := 0, len(nums)-1
+	res := make([]int, len(nums))
+	for k := 0; k < len(nums); k++ {
+		if nums[k] < pivot {
+			res[i] = nums[k]
+			i++
+		}
+	}
+	for k := len(nums) - 1; k >= 0; k-- {
+		if nums[k] > pivot {
+			res[j] = nums[k]
+			j--
+		}
+	}
+	for ; i <= j; i++ {
+		res[i] = pivot
+	}
+	return res
+}
