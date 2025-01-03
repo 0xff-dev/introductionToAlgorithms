d247e542cf91df2f6a73d6c817a066833a566b90
0xff-dev <stevenshuang521@gmail.com>
2025-01-03 08:51:20 +0800

    Number Of Ways To Split Array

---
+++ leetcode/2270NumberOfWaysToSplitArray.go
@@ -0,0 +1,15 @@
+package leetcode
+
+func waysToSplitArray(nums []int) int {
+	ans := 0
+	l := len(nums)
+	for i := 1; i < l; i++ {
+		nums[i] += nums[i-1]
+	}
+	for i := 0; i < l-1; i++ {
+		if nums[i] >= nums[l-1]-nums[i] {
+			ans++
+		}
+	}
+	return ans
+}
