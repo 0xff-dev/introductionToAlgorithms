7b34675515c9ae8d93776a41232796fed0d5c154
0xff-dev <stevenshuang521@gmail.com>
2025-01-08 09:25:48 +0800

    Count Prefix And Suffix Pairs I

---
+++ leetcode/3042CountPrefixAndSuffixPairsI.go
@@ -0,0 +1,15 @@
+package leetcode
+
+import "strings"
+
+func countPrefixSuffixPairs(words []string) int {
+	ans := 0
+	for i := 0; i < len(words); i++ {
+		for j := i + 1; j < len(words); j++ {
+			if strings.HasPrefix(words[j], words[i]) && strings.HasSuffix(words[j], words[i]) {
+				ans++
+			}
+		}
+	}
+	return ans
+}
