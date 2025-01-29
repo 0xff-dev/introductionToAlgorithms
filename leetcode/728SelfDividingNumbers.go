632c1aad2bb7cd33150f229491b813c6b776f4d4
0xff-dev <stevenshuang521@gmail.com>
2025-01-29 16:18:31 +0800

    Self Dividing Numbers

---
+++ leetcode/728SelfDividingNumbers.go
@@ -0,0 +1,26 @@
+package leetcode
+
+func selfDividingNumbers(left int, right int) []int {
+	var res []int
+	var ok func(int) bool
+	ok = func(n int) bool {
+		self := n
+		for n > 0 {
+			mod := n % 10
+			if mod == 0 {
+				return false
+			}
+			if self%mod != 0 {
+				return false
+			}
+			n /= 10
+		}
+		return true
+	}
+	for i := left; i <= right; i++ {
+		if ok(i) {
+			res = append(res, i)
+		}
+	}
+	return res
+}
