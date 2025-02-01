753afd7d982a6ab74f2a832ec9414e746d87ca5a
0xff-dev <stevenshuang521@gmail.com>
2025-02-01 08:59:33 +0800

    Special Array I

---
+++ leetcode/3151SpecialArrayI.go
@@ -0,0 +1,13 @@
+package leetcode
+
+func isArraySpecial3151(nums []int) bool {
+	if len(nums) == 1 {
+		return true
+	}
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i]&1 == nums[i+1]&1 {
+			return false
+		}
+	}
+	return true
+}
