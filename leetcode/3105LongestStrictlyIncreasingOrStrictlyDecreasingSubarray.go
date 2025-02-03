14fbe22537bb7f761753bfca9eb13101214d83a1
0xff-dev <stevenshuang521@gmail.com>
2025-02-03 09:51:10 +0800

    Longest Strictly Increasing Or Strictly Decreasing Subarray

---
+++ leetcode/3105LongestStrictlyIncreasingOrStrictlyDecreasingSubarray.go
@@ -0,0 +1,24 @@
+package leetcode
+
+func helper(nums []int, less func(i, j int) bool) int {
+	l := 1
+	res := 1
+	for i := 1; i < len(nums); i++ {
+		if less(nums[i-1], nums[i]) {
+			l++
+			res = max(res, l)
+			continue
+		}
+		l = 1
+	}
+	return max(res, l)
+}
+func longestMonotonicSubarray(nums []int) int {
+	a := helper(nums, func(i, j int) bool {
+		return i < j
+	})
+	b := helper(nums, func(i, j int) bool {
+		return i > j
+	})
+	return max(a, b)
+}
