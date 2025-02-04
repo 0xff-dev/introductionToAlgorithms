abe0f8f0b13b8702759eb550b007c2990b8175bb
0xff-dev <stevenshuang521@gmail.com>
2025-02-04 09:27:12 +0800

    Maximum Ascending Subarray Sum

---
+++ leetcode/1800MaximumAscendingSubarraySum.go
@@ -0,0 +1,18 @@
+package leetcode
+
+func maxAscendingSum(nums []int) int {
+	sum := 0
+	pre := 0
+	ans := 0
+	for _, n := range nums {
+		if n > pre {
+			sum += n
+			pre = n
+			continue
+		}
+		ans = max(ans, sum)
+		sum = n
+		pre = n
+	}
+	return max(ans, sum)
+}
