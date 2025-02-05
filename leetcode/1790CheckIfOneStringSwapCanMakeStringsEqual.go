df289a0bdd11a0663de0d757a2ba29f355f1dd60
0xff-dev <stevenshuang521@gmail.com>
2025-02-05 09:13:56 +0800

    Check If One String SWap Can Make Strings Equal

---
+++ leetcode/1790CheckIfOneStringSwapCanMakeStringsEqual.go
@@ -0,0 +1,25 @@
+package leetcode
+
+func areAlmostEqual(s1 string, s2 string) bool {
+	if s1 == s2 {
+		return true
+	}
+	var a, b uint8
+	diff := 0
+	for i := range s1 {
+		if s1[i] == s2[i] {
+			continue
+		}
+		diff++
+		if diff == 1 {
+			a, b = s1[i], s2[i]
+		} else if diff == 2 {
+			if !(a == s2[i] && b == s1[i]) {
+				return false
+			}
+		} else {
+			return false
+		}
+	}
+	return diff != 1
+}
