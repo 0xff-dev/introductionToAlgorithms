57b622130cecb72e2eea1bc79d150d176a232f7a
0xff-dev <stevenshuang521@gmail.com>
2025-02-06 09:30:01 +0800

    Find the Smallest Divisor Given A Threshold

---
+++ leetcode/1283FindTheSmallestDivisorGivenAThreshold.go
@@ -0,0 +1,29 @@
+package leetcode
+
+import (
+	"slices"
+	"sort"
+)
+
+func smallestDivisor(nums []int, threshold int) int {
+	m := slices.Max(nums)
+	var ok func(int) int
+	ok = func(divisor int) int {
+		sum := 0
+		for _, n := range nums {
+			a := n / divisor
+			if n%divisor != 0 {
+				a++
+			}
+			sum += a
+		}
+		return sum
+	}
+	idx := sort.Search(m+1, func(n int) bool {
+		if n == 0 {
+			return false
+		}
+		return ok(n) <= threshold
+	})
+	return idx
+}
