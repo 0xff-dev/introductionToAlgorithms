9ac17af8c5ef20832ae9facf5cc4905f5ed93468
0xff-dev <stevenshuang521@gmail.com>
2025-02-07 09:17:03 +0800

    Find The Number Of Distinct Colors Among The Balls

---
+++ leetcode/3160FindTheNumberOfDistinctColorsAmongTheBalls.go
@@ -0,0 +1,22 @@
+package leetcode
+
+func queryResults(limit int, queries [][]int) []int {
+	ans := make([]int, len(queries))
+	colors := make(map[int]int)
+	colored := make(map[int]int)
+	for i, q := range queries {
+		ball, color := q[0], q[1]
+		sourceColor, ok := colored[ball]
+		if ok {
+			colors[sourceColor]--
+			if colors[sourceColor] == 0 {
+				delete(colors, sourceColor)
+			}
+		}
+
+		colors[color]++
+		colored[ball] = color
+		ans[i] = len(colors)
+	}
+	return ans
+}
