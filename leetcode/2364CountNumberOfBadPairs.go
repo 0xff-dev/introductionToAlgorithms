bf6c80a89e5c74df893de29606fbc51a564a2cd5
0xff-dev <stevenshuang521@gmail.com>
2025-02-09 17:22:04 +0800

    Count Number Of Bad Pairs

---
+++ leetcode/2364CountNumberOfBadPairs.go
@@ -0,0 +1,17 @@
+package leetcode
+
+func countBadPairs(nums []int) int64 {
+	l := len(nums)
+	diff := make(map[int]int64)
+	diff[0-nums[0]] = 1
+
+	var ans, count int64
+	count = 1
+	for i := 1; i < l; i++ {
+		a := i - nums[i]
+		ans += count - diff[a]
+		diff[a]++
+		count++
+	}
+	return ans
+}
