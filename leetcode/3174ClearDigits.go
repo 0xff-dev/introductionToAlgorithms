b67f4e5bd4c50f570f900b84c5e21f62fcf7792a
0xff-dev <stevenshuang521@gmail.com>
2025-02-10 08:57:34 +0800

    Clear Digits

---
+++ leetcode/3174ClearDigits.go
@@ -0,0 +1,16 @@
+package leetcode
+
+func clearDigits(s string) string {
+	bs := []byte(s)
+	index := -1
+	for i := range len(bs) {
+
+		if !(bs[i] >= '0' && bs[i] <= '9') {
+			index++
+			bs[index] = bs[i]
+			continue
+		}
+		index--
+	}
+	return string(bs[:index+1])
+}
