b2bd2e466e49dcae0a70f224b84c8d8558cc57a8
0xff-dev <stevenshuang521@gmail.com>
2025-02-12 09:03:41 +0800

    Max Sum of a Pair With Equal Sum of Digits

---
+++ leetcode/2342MaxSumOfAPairWithEqualSumOfDigits.go
@@ -0,0 +1,39 @@
+package leetcode
+
+func countOfOne2342(n int) int {
+	one := 0
+	for n > 0 {
+		one += n % 10
+		n /= 10
+	}
+	return one
+}
+
+func maximumSum2342(nums []int) int {
+	ans := -1
+	sum := make(map[int][2]int)
+	for i := range nums {
+		one := countOfOne2342(nums[i])
+		v, ok := sum[one]
+		if !ok {
+			sum[one] = [2]int{nums[i], -1}
+			continue
+		}
+		if nums[i] >= sum[one][0] {
+			v[1] = v[0]
+			v[0] = nums[i]
+			sum[one] = v
+			continue
+		}
+		if nums[i] > sum[one][1] {
+			v[1] = nums[i]
+			sum[one] = v
+		}
+	}
+	for _, v := range sum {
+		if v[0] != -1 && v[1] != -1 {
+			ans = max(ans, v[0]+v[1])
+		}
+	}
+	return ans
+}
