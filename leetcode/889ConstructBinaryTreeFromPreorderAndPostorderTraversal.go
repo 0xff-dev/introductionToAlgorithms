91d9de2d80be675dd775e56ce8e78837f3c1e005
0xff-dev <stevenshuang521@gmail.com>
2025-02-23 16:22:15 +0800

    Cosntruct Binary Tree From Preorder and Postorder Traversal

---
+++ leetcode/889ConstructBinaryTreeFromPreorderAndPostorderTraversal.go
@@ -0,0 +1,33 @@
+package leetcode
+
+func constructFromPrePost(preorder []int, postorder []int) *TreeNode {
+	if len(preorder) == 0 {
+		return nil
+	}
+	root := &TreeNode{
+		Val: preorder[0],
+	}
+	if len(preorder) == 1 {
+		return root
+	}
+
+	postIndex := 0
+	next := preorder[1]
+	for i := range postorder {
+		if postorder[i] == next {
+			postIndex = i
+			break
+		}
+	}
+	preIndex := 0
+	m := map[int]int{}
+	for i, n := range preorder {
+		m[n] = i
+	}
+	for i := 0; i <= postIndex; i++ {
+		preIndex = max(preIndex, m[postorder[i]])
+	}
+	root.Left = constructFromPrePost(preorder[1:preIndex+1], postorder[:postIndex+1])
+	root.Right = constructFromPrePost(preorder[preIndex+1:], postorder[postIndex+1:len(postorder)-1])
+	return root
+}
