7cdc853fd1176a9c447aff42178c853f0a6380e5
0xff-dev <stevenshuang521@gmail.com>
2025-03-02 15:47:25 +0800

    Merge Two 2D Arrays By Summing Values

---
+++ leetcode/2570MergeTwo2DArraysBySummingValues.go
@@ -0,0 +1,26 @@
+package leetcode
+
+func mergeArrays(nums1 [][]int, nums2 [][]int) [][]int {
+	if len(nums1) == 0 {
+		return nums2
+	}
+	if len(nums2) == 0 {
+		return nums1
+	}
+	res := [][]int{}
+	if nums1[0][0] == nums2[0][0] {
+		res = append(res, []int{nums1[0][0], nums1[0][1] + nums2[0][1]})
+		nums1 = nums1[1:]
+		nums2 = nums2[1:]
+	} else {
+		if nums1[0][0] < nums2[0][0] {
+			res = append(res, nums1[0])
+			nums1 = nums1[1:]
+		} else {
+			res = append(res, nums2[0])
+			nums2 = nums2[1:]
+		}
+	}
+	res = append(res, mergeArrays(nums1, nums2)...)
+	return res
+}
