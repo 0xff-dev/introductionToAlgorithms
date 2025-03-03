2c973ffc4c9504746987587e40f5f5ada06b08c6
0xff-dev <stevenshuang521@gmail.com>
2025-03-03 09:17:49 +0800

    Queries On Number Of Points Inside a Circle

---
+++ leetcode/1828QueriesOnNumberOfPointsInsideACircle.go
@@ -0,0 +1,17 @@
+package leetcode
+
+func countPoints(points [][]int, queries [][]int) []int {
+	ans := make([]int, len(queries))
+	for idx, q := range queries {
+		distance := q[2] * q[2]
+		for _, p := range points {
+			a := q[0] - p[0]
+			b := q[1] - p[1]
+			if a*a+b*b <= distance {
+				ans[idx]++
+			}
+		}
+	}
+	return ans
+}
+
