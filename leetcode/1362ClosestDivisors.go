2acbec2963507c29b4b9f8cd37f2b22b528d270b
0xff-dev <stevenshuang521@gmail.com>
2025-03-04 09:20:02 +0800

    Closest Divisors

---
+++ leetcode/1362ClosestDivisors.go
@@ -0,0 +1,32 @@
+package leetcode
+
+import "math"
+
+func factor(n int) (int, int) {
+	sn := int(math.Sqrt(float64(n)))
+	a, b := 1, n
+	for i := 1; i <= sn; i++ {
+		if n%i != 0 {
+			continue
+		}
+		a, b = i, n/i
+	}
+	return a, b
+}
+
+func closestDivisors(num int) []int {
+	a1, b1 := factor(num + 1)
+	a2, b2 := factor(num + 2)
+	diff1 := b1 - a1
+	if diff1 < 0 {
+		diff1 = -diff1
+	}
+	diff2 := b2 - a2
+	if diff2 < 0 {
+		diff2 = -diff2
+	}
+	if diff1 < diff2 {
+		return []int{a1, b1}
+	}
+	return []int{a2, b2}
+}
