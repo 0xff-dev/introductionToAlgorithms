7a0bfb7010788db1faa3d4532e3736ae3be0ecae
0xff-dev <stevenshuang521@gmail.com>
2025-03-15 18:30:10 +0800

    House Robber IV

---
+++ leetcode/2560HouseRobberIV.go
@@ -0,0 +1,37 @@
+package leetcode
+
+import "slices"
+
+func minCapability(nums []int, k int) int {
+	l := len(nums)
+	left, right := 0, slices.Max(nums)
+
+	capFn := func(capability int) bool {
+		count := 0
+		i := 0
+		for i < l {
+			if nums[i] <= capability {
+				count++
+				i++
+				if i < l {
+					i++
+				}
+			} else {
+				i++
+			}
+		}
+
+		return count >= k
+	}
+
+	for left < right {
+		mid := (left + right) / 2
+		if capFn(mid) {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+
+	return left
+}
