8de2e8f75d54f22aeef0379069858aca27992208
0xff-dev <stevenshuang521@gmail.com>
2025-03-18 22:24:26 +0800

    Longest Nice Subarray

---
+++ leetcode/2401LongestNiceSubarray.go
@@ -0,0 +1,28 @@
+package leetcode
+
+func longestNiceSubarray(nums []int) int {
+	if len(nums) == 1 {
+		return 1
+	}
+	cur := 0
+	start, end := 0, 0
+	ans := 0
+	for ; end < len(nums); end++ {
+		if cur&nums[end] == 0 {
+			cur |= nums[end]
+			continue
+		}
+		ans = max(ans, end-start)
+
+		for ; start <= end; start++ {
+			cur ^= nums[start]
+			if cur&nums[end] == 0 {
+				start++
+				cur |= nums[end]
+				break
+			}
+		}
+	}
+	ans = max(ans, end-start)
+	return ans
+}
