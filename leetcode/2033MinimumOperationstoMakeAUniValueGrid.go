bd259c53571039969221be5d6506cad115541bbd
0xff-dev <stevenshuang521@gmail.com>
2025-03-26 09:18:46 +0800

    Minimum Operations to Make A Uni-value Grid

---
+++ leetcode/2033MinimumOperationstoMakeAUniValueGrid.go
@@ -0,0 +1,27 @@
+package leetcode
+
+import "sort"
+
+func minOperations2033(grid [][]int, x int) int {
+	list := []int{}
+	rows, cols := len(grid), len(grid[0])
+	for i := range rows {
+		for j := range cols {
+			list = append(list, grid[i][j])
+		}
+	}
+	sort.Ints(list)
+	mid := list[len(list)/2]
+	ans := 0
+	for _, n := range list {
+		if n%x != mid%x {
+			return -1
+		}
+		diff := n - mid
+		if diff < 0 {
+			diff = -diff
+		}
+		ans += diff / x
+	}
+	return ans
+}
