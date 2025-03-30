ebbdf029128f94d0e2eb44c8d8ba67d0288768dd
0xff-dev <stevenshuang521@gmail.com>
2025-03-30 18:53:32 +0800

    Optimization Algorithm 3227

--- leetcode/3227VowelsGameInAString.go
+++ leetcode/3227VowelsGameInAString.go
@@ -1,11 +1,10 @@
 package leetcode
 
 func doesAliceWin(s string) bool {
-	cnt := 0
 	for _, e := range s {
 		if e == 'a' || e == 'e' || e == 'i' || e == 'o' || e == 'u' {
-			cnt++
+			return true
 		}
 	}
-	return cnt != 0
+	return false
 }
