4bb25f5212e0178e1178b38905f021145ca602ae
0xff-dev <stevenshuang521@gmail.com>
2025-04-17 09:30:11 +0800

    Count Equal And Divisible Pairs in an array

---
+++ leetcode/2176CountEqualAndDivisiblePairsInAnArray.go
@@ -0,0 +1,20 @@
+package leetcode
+
+func countPairs2176(nums []int, k int) int {
+	count := make(map[int][]int)
+	for i, n := range nums {
+		count[n] = append(count[n], i)
+	}
+	ans := 0
+	for _, list := range count {
+		l := len(list)
+		for i := 0; i < l-1; i++ {
+			for j := i + 1; j < l; j++ {
+				if (list[i]*list[j])%k == 0 {
+					ans++
+				}
+			}
+		}
+	}
+	return ans
+}
