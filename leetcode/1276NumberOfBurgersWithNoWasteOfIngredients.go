c786969f179ae6eae9a7bf30c68dbcfd06498b83
0xff-dev <stevenshuang521@gmail.com>
2025-04-19 20:17:21 +0800

    Number Of Burgers with No Waste Of Ingredients

---
+++ leetcode/1276NumberOfBurgersWithNoWasteOfIngredients.go
@@ -0,0 +1,16 @@
+package leetcode
+
+func numOfBurgers(tomatoSlices int, cheeseSlices int) []int {
+	// 4x + 2y = tomatoSlices
+	// 2x + 2y = 2*cheeseSlices
+	jumbo := tomatoSlices - 2*cheeseSlices
+	if jumbo < 0 || jumbo&1 == 1 {
+		return []int{}
+	}
+	a := jumbo / 2
+	b := cheeseSlices - a
+	if b < 0 {
+		return []int{}
+	}
+	return []int{a, b}
+}
