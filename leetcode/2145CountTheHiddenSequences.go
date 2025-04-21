95d10bf29a623bd08e9d4b6d1961a98ff8a68e58
0xff-dev <stevenshuang521@gmail.com>
2025-04-21 09:08:29 +0800

    Count The Hidden Sequences

---
+++ leetcode/2145CountTheHiddenSequences.go
@@ -0,0 +1,25 @@
+package leetcode
+
+import "math"
+
+func numberOfArrays2145(differences []int, lower int, upper int) int {
+	sum := int64(0)
+	var maxSum, minSum int64
+	maxSum, minSum = int64(0), math.MaxInt64
+	for _, n := range differences {
+		sum += int64(n)
+		maxSum = max(sum, maxSum)
+		minSum = min(sum, minSum)
+	}
+
+	ans := 0
+	il, ip := int64(lower), int64(upper)
+	for i := lower; i <= upper; i++ {
+		a := int64(i) + maxSum
+		b := int64(i) + minSum
+		if a >= il && a <= ip && b >= il && b <= ip {
+			ans++
+		}
+	}
+	return ans
+}
