e0738da91fe109f5a808e46b8ff900563f9ec891
0xff-dev <stevenshuang521@gmail.com>
2025-04-28 09:16:02 +0800

    Generate Binary Strings Without Adjacent Zeros

---
+++ leetcode/3211GenerateBinaryStringsWithoutAdjacentZeros.go
@@ -0,0 +1,20 @@
+package leetcode
+
+func validStrings(n int) []string {
+	var res []string
+	var dfs func(int, string, bool)
+	dfs = func(index int, cur string, isZero bool) {
+		if index == n {
+			res = append(res, cur)
+			return
+		}
+		if isZero {
+			dfs(index+1, cur+"1", false)
+			return
+		}
+		dfs(index+1, cur+"0", true)
+		dfs(index+1, cur+"1", false)
+	}
+	dfs(0, "", false)
+	return res
+}
