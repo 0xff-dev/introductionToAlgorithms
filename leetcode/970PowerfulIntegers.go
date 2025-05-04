de81aa3aee8162dd9cfa8a1937cc76d67064594b
0xff-dev <stevenshuang521@gmail.com>
2025-05-04 14:15:09 +0800

    Powerful Integers

---
+++ leetcode/970PowerfulIntegers.go
@@ -0,0 +1,39 @@
+package leetcode
+
+func powerfulIntegers(x int, y int, bound int) []int {
+	xm := map[int]struct{}{
+		1: struct{}{},
+	}
+	ym := map[int]struct{}{
+		1: struct{}{},
+	}
+	if x != 1 {
+		base := x
+		for base <= bound {
+			xm[base] = struct{}{}
+			base *= x
+		}
+	}
+	if y != 1 {
+		base := 1
+		for base <= bound {
+			ym[base] = struct{}{}
+			base *= y
+		}
+	}
+
+	// xi + yi == bound
+	ans := make([]int, 0)
+	exist := make(map[int]struct{})
+	for xi := range xm {
+		for yi := range ym {
+			if r := xi + yi; r <= bound {
+				if _, ok := exist[r]; !ok {
+					exist[r] = struct{}{}
+					ans = append(ans, r)
+				}
+			}
+		}
+	}
+	return ans
+}
