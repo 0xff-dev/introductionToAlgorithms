74cfda5857a82268fdce17e5804d355d7b33d9e7
0xff-dev <stevenshuang521@gmail.com>
2025-05-07 09:09:58 +0800

    Minimum Number Game

---
+++ leetcode/2974MinimumNumberGame.go
@@ -0,0 +1,13 @@
+package leetcode
+
+import "sort"
+
+func numberGame(nums []int) []int {
+	sort.Ints(nums)
+	ans := make([]int, len(nums))
+	for i := 1; i < len(nums); i += 2 {
+		ans[i-1] = nums[i]
+		ans[i] = nums[i-1]
+	}
+	return ans
+}
