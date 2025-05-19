3fee79ab9e695fc8ae76891e6aad5da49a307095
0xff-dev <stevenshuang521@gmail.com>
2025-05-19 08:55:39 +0800

    Type of Triangle

---
+++ leetcode/3024TypeOfTriangle.go
@@ -0,0 +1,14 @@
+package leetcode
+
+func triangleType(nums []int) string {
+	if !(nums[0]+nums[1] > nums[2] && nums[0]+nums[2] > nums[1] && nums[1]+nums[2] > nums[0]) {
+		return "none"
+	}
+	if nums[0] == nums[1] && nums[0] == nums[2] {
+		return "equilateral"
+	}
+	if nums[0] == nums[1] || nums[0] == nums[2] || nums[1] == nums[2] {
+		return "isosceles"
+	}
+	return "scalene"
+}
