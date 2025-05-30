c736bc5ed9683abd8811d56ec64811f4164be362
0xff-dev <stevenshuang521@gmail.com>
2025-05-30 09:24:03 +0800

    Find The Value of The Partition

---
+++ leetcode/2740FindTheValueOfThePartition.go
@@ -0,0 +1,15 @@
+package leetcode
+
+import "sort"
+
+func findValueOfPartition(nums []int) int {
+	sort.Ints(nums)
+	ans := -1
+	for i := 1; i < len(nums); i++ {
+		diff := nums[i] - nums[i-1]
+		if ans == -1 || ans > diff {
+			ans = diff
+		}
+	}
+	return ans
+}
