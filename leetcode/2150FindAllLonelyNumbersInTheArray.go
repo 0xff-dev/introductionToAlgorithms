4483348e8a019c4d741eedb1eddbf1a363a3dd20
0xff-dev <stevenshuang521@gmail.com>
2025-06-02 15:54:07 +0800

    Find All Lonely Number in the Array

---
+++ leetcode/2150FindAllLonelyNumbersInTheArray.go
@@ -0,0 +1,20 @@
+package leetcode
+
+func findLonely(nums []int) []int {
+	cnt := make(map[int]int)
+	for _, n := range nums {
+		cnt[n]++
+	}
+	ans := make([]int, 0)
+	for _, n := range nums {
+		if cnt[n] != 1 {
+			continue
+		}
+		_, ok1 := cnt[n-1]
+		_, ok2 := cnt[n+1]
+		if !ok1 && !ok2 {
+			ans = append(ans, n)
+		}
+	}
+	return ans
+}
