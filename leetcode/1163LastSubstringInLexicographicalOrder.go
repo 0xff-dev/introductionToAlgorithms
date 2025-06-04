b946c572c4ec507da700a58f1158f3a91d45f2a3
0xff-dev <stevenshuang521@gmail.com>
2025-06-04 22:33:12 +0800

    Last Substring in Lexicographical Order

---
+++ leetcode/1163LastSubstringInLexicographicalOrder.go
@@ -0,0 +1,17 @@
+package leetcode
+
+func lastSubstring(s string) string {
+	indies := [26][]int{}
+	for i, b := range s {
+		indies[b-'a'] = append(indies[b-'a'], i)
+	}
+	i := 25
+	for ; i >= 0 && len(indies[i]) == 0; i-- {
+	}
+	// bab, b
+	ans := ""
+	for _, index := range indies[i] {
+		ans = max(ans, s[index:])
+	}
+	return ans
+}
