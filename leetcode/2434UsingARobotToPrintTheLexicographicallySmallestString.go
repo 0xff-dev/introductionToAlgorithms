545171837e236fbddda96fa8342e2889ce02bce6
0xff-dev <stevenshuang521@gmail.com>
2025-06-06 09:25:02 +0800

    Using A Robot To Print The Lexicographically Smallest String

---
+++ leetcode/2434UsingARobotToPrintTheLexicographicallySmallestString.go
@@ -0,0 +1,25 @@
+package leetcode
+
+func robotWithString(s string) string {
+	cnt := make([]int, 26)
+	for _, c := range s {
+		cnt[c-'a']++
+	}
+
+	stack := []rune{}
+	res := []rune{}
+	minCharacter := 'a'
+	for _, c := range s {
+		stack = append(stack, c)
+		cnt[c-'a']--
+		for minCharacter != 'z' && cnt[minCharacter-'a'] == 0 {
+			minCharacter++
+		}
+		for len(stack) > 0 && stack[len(stack)-1] <= minCharacter {
+			res = append(res, stack[len(stack)-1])
+			stack = stack[:len(stack)-1]
+		}
+	}
+
+	return string(res)
+}
