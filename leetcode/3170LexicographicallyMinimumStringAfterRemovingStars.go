3f646a6ec45921d12244e55bec39ae5f17c24519
0xff-dev <stevenshuang521@gmail.com>
2025-06-07 20:29:29 +0800

    Lexicographically Minimum String After Removing Stars

---
+++ leetcode/3170LexicographicallyMinimumStringAfterRemovingStars.go
@@ -0,0 +1,30 @@
+package leetcode
+
+func clearStars(s string) string {
+	cnt := make([][]int, 26)
+	for i := range cnt {
+		cnt[i] = make([]int, 0)
+	}
+	arr := []rune(s)
+	for i, c := range arr {
+		if c != '*' {
+			cnt[c-'a'] = append(cnt[c-'a'], i)
+		} else {
+			for j := 0; j < 26; j++ {
+				if len(cnt[j]) > 0 {
+					last := len(cnt[j]) - 1
+					arr[cnt[j][last]] = '*'
+					cnt[j] = cnt[j][:last]
+					break
+				}
+			}
+		}
+	}
+	var ans []rune
+	for _, c := range arr {
+		if c != '*' {
+			ans = append(ans, c)
+		}
+	}
+	return string(ans)
+}
