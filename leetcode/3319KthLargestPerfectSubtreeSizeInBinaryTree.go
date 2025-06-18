9753110078ffd316c73103ffeac029f247004dbd
0xff-dev <stevenshuang521@gmail.com>
2025-06-18 09:10:26 +0800

    K-th Largest Perfect Subtree Size in Binary Tree

---
+++ leetcode/3319KthLargestPerfectSubtreeSizeInBinaryTree.go
@@ -0,0 +1,43 @@
+package leetcode
+
+import "sort"
+
+func kthLargestPerfectSubtree(root *TreeNode, k int) int {
+	var dfs func(*TreeNode) int
+	count := make(map[int]int)
+	dfs = func(cur *TreeNode) int {
+		if cur == nil {
+			return -1
+		}
+		if cur.Left == nil && cur.Right == nil {
+			count[1]++
+			return 1
+		}
+		left := dfs(cur.Left)
+		right := dfs(cur.Right)
+
+		if left != -1 && left == right {
+			count[left+right+1]++
+			return left + right + 1
+		}
+		return -1
+	}
+	dfs(root)
+	keys := make([]int, 0)
+	for i := range count {
+		keys = append(keys, i)
+	}
+
+	sort.Slice(keys, func(i, j int) bool {
+		return keys[i] > keys[j]
+	})
+	var cnt int
+	for _, n := range keys {
+		cnt = count[n]
+		if cnt >= k {
+			return n
+		}
+		k -= cnt
+	}
+	return -1
+}
