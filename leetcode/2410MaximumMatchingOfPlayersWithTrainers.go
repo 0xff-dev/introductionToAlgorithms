7775a626c2cbf64d03fc586d6ab418c7aa7b48da
0xff-dev <stevenshuang521@gmail.com>
2025-06-21 08:51:58 +0800

    Maximum Matching Of Players With Trainers

---
+++ leetcode/2410MaximumMatchingOfPlayersWithTrainers.go
@@ -0,0 +1,19 @@
+package leetcode
+
+import "sort"
+
+func matchPlayersAndTrainers(players []int, trainers []int) int {
+	sort.Ints(players)
+	sort.Ints(trainers)
+	i, j := 0, 0
+	ans := 0
+	for i < len(players) && j < len(trainers) {
+		if trainers[j] >= players[i] {
+			i, j = i+1, j+1
+			ans++
+			continue
+		}
+		j++
+	}
+	return ans
+}
