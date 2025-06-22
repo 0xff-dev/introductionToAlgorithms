e803b0a8d37aa31fc34ec04bbd57f7fec54d3c78
0xff-dev <stevenshuang521@gmail.com>
2025-06-22 16:49:16 +0800

    Divide a String Into Groups Of Size K

---
+++ leetcode/2138DivideAStringIntoGroupsOfSizeK.go
@@ -0,0 +1,17 @@
+package leetcode
+
+func divideString(s string, k int, fill byte) []string {
+	l := len(s)
+	bs := []byte(s)
+	mod := l % k
+	if mod != 0 {
+		for i := 0; i < k-mod; i++ {
+			bs = append(bs, fill)
+		}
+	}
+	var ans []string
+	for i := 0; i < len(bs); i += k {
+		ans = append(ans, string(bs[i:i+k]))
+	}
+	return ans
+}
