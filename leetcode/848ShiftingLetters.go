5e78de7c50f8dd7043eafbd2e8b00945dbf17169
0xff-dev <stevenshuang521@gmail.com>
2025-06-28 17:45:07 +0800

    Shifting Letters

---
+++ leetcode/848ShiftingLetters.go
@@ -0,0 +1,12 @@
+package leetcode
+
+func shiftingLetters848(s string, shifts []int) string {
+	ans := []byte(s)
+	sum := 0
+	for i := len(s) - 1; i >= 0; i-- {
+		sum += shifts[i]
+		now := (int(ans[i]-'a') + sum) % 26
+		ans[i] = byte(now) + 'a'
+	}
+	return string(ans)
+}
