cf259faa9dceee36b511ff4d9b4d3667e5b2cc07
0xff-dev <stevenshuang521@gmail.com>
2025-07-01 08:52:40 +0800

    Find the Original Typed String I

---
+++ leetcode/3330FindTheOriginalTypedStringI.go
@@ -0,0 +1,22 @@
+package leetcode
+
+func possibleStringCount(word string) int {
+	ans := 1
+	c := 0
+	pre := byte(' ')
+	for _, b := range []byte(word) {
+		if b == pre {
+			c++
+			continue
+		}
+		if c > 1 {
+			ans += c - 1
+		}
+		c = 1
+		pre = b
+	}
+	if c > 1 {
+		ans += c - 1
+	}
+	return ans
+}
