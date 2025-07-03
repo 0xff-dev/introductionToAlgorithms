588e29c4a48942ed3e511a10dd556e3624ba2079
0xff-dev <stevenshuang521@gmail.com>
2025-07-03 08:56:59 +0800

    Find the K-th Character in String Game I

---
+++ leetcode/3304FindTheKthCharacterInStringGameI.go
@@ -0,0 +1,13 @@
+package leetcode
+
+func kthCharacter(k int) byte {
+	str := "a"
+	for len(str) < k {
+		tmp := []byte(str)
+		for i := range tmp {
+			tmp[i] = (tmp[i]-'a'+1)%26 + 'a'
+		}
+		str += string(tmp)
+	}
+	return str[k-1]
+}
