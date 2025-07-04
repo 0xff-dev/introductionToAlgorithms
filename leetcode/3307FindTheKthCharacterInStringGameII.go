bce534a6a513ff3c1901129280faed5329a70717
0xff-dev <stevenshuang521@gmail.com>
2025-07-04 08:55:37 +0800

    Find The kth Character in String Game II

---
+++ leetcode/3307FindTheKthCharacterInStringGameII.go
@@ -0,0 +1,18 @@
+package leetcode
+
+import "math/bits"
+
+func kthCharacter3307(k int64, operations []int) byte {
+	ans := 0
+	for k != 1 {
+		t := bits.Len64(uint64(k)) - 1
+		if (1 << t) == k {
+			t--
+		}
+		k -= (1 << t)
+		if operations[t] != 0 {
+			ans++
+		}
+	}
+	return byte('a' + (ans % 26))
+}
