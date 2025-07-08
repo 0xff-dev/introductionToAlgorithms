8b3173b5b37715654aed68738b900894e79bd172
0xff-dev <stevenshuang521@gmail.com>
2025-07-08 09:14:28 +0800

    Incremental Memory Leak

---
+++ leetcode/1860IncrementalMemoryLeak.go
@@ -0,0 +1,14 @@
+package leetcode
+
+func memLeak(memory1 int, memory2 int) []int {
+	ans := []int{1, memory1, memory2}
+	for ans[0] <= ans[1] || ans[0] <= ans[2] {
+		if ans[1] >= ans[2] {
+			ans[1] -= ans[0]
+		} else {
+			ans[2] -= ans[0]
+		}
+		ans[0]++
+	}
+	return ans
+}
