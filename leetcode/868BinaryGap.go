9729bc89ea91fd5dc002818faf0451223fdcb499
0xff-dev <stevenshuang521@gmail.com>
2025-07-11 09:27:03 +0800

    Binary Gap

---
+++ leetcode/868BinaryGap.go
@@ -0,0 +1,21 @@
+package leetcode
+
+func binaryGap(n int) int {
+	ans, count := 0, 0
+	first := true
+	for ; n > 0; n >>= 1 {
+		if n&1 == 0 {
+			if !first {
+				count++
+			}
+			continue
+		}
+		if first {
+			first = false
+		} else {
+			ans = max(ans, count)
+		}
+		count = 1
+	}
+	return ans
+}
