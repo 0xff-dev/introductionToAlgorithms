893151fcb3f6e36fdac8aeaf9bd5c3715904bcc0
0xff-dev <stevenshuang521@gmail.com>
2025-07-16 09:11:00 +0800

    Find the Maximum Length Of Valid Subsequence I

---
+++ leetcode/3201FindTheMaximumLengthOfValidSubsequenceI.go
@@ -0,0 +1,24 @@
+package leetcode
+
+func maximumLength3201(nums []int) int {
+	// 要么一连串偶数(连串奇数)，可以保证%2都是0(1)
+	a, b := 0, 0
+	for _, n := range nums {
+		if n&1 == 0 {
+			a++
+			continue
+		}
+		b++
+	}
+	ans := max(a, b)
+	// 开始找一个奇数一个偶数, 保证了结果一定是奇数
+	evenLen, oddLen := 0, 0
+	for _, n := range nums {
+		if n&1 == 0 {
+			evenLen = oddLen + 1
+			continue
+		}
+		oddLen = evenLen + 1
+	}
+	return max(ans, oddLen, evenLen)
+}
