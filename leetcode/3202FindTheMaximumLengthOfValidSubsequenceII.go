86af47e646496cbea33e029572324167647dd5c9
0xff-dev <stevenshuang521@gmail.com>
2025-07-17 22:14:42 +0800

    Find the Maximum Length of Valid Subsequence II

---
+++ leetcode/3202FindTheMaximumLengthOfValidSubsequenceII.go
@@ -0,0 +1,59 @@
+package leetcode
+
+func longestAlternatingSubsequnece(l1, l2 []int) int {
+	a, b := 0, 0
+	ai, bi := 0, 0
+	for ai < len(l1) && bi < len(l2) {
+		if l1[ai] < l2[bi] {
+			ai++
+			a = b + 1
+			continue
+		}
+		b = a + 1
+		bi++
+	}
+	return max(a, b) + 1
+}
+
+func maximumLength3202(nums []int, k int) int {
+	// 100 n^3
+	// 0, 1, 2, .. k-1
+	// 检查连续和为 其中一个的，
+	group := make([][]int, k)
+	for i := range nums {
+		nums[i] %= k
+		// 元素分组
+		group[nums[i]] = append(group[nums[i]], i)
+	}
+	// 1,0,1,0,1
+	ans, l := 0, len(nums)
+	sums := make(map[int]struct{})
+	for i := 0; i < l; i++ {
+		for j := i + 1; j < l; j++ {
+			sums[nums[i]+nums[j]] = struct{}{}
+		}
+	}
+
+	for sum := range sums {
+		used := map[int]struct{}{}
+		for i := 0; i < l; i++ {
+			if _, ok := used[nums[i]]; ok {
+				continue
+			}
+			used[nums[i]] = struct{}{}
+			target := (k + sum - nums[i]) % k
+			used[target] = struct{}{}
+
+			ll := len(group[target])
+			if ll == 0 {
+				continue
+			}
+			if nums[i] == target {
+				ans = max(ans, ll)
+				continue
+			}
+			ans = max(ans, longestAlternatingSubsequnece(group[nums[i]], group[target]))
+		}
+	}
+	return ans
+}
