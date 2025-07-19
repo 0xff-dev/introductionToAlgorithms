9d28d0fb05ce4d7830fb40b493545bf738ef46f0
0xff-dev <stevenshuang521@gmail.com>
2025-07-19 09:17:25 +0800

    Number Of subarrays That Match a Pattern I

---
+++ leetcode/3034NumberOfSubarraysThatMatchaPatternI.go
@@ -0,0 +1,27 @@
+package leetcode
+
+func countMatchingSubarrays(nums []int, pattern []int) int {
+	var ans int
+	m := len(pattern)
+	n := len(nums)
+	k := 0
+	for i := 0; i < n-m; i++ {
+		k = 0
+		for ; k < m; k++ {
+			if pattern[k] == 1 && nums[i+k+1] > nums[i+k] {
+				continue
+			}
+			if pattern[k] == 0 && nums[i+k+1] == nums[i+k] {
+				continue
+			}
+			if pattern[k] == -1 && nums[i+k+1] < nums[i+k] {
+				continue
+			}
+			break
+		}
+		if k == m {
+			ans++
+		}
+	}
+	return ans
+}
