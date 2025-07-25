019fedecf7931494a58e417b69280f869e1bd82c
0xff-dev <stevenshuang521@gmail.com>
2025-07-25 08:52:33 +0800

    Maximum Unique Subarray Sum After Deletion

---
+++ leetcode/3487MaximumUniqueSubarraySumAfterDeletion.go
@@ -0,0 +1,24 @@
+package leetcode
+
+import "sort"
+
+func maxSum3487(nums []int) int {
+	l := len(nums)
+	sort.Ints(nums)
+	index := sort.Search(l, func(i int) bool {
+		return nums[i] >= 0
+	})
+	sum := nums[l-1]
+	if index == l {
+		return sum
+	}
+	pre := sum
+	for i := l - 2; i >= index; i-- {
+		if nums[i] == pre {
+			continue
+		}
+		pre = nums[i]
+		sum += nums[i]
+	}
+	return sum
+}
