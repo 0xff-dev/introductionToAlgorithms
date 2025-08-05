330e4dfb6833722212ad52d9d2cf4ab397f53273
0xff-dev <stevenshuang521@gmail.com>
2025-08-05 08:44:00 +0800

    Fruits Into Baskets

---
+++ leetcode/3477FruitsIntoBasketsII.go
@@ -0,0 +1,15 @@
+package leetcode
+
+func numOfUnplacedFruits(fruits []int, baskets []int) int {
+	left := len(fruits)
+	for _, f := range fruits {
+		for i := range baskets {
+			if baskets[i] >= f {
+				left--
+				baskets[i] = -1
+				break
+			}
+		}
+	}
+	return left
+}
