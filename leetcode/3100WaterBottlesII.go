0b17cad904fbef922eaf2ebcde345c73a14f68f4
0xff-dev <stevenshuang521@gmail.com>
2025-08-08 09:04:18 +0800

    Water Bottles II

---
+++ leetcode/3100WaterBottlesII.go
@@ -0,0 +1,22 @@
+package leetcode
+
+func maxBottlesDrunk(numBottles int, numExchange int) int {
+	var ans int = numBottles
+	drink := 0
+	empty := numBottles
+	for {
+		if empty >= numExchange {
+			drink++
+			empty -= numExchange
+			numExchange++
+			continue
+		}
+		empty += drink
+		ans += drink
+		drink = 0
+		if empty < numExchange {
+			break
+		}
+	}
+	return ans + drink
+}
