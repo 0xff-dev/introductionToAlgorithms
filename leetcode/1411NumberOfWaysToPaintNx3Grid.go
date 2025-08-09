4513b0ddfc39282b7eaad325251450b3cd0dd2b6
0xff-dev <stevenshuang521@gmail.com>
2025-08-09 18:25:08 +0800

    Number Of Ways to Paint N x 3 Grid

---
+++ leetcode/1411NumberOfWaysToPaintNx3Grid.go
@@ -0,0 +1,44 @@
+package leetcode
+
+const mod1411 = 1000000007
+
+func numOfWays(n int) int {
+	rel := [12][]int{
+		{1, 4, 10, 2, 5},
+		{0, 3, 6, 8, 11},
+		{0, 3, 6, 7},
+
+		{1, 7, 10, 2},
+		{0, 6, 9, 8},
+		{0, 6, 9, 7, 10},
+
+		{1, 4, 2, 5, 11},
+		{3, 2, 5, 11},
+		{9, 1, 4, 10},
+
+		{4, 5, 8, 11},
+		{0, 3, 5, 8, 11},
+		{1, 7, 10, 6, 9},
+	}
+	var dfs func(int, int) int
+	cache := make(map[[2]int]int)
+	dfs = func(parent, left int) int {
+		if left == 0 {
+			return 1
+		}
+		ans := 0
+		l := len(rel[parent])
+		key := [2]int{l, left}
+		if v, ok := cache[key]; ok {
+			return v
+		}
+		for _, next := range rel[parent] {
+			ans = (ans + dfs(next, left-1)) % mod1411
+		}
+		cache[key] = ans
+		return ans
+	}
+	a := (dfs(0, n-1) * 6) % mod1411
+	b := (dfs(2, n-1) * 6) % mod1411
+	return (a + b) % mod1411
+}
