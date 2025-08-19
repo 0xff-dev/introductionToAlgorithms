effa3b9d7132f17f7c305172a102fdd9e3d70c92
0xff-dev <stevenshuang521@gmail.com>
2025-08-19 08:54:41 +0800

    Number of Smooth Descent Periods of a Stock

---
+++ leetcode/2110NumberOfSmoothDescentPeriodsOfAStock.go
@@ -0,0 +1,16 @@
+package leetcode
+
+func getDescentPeriods(prices []int) int64 {
+	l := len(prices)
+	c := int64(1)
+	ans := c
+	for i := 1; i < l; i++ {
+		if prices[i] == prices[i-1]-1 {
+			c++
+		} else {
+			c = 1
+		}
+		ans += c
+	}
+	return ans
+}
