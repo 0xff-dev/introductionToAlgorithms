670e3ffe5550fc5fae5deb758d31330941ad4558
0xff-dev <stevenshuang521@gmail.com>
2025-08-20 08:45:00 +0800

    Length of the Longest Alphabetical Continuous Substring

---
+++ leetcode/2414LengthOfTheLongestAlphabeticalContinuousSubstring.go
@@ -0,0 +1,15 @@
+package leetcode
+
+func longestContinuousSubstring(s string) int {
+	ans := 1
+	cur := 1
+	for i := 1; i < len(s); i++ {
+		if s[i] == s[i-1]+1 {
+			cur++
+		} else {
+			cur = 1
+		}
+		ans = max(ans, cur)
+	}
+	return ans
+}
