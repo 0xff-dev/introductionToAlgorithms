71067c8a0fdcecf3fe10c191855a7fd3b9fc49fd
0xff-dev <stevenshuang521@gmail.com>
2025-08-24 14:53:06 +0800

    Find XOr Sum of All Pairs Bitwise AND

---
+++ leetcode/1835FindXORSumOfAllPairsBitwiseAnd.go
@@ -0,0 +1,13 @@
+package leetcode
+
+func getXORSum(arr1 []int, arr2 []int) int {
+	xor2 := 0
+	for _, n := range arr2 {
+		xor2 ^= n
+	}
+	ans := 0
+	for _, n := range arr1 {
+		ans ^= (n & xor2)
+	}
+	return ans
+}
