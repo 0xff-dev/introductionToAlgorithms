a8f794d8403646147e7a80dce9566268da045bfb
0xff-dev <stevenshuang521@gmail.com>
2025-08-26 08:51:51 +0800

    Maximum Area of Longest Diagonal Rectangle

---
+++ leetcode/3000MaximumAreaOfLongestDiagonalRectangle.go
@@ -0,0 +1,17 @@
+package leetcode
+
+func areaOfMaxDiagonal(dimensions [][]int) int {
+	diagonal, area := 0, 0
+	for _, n := range dimensions {
+		tmp := n[0]*n[0] + n[1]*n[1]
+		if tmp == diagonal {
+			area = max(area, n[0]*n[1])
+		}
+		if tmp > diagonal {
+			diagonal = tmp
+			area = n[0] * n[1]
+		}
+	}
+
+	return area
+}
