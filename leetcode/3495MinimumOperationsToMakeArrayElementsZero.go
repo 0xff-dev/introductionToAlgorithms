bb727aa749c1229bc9b664ddfaba641d50740b59
0xff-dev <stevenshuang521@gmail.com>
2025-09-06 20:44:44 +0800

    Minimum Operations to Make Array Elements Zero

---
+++ leetcode/3495MinimumOperationsToMakeArrayElementsZero.go
@@ -0,0 +1,28 @@
+package leetcode
+
+func get(num int) int64 {
+	var cnt int64
+	i := 1
+	base := 1
+
+	for base <= num {
+		end := base*2 - 1
+		if end > num {
+			end = num
+		}
+		cnt += int64((i+1)/2) * int64(end-base+1)
+		i++
+		base *= 2
+	}
+	return cnt
+}
+
+func minOperations3495(queries [][]int) int64 {
+	var res int64
+	for _, q := range queries {
+		count1 := get(q[1])
+		count2 := get(q[0] - 1)
+		res += (count1 - count2 + 1) / 2
+	}
+	return res
+}
