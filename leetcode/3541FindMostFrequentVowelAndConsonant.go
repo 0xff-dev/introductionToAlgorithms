9c7e1cecff35072b5b1fc4d568ae6169259dfbea
0xff-dev <stevenshuang521@gmail.com>
2025-09-13 08:55:21 +0800

    Find Most Frequent Vowel And Consonant

---
+++ leetcode/3541FindMostFrequentVowelAndConsonant.go
@@ -0,0 +1,17 @@
+package leetcode
+
+func maxFreqSum(s string) int {
+	count := [26]int{}
+	for _, b := range []byte(s) {
+		count[b-'a']++
+	}
+	vowel, consonant := 0, 0
+	for i := 'a'; i <= 'z'; i++ {
+		if i == 'a' || i == 'e' || i == 'i' || i == 'o' || i == 'u' {
+			vowel = max(vowel, count[i-'a'])
+			continue
+		}
+		consonant = max(consonant, count[i-'a'])
+	}
+	return vowel + consonant
+}
