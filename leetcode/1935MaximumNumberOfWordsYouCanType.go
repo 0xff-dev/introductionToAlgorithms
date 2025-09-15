28f55a5c7b36ac8b95c1e725a3143b057370f76a
0xff-dev <stevenshuang521@gmail.com>
2025-09-15 08:48:56 +0800

    Maximum Number Of Words You Can Type

---
+++ leetcode/1935MaximumNumberOfWordsYouCanType.go
@@ -0,0 +1,22 @@
+package leetcode
+
+import "strings"
+
+func canBeTypedWords(text string, brokenLetters string) int {
+	broken := [26]bool{}
+	for _, b := range brokenLetters {
+		broken[b-'a'] = true
+	}
+	ret, i := 0, 0
+	for _, word := range strings.Split(text, " ") {
+		for i = 0; i < len(word); i++ {
+			if broken[word[i]-'a'] {
+				break
+			}
+		}
+		if i == len(word) {
+			ret++
+		}
+	}
+	return ret
+}
