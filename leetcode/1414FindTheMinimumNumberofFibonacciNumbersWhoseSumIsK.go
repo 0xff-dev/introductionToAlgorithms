3a9785a6784efb57e28933a635f3c6e44cf81c81
0xff-dev <stevenshuang521@gmail.com>
2025-09-17 09:04:25 +0800

    Find the Minimum Number of fibonacci Numbers Whose Sum is K

---
+++ leetcode/1414FindTheMinimumNumberofFibonacciNumbersWhoseSumIsK.go
@@ -0,0 +1,19 @@
+package leetcode
+
+func findMinFibonacciNumbers(k int) int {
+	// 最多44个
+	fb := []int{1, 1}
+	for i := 2; i < 44 && fb[i-1]+fb[i-2] <= k; i++ {
+		fb = append(fb, fb[i-1]+fb[i-2])
+	}
+	ret := 0
+	// 5
+	for index := len(fb) - 1; index >= 0 && k > 0; index-- {
+		if k < fb[index] {
+			continue
+		}
+		ret += k / fb[index]
+		k %= fb[index]
+	}
+	return ret
+}
