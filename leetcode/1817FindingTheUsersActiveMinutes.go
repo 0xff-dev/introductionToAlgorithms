348963be6c5a37f878448c939c57f56d910e4bc9
0xff-dev <stevenshuang521@gmail.com>
2025-09-23 09:10:55 +0800

    Finding the User Active Minutes

---
+++ leetcode/1817FindingTheUsersActiveMinutes.go
@@ -0,0 +1,16 @@
+package leetcode
+
+func findingUsersActiveMinutes(logs [][]int, k int) []int {
+	ret := make([]int, k)
+	count := make(map[int]map[int]struct{})
+	for _, log := range logs {
+		if _, ok := count[log[0]]; !ok {
+			count[log[0]] = make(map[int]struct{})
+		}
+		count[log[0]][log[1]] = struct{}{}
+	}
+	for _, c := range count {
+		ret[len(c)-1]++
+	}
+	return ret
+}
