8b1bac540c511adc517d09083b5b45f3d7ec181c
0xff-dev <stevenshuang521@gmail.com>
2025-09-27 09:05:33 +0800

    Largest Triangle Area

---
+++ leetcode/812LargestTriangleArea.go
@@ -0,0 +1,34 @@
+package leetcode
+
+import "math"
+
+const epsilon = 1e-9
+
+func canForm(a, b, c []int) (float64, bool) {
+	abx := b[0] - a[0]
+	aby := b[1] - a[1]
+	acx := c[0] - a[0]
+	acy := c[1] - a[1]
+	area := math.Abs(float64(abx*acy - aby*acx))
+	if area > epsilon {
+		return area, true
+	}
+	return 0.0, false
+}
+
+func largestTriangleArea(points [][]int) float64 {
+	var ret float64
+	l := len(points)
+	// 1, 2, 3
+	for i := 0; i < l-2; i++ {
+		for j := i + 1; j < l-1; j++ {
+			for k := j + 1; k < l; k++ {
+				area, ok := canForm(points[i], points[j], points[k])
+				if ok {
+					ret = max(ret, area/2.0)
+				}
+			}
+		}
+	}
+	return ret
+}
