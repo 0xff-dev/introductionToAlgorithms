9c947f5bb66a0052a3f32e46f85a88b05921efbb
0xff-dev <stevenshuang521@gmail.com>
2025-09-28 16:51:49 +0800

    Largest Perimeter Triangle

---
+++ leetcode/976LargestPerimeterTriangle.go
@@ -0,0 +1,16 @@
+package leetcode
+
+import "sort"
+
+func largestPerimeter976(nums []int) int {
+	sort.Ints(nums)
+	// 1, 1, 2, 10
+	l := len(nums)
+	for i := l - 1; i >= 2; i-- {
+		a, b, c := nums[i], nums[i-1], nums[i-2]
+		if a+b > c && a+c > b && b+c > a {
+			return a + b + c
+		}
+	}
+	return 0
+}
