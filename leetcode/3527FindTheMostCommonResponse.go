3ec2662e27f0e27dafef4c4c83b33b4fd8dd9fbf
0xff-dev <stevenshuang521@gmail.com>
2025-10-02 14:05:05 +0800

    Find The Most Common Response

---
+++ leetcode/3527FindTheMostCommonResponse.go
@@ -0,0 +1,31 @@
+package leetcode
+
+func findCommonResponse(responses [][]string) string {
+	store := make(map[string]int)
+	for _, resp := range responses {
+		cur := make(map[string]struct{})
+		for _, str := range resp {
+			cur[str] = struct{}{}
+		}
+		for key := range cur {
+			store[key]++
+		}
+	}
+	var (
+		ret string
+		cnt int
+	)
+	for key, count := range store {
+		if count > cnt {
+			ret = key
+			cnt = count
+			continue
+		}
+
+		if count == cnt {
+			ret = min(ret, key)
+		}
+	}
+
+	return ret
+}
