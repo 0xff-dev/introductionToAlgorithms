9119292aebc395acad92547fc928cc5c40b9f4d8
0xff-dev <stevenshuang521@gmail.com>
2025-10-10 09:07:25 +0800

    Taking Maximum Energy From the Mystic Dungeon

---
+++ leetcode/3147TakingMaximumEnergyFromTheMysticDungeon.go
@@ -0,0 +1,19 @@
+package leetcode
+
+func maximumEnergy(energy []int, k int) int {
+	dp := make([]int, len(energy))
+	// k = 2
+	// 1, 2, 3, 4,
+	l := len(energy)
+	ret := energy[l-1]
+	for i := l - 1; i >= l-k; i-- {
+		// 这些是无法跳的，所以默认就有自己的值
+		dp[i] = energy[i]
+		ret = max(ret, dp[i])
+	}
+	for i := l - k - 1; i >= 0; i-- {
+		dp[i] = dp[i+k] + energy[i]
+		ret = max(ret, dp[i])
+	}
+	return ret
+}
