687baf41bfa7741ec5ad5d040f73f1ce485fea6c
0xff-dev <stevenshuang521@gmail.com>
2025-10-13 08:59:05 +0800

    Find Resultant Array After Removing Anagrams

---
+++ leetcode/2273FindResultantArrayAfterRemovingAnagrams.go
@@ -0,0 +1,34 @@
+package leetcode
+
+func equal2273(a, b [26]int) bool {
+	for i := range 26 {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func removeAnagrams(words []string) []string {
+	l := len(words)
+	indies := make([][26]int, l)
+	for index, word := range words {
+		for _, b := range word {
+			indies[index][b-'a']++
+		}
+	}
+	index := 0
+	stack := make([]int, l)
+	for i := 1; i < l; i++ {
+		if equal2273(indies[stack[index]], indies[i]) {
+			continue
+		}
+		index++
+		stack[index] = i
+	}
+	var ret []string
+	for i := range index + 1 {
+		ret = append(ret, words[stack[i]])
+	}
+	return ret
+}
