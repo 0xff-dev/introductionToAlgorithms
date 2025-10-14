46840abcf9dbfd92a59d16235558593e7d5951a2
0xff-dev <stevenshuang521@gmail.com>
2025-10-14 09:12:55 +0800

    Adjacent Increasing Subarrays Detection I

---
+++ leetcode/3349AdjacentIncreasingSubarraysDetectionI.go
@@ -0,0 +1,27 @@
+package leetcode
+
+func hasIncreasingSubarrays(nums []int, k int) bool {
+	indies := map[int]struct{}{}
+	start, end := 0, 0
+	curLen := 0
+	pre := -1001
+	for ; end < len(nums); end++ {
+		if nums[end] <= pre {
+			start, curLen = end, 1
+		} else {
+			curLen++
+		}
+		pre = nums[end]
+		if curLen == k {
+			indies[start] = struct{}{}
+			start++
+			curLen--
+		}
+	}
+	for index := range indies {
+		if _, ok := indies[index+k]; ok {
+			return true
+		}
+	}
+	return false
+}
