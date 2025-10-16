999f600fdc0338844010ad4861516f2f6e979806
0xff-dev <stevenshuang521@gmail.com>
2025-10-16 09:10:56 +0800

    Smallest Missing Non-Negative Integer After Operations

---
+++ leetcode/2598SmallestMissingNonNegativeIntegerAfterOperations.go
@@ -0,0 +1,15 @@
+package leetcode
+
+func findSmallestInteger(nums []int, value int) int {
+	mp := make([]int, value)
+	for _, x := range nums {
+		v := ((x % value) + value) % value
+		mp[v]++
+	}
+	mex := 0
+	for mp[mex%value] > 0 {
+		mp[mex%value]--
+		mex++
+	}
+	return mex
+}
