b4a5017e3f3943e98a6af46f2fb614db6942af14
0xff-dev <stevenshuang521@gmail.com>
2025-10-18 09:06:55 +0800

    Maximum Number Of Distinct Elements After Operations

---
+++ leetcode/3397MaximumNumberOfDistinctElementsAfterOperations.go
@@ -0,0 +1,21 @@
+package leetcode
+
+import (
+	"math"
+	"sort"
+)
+
+func maxDistinctElements(nums []int, k int) int {
+	sort.Ints(nums)
+	cnt := 0
+	prev := math.MinInt32
+
+	for _, num := range nums {
+		curr := min(max(num-k, prev+1), num+k)
+		if curr > prev {
+			cnt++
+			prev = curr
+		}
+	}
+	return cnt
+}
