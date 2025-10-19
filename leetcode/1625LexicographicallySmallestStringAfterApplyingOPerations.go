c3879e9d37e4af7543fed63ddc1350e87d940f42
0xff-dev <stevenshuang521@gmail.com>
2025-10-19 19:25:32 +0800

    Lexicographically Smallest String After Apply Operations

---
+++ leetcode/1625LexicographicallySmallestStringAfterApplyingOPerations.go
@@ -0,0 +1,33 @@
+package leetcode
+
+func findLexSmallestString(s string, a int, b int) string {
+	n := len(s)
+	vis := make([]bool, n)
+	res := s
+	// double the length of s for convenience in extracting the rotated string t
+	s = s + s
+	for i := 0; !vis[i]; i = (i + b) % n {
+		vis[i] = true
+		for j := 0; j < 10; j++ {
+			kLimit := 0
+			if b%2 != 0 {
+				kLimit = 9
+			}
+			for k := 0; k <= kLimit; k++ {
+				// before each accumulation, re-truncate t
+				t := []byte(s[i : i+n])
+				for p := 1; p < n; p += 2 {
+					t[p] = '0' + byte((int(t[p]-'0')+j*a)%10)
+				}
+				for p := 0; p < n; p += 2 {
+					t[p] = '0' + byte((int(t[p]-'0')+k*a)%10)
+				}
+				tStr := string(t)
+				if tStr < res {
+					res = tStr
+				}
+			}
+		}
+	}
+	return res
+}
