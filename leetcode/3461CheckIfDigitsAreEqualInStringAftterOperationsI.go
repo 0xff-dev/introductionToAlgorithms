1d79982d9949241bacc08df94988d5cffc706c28
0xff-dev <stevenshuang521@gmail.com>
2025-10-23 09:39:41 +0800

    Check If Digits Are Equal in String After Operations I

---
+++ leetcode/3461CheckIfDigitsAreEqualInStringAftterOperationsI.go
@@ -0,0 +1,12 @@
+package leetcode
+
+func hasSameDigits(s string) bool {
+	count := len(s) - 2
+	bs := []byte(s)
+	for i := count; i > 0; i-- {
+		for j := 0; j <= i; j++ {
+			bs[j] = byte((int(bs[j]-'0')+int(bs[j+1]-'0'))%10) + '0'
+		}
+	}
+	return bs[0] == bs[1]
+}
