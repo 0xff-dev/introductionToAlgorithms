23de73fbba20ab7acf66fdd7e45a446b64da8a74
0xff-dev <stevenshuang521@gmail.com>
2025-10-26 20:16:04 +0800

    Number Of Ways To Buy Pen and Pencils

---
+++ leetcode/2240NumberOfWaysToBuyPensAndPencils.go
@@ -0,0 +1,11 @@
+package leetcode
+
+func waysToBuyPensPencils(total int, cost1 int, cost2 int) int64 {
+	var ret int64
+	penCount := 0
+	for ; penCount*cost1 <= total; penCount++ {
+		left := (total-penCount*cost1)/cost2 + 1 // 0
+		ret += int64(left)
+	}
+	return ret
+}
