fdc249d016a9891d917aaacd28464910e9fa95bd
0xff-dev <stevenshuang521@gmail.com>
2025-10-30 22:47:14 +0800

    Minimum Number Of Increments on Subarray to Form a Target Array

---
+++ leetcode/1526MinimumNumberOfIncrementsOnSubarraystoFormAtargetArray.go
@@ -0,0 +1,10 @@
+package leetcode
+
+func minNumberOperations(target []int) int {
+	n := len(target)
+	ans := target[0]
+	for i := 1; i < n; i++ {
+		ans += max(target[i]-target[i-1], 0)
+	}
+	return ans
+}
