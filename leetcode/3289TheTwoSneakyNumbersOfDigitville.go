dd20b132136894dd4c4c90ff9b57f8ee624db9b0
0xff-dev <stevenshuang521@gmail.com>
2025-10-31 09:41:57 +0800

    The Two Sneaky Numbers of Digitville

---
+++ leetcode/3289TheTwoSneakyNumbersOfDigitville.go
@@ -0,0 +1,16 @@
+package leetcode
+
+func getSneakyNumbers(nums []int) []int {
+	ret := make([]int, 2)
+	index := 0
+	exists := make([]bool, len(nums)-2)
+	for _, n := range nums {
+		if exists[n] {
+			ret[index] = n
+			index++
+			continue
+		}
+		exists[n] = true
+	}
+	return ret
+}
