dc93f94c48397595402518188114d1e8e6f2dc26
0xff-dev <stevenshuang521@gmail.com>
2025-11-08 10:05:14 +0800

    Hash Divided String

---
+++ leetcode/3271HashDividedString.go
@@ -0,0 +1,18 @@
+package leetcode
+
+import "bytes"
+
+func stringHash(s string, k int) string {
+	buf := bytes.NewBuffer([]byte{})
+	sum := 0
+	for i := 0; i < len(s); i++ {
+		if i != 0 && i%k == 0 {
+			buf.WriteByte(byte(sum%26 + 'a'))
+			sum = int(s[i] - 'a')
+			continue
+		}
+		sum += int(s[i] - 'a')
+	}
+	buf.WriteByte(byte(sum%26 + 'a'))
+	return buf.String()
+}
