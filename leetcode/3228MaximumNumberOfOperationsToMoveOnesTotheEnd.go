d8c3d61f8af3d140bab5a95a0abedb504d10f628
0xff-dev <stevenshuang521@gmail.com>
2025-11-13 22:10:27 +0800

    Maximum Number of Operations to Move Ones to the End

---
+++ leetcode/3228MaximumNumberOfOperationsToMoveOnesTotheEnd.go
@@ -0,0 +1,19 @@
+package leetcode
+
+func maxOperations3228(s string) int {
+	countOne := 0
+	ans := 0
+	i := 0
+	for i < len(s) {
+		if s[i] == '0' {
+			for i+1 < len(s) && s[i+1] == '0' {
+				i++
+			}
+			ans += countOne
+		} else {
+			countOne++
+		}
+		i++
+	}
+	return ans
+}
