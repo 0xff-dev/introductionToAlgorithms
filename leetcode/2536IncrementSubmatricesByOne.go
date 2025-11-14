11ff8baf60aac3119eef3cd3198f2eaa50f60533
0xff-dev <stevenshuang521@gmail.com>
2025-11-14 21:44:54 +0800

    Increment Submatrices by One

---
+++ leetcode/2536IncrementSubmatricesByOne.go
@@ -0,0 +1,16 @@
+package leetcode
+
+func rangeAddQueries(n int, queries [][]int) [][]int {
+	mat := make([][]int, n)
+	for i := 0; i < n; i++ {
+		mat[i] = make([]int, n)
+	}
+	for _, q := range queries {
+		for i := q[0]; i <= q[2]; i++ {
+			for j := q[1]; j <= q[3]; j++ {
+				mat[i][j]++
+			}
+		}
+	}
+	return mat
+}
