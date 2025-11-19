b331b836707d64d717df883f4a3d88fd99e9c958
0xff-dev <stevenshuang521@gmail.com>
2025-11-19 23:05:10 +0800

    Keep Mulitplying Found Values by Two

---
+++ leetcode/2154KeepMultiplyingFoundValuesByTwo.go
@@ -0,0 +1,21 @@
+package leetcode
+
+func findFinalValue(nums []int, original int) int {
+	var (
+		ret int  = original
+		ok  bool = false
+		end int  = 0
+	)
+	m := make(map[int]struct{})
+	for _, n := range nums {
+		end = max(end, n)
+		m[n] = struct{}{}
+	}
+	for ; ret <= end; ret *= 2 {
+		_, ok = m[ret]
+		if !ok {
+			break
+		}
+	}
+	return ret
+}
