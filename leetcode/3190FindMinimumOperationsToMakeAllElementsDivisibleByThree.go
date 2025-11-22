e4b5b7a59a7ffb304541ff2e314fc309ce5483e3
0xff-dev <stevenshuang521@gmail.com>
2025-11-22 22:20:43 +0800

    Find Minimum Operations to Make All Elements Divisible by Three

---
+++ leetcode/3190FindMinimumOperationsToMakeAllElementsDivisibleByThree.go
@@ -0,0 +1,18 @@
+package leetcode
+
+import "sort"
+
+func minimumOperations3190(nums []int) int {
+	byThree := []int{0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51}
+	l := len(byThree)
+	var ret int
+	for _, n := range nums {
+
+		index := sort.Search(l, func(i int) bool {
+			return byThree[i] >= n
+		})
+
+		ret += min(byThree[index]-n, n-byThree[index-1])
+	}
+	return ret
+}
