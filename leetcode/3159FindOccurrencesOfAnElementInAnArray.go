5f5b63a901d42125199bd41dd6ad330ad960aee3
0xff-dev <stevenshuang521@gmail.com>
2025-11-24 22:25:13 +0800

    Find Occurrences of an Element in an Array

---
+++ leetcode/3159FindOccurrencesOfAnElementInAnArray.go
@@ -0,0 +1,18 @@
+package leetcode
+
+func occurrencesOfElement(nums []int, queries []int, x int) []int {
+	indies := []int{}
+	for idx := 0; idx < len(nums); idx++ {
+		if nums[idx] == x {
+			indies = append(indies, idx)
+		}
+	}
+	ret := make([]int, len(queries))
+	for idx, q := range queries {
+		ret[idx] = -1
+		if q <= len(indies) {
+			ret[idx] = indies[q-1]
+		}
+	}
+	return ret
+}
