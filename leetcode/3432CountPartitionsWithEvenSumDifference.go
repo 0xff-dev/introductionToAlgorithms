3ec748756a9e06b9de1141f9e4b6ace10e677cb2
0xff-dev <stevenshuang521@gmail.com>
2025-12-05 23:03:20 +0800

    Count Partitions With Even Sum Difference

---
+++ leetcode/3432CountPartitionsWithEvenSumDifference.go
@@ -0,0 +1,16 @@
+package leetcode
+
+func countPartitions(nums []int) int {
+	var ret, diff int
+	l := len(nums)
+	for i := 1; i < l; i++ {
+		nums[i] += nums[i-1]
+	}
+	// 1, 2, 3, 4
+	for i := 0; i < l-1; i++ {
+		if diff = nums[l-1] - nums[i] - nums[i]; diff&1 == 0 {
+			ret++
+		}
+	}
+	return ret
+}
