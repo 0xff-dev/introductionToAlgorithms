6c48de6b242d1ee14c0bdb17f06a08d0308641b4
0xff-dev <stevenshuang521@gmail.com>
2025-12-08 22:21:51 +0800

    Count Square Sum Triples

---
+++ leetcode/1925CountSquareSumTriples.go
@@ -0,0 +1,18 @@
+package leetcode
+
+func countTriples(n int) int {
+	cache := make(map[int]struct{})
+	for i := 1; i <= n; i++ {
+		cache[i*i] = struct{}{}
+	}
+
+	var ret int
+	for i := 1; i <= n-1; i++ {
+		for j := i + 1; j <= n; j++ {
+			if _, ok := cache[i*i+j*j]; ok {
+				ret += 2
+			}
+		}
+	}
+	return ret
+}
