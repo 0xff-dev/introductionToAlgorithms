ccebcb234a2584090fb2115d7909a4ccc1f91992
0xff-dev <stevenshuang521@gmail.com>
2025-12-09 22:37:38 +0800

    Count Special Triplets

---
+++ leetcode/3583CountSpecialTriplets.go
@@ -0,0 +1,23 @@
+package leetcode
+
+func specialTriplets(nums []int) int {
+	const MOD = 1000000007
+	numCnt := make(map[int]int)
+	numPartialCnt := make(map[int]int)
+
+	for _, v := range nums {
+		numCnt[v]++
+	}
+
+	ans := 0
+	for _, v := range nums {
+		target := v * 2
+		lCnt := numPartialCnt[target]
+		numPartialCnt[v]++
+		rCnt := numCnt[target] - numPartialCnt[target]
+
+		ans = (ans + lCnt*rCnt) % MOD
+	}
+
+	return ans
+}
