d7a96c9e47e6eada59555f2201963b0e38f2320d
0xff-dev <stevenshuang521@gmail.com>
2025-12-10 23:04:07 +0800

    Count the Number of Computer Unlocking Permutations

---
+++ leetcode/3577CountTheNumberOfComputerUnlockingpermutations.go
@@ -0,0 +1,17 @@
+package leetcode
+
+func countPermutations(complexity []int) int {
+	n := len(complexity)
+	for i := 1; i < n; i++ {
+		if complexity[i] <= complexity[0] {
+			return 0
+		}
+	}
+
+	ans := 1
+	mod := 1000000007
+	for i := 2; i < n; i++ {
+		ans = ans * i % mod
+	}
+	return ans
+}
