3c38fe5ee6023b58597a10cbbe0ea6ac35f11c4d
0xff-dev <stevenshuang521@gmail.com>
2025-12-18 22:52:32 +0800

    Best Time To Buy And Sell Stock Using Strategy

---
+++ leetcode/3652BestTimeToBuyAndSellStockUsingStrategy.go
@@ -0,0 +1,19 @@
+package leetcode
+
+func maxProfit3652(prices []int, strategy []int, k int) int64 {
+	n := len(prices)
+	profitSum := make([]int64, n+1)
+	priceSum := make([]int64, n+1)
+	for i := 0; i < n; i++ {
+		profitSum[i+1] = profitSum[i] + int64(prices[i])*int64(strategy[i])
+		priceSum[i+1] = priceSum[i] + int64(prices[i])
+	}
+	res := profitSum[n]
+	for i := k - 1; i < n; i++ {
+		leftProfit := profitSum[i-k+1]
+		rightProfit := profitSum[n] - profitSum[i+1]
+		changeProfit := priceSum[i+1] - priceSum[i-k/2+1]
+		res = max(res, leftProfit+changeProfit+rightProfit)
+	}
+	return res
+}
