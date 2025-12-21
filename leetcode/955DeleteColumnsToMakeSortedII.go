f6f64b6c7c00efa869c0c38f6a2f4bf730bcec6f
0xff-dev <stevenshuang521@gmail.com>
2025-12-21 19:15:28 +0800

    Delete Columns to Make Sorted II

---
+++ leetcode/955DeleteColumnsToMakeSortedII.go
@@ -0,0 +1,30 @@
+package leetcode
+
+func minDeletionSize955(strs []string) int {
+	var ret, row int
+	rows := len(strs)
+	cols := len(strs[0])
+
+	sorted := make([]bool, rows-1)
+	/*
+		bbjwefkpb
+		axmksfchw
+	*/
+	for col := 0; col < cols; col++ {
+		for row = 0; row < rows-1; row++ {
+			if !sorted[row] && strs[row][col] > strs[row+1][col] {
+				ret++
+				break
+			}
+		}
+		if row == rows-1 {
+			for row = 0; row < rows-1; row++ {
+				if !sorted[row] && strs[row][col] < strs[row+1][col] {
+					sorted[row] = true
+				}
+			}
+		}
+
+	}
+	return ret
+}
