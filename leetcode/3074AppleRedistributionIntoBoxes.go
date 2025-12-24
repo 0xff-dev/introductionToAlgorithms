8f1ff90e1334339c9abfbfcd52300bdce8d1c6be
0xff-dev <stevenshuang521@gmail.com>
2025-12-24 22:35:49 +0800

    Apple Redistribution Into Boxes

---
+++ leetcode/3074AppleRedistributionIntoBoxes.go
@@ -0,0 +1,17 @@
+package leetcode
+
+import "sort"
+
+func minimumBoxes(apple []int, capacity []int) int {
+	sum := 0
+	for _, n := range apple {
+		sum += n
+	}
+	var ret int
+	sort.Ints(capacity)
+	for i := len(capacity) - 1; i >= 0 && sum > 0; i-- {
+		sum -= capacity[i]
+		ret++
+	}
+	return ret
+}
