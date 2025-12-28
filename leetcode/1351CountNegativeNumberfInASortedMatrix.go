7e63f78395f2ad05d880b7c09b3ae8c31566ea8a
0xff-dev <stevenshuang521@gmail.com>
2025-12-28 21:36:00 +0800

    optimize 1351

--- leetcode/1351CountNegativeNumberfInASortedMatrix.go
+++ leetcode/1351CountNegativeNumberfInASortedMatrix.go
@@ -1,16 +1,13 @@
 package leetcode
 
 func countNegatives(grid [][]int) int {
-	ans := 0
-	for row := 0; row < len(grid); row++ {
-		// 4, 3, 2, -1
-		l, i := len(grid[row]), 0
-		for ; i < l; i++ {
-			if grid[row][i] < 0 {
-				break
-			}
+	var ret int
+	m, n := len(grid), len(grid[0])
+	index := n - 1
+	for row := 0; row < m; row++ {
+		for ; index >= 0 && grid[row][index] < 0; index-- {
 		}
-		ans += l - i
+		ret += n - index - 1
 	}
-	return ans
+	return ret
 }
