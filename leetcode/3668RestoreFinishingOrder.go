c5e961b38bb04679017d37571865eefa3bfbebea
0xff-dev <stevenshuang521@gmail.com>
2026-01-01 22:53:01 +0800

    Restore Finishing Order

---
+++ leetcode/3668RestoreFinishingOrder.go
@@ -0,0 +1,17 @@
+package leetcode
+
+func recoverOrder(order []int, friends []int) []int {
+	friendsMap := make(map[int]struct{})
+	for _, n := range friends {
+		friendsMap[n] = struct{}{}
+	}
+
+	index := 0
+	for _, n := range order {
+		if _, ok := friendsMap[n]; ok {
+			friends[index] = n
+			index++
+		}
+	}
+	return friends
+}
