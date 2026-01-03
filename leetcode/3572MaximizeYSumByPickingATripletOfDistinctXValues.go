156e10b81d07acd9878b10bf5a377228fdf561f3
0xff-dev <stevenshuang521@gmail.com>
2026-01-03 18:27:31 +0800

    Maximize Y-Sum by Picking a Triplet of Distinct X-Values

---
+++ leetcode/3572MaximizeYSumByPickingATripletOfDistinctXValues.go
@@ -0,0 +1,29 @@
+package leetcode
+
+import (
+	"sort"
+)
+
+func maxSumDistinctTriplet(x []int, y []int) int {
+	maxItem := make(map[int]int)
+	for index, n := range x {
+		maxItem[n] = max(maxItem[n], y[index])
+	}
+	if len(maxItem) < 3 {
+		return -1
+	}
+
+	values := make([]int, 0)
+	for _, v := range maxItem {
+		values = append(values, v)
+	}
+	sort.Slice(values, func(i, j int) bool {
+		return values[i] > values[j]
+	})
+
+	ret := 0
+	for i := range 3 {
+		ret += values[i]
+	}
+	return ret
+}
