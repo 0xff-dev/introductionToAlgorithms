845435fb8c9cf1e808c7fcaa3b3ee43ee54143c4
0xff-dev <stevenshuang521@gmail.com>
2026-01-10 21:40:17 +0800

    Build H2O

--- leetcode/1117BuildingH2O.go
+++ leetcode/1117BuildingH2O.go
@@ -1,28 +1,35 @@
 package leetcode
 
 type H2O struct {
-	h, o chan struct{}
+	hChan chan struct{}
+	oChan chan struct{}
+	done  chan struct{}
 }
 
 func NewH2O() *H2O {
-	h := &H2O{
-		h: make(chan struct{}, 2),
-		o: make(chan struct{}, 1),
+	return &H2O{
+		hChan: make(chan struct{}, 2), // 允许 2 个 H 进入
+		oChan: make(chan struct{}, 1), // 允许 1 个 O 进入
+		done:  make(chan struct{}),    // 用于三者同步
 	}
-	return h
 }
 
 func (h *H2O) Hydrogen(releaseHydrogen func()) {
-	h.h <- struct{}{}
-	// releaseHydrogen() outputs "H". Do not change or remove this line.
+	h.hChan <- struct{}{}
+
 	releaseHydrogen()
+	h.done <- struct{}{}
 }
 
 func (h *H2O) Oxygen(releaseOxygen func()) {
-	h.o <- struct{}{}
-	// releaseOxygen() outputs "H". Do not change or remove this line.
+	h.oChan <- struct{}{}
+
 	releaseOxygen()
-	<-h.h
-	<-h.h
-	<-h.o
+
+	<-h.done
+	<-h.done
+
+	<-h.hChan
+	<-h.hChan
+	<-h.oChan
 }
