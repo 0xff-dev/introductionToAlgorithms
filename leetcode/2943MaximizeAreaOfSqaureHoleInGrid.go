02dc3058d628631970a5b990dd92493a757d2652
0xff-dev <stevenshuang521@gmail.com>
2026-01-15 22:22:59 +0800

    Maximize Area of Square Hole in Grid

---
+++ leetcode/2943MaximizeAreaOfSqaureHoleInGrid.go
@@ -0,0 +1,34 @@
+package leetcode
+
+import (
+	"sort"
+)
+
+func maximizeSquareHoleArea(n int, m int, hBars []int, vBars []int) int {
+	// 能相邻，就会扩大最大相邻长度
+	sort.Ints(hBars)
+	sort.Ints(vBars)
+	mh, mv, cnt := 1, 1, 1
+
+	for i := 1; i < len(hBars); i++ {
+		if hBars[i] == hBars[i-1]+1 {
+			cnt++
+		} else {
+			cnt = 1
+		}
+		mh = max(mh, cnt)
+	}
+	mh = max(mh, cnt)
+	cnt = 1
+	for i := 1; i < len(vBars); i++ {
+		if vBars[i] == vBars[i-1]+1 {
+			cnt++
+		} else {
+			cnt = 1
+		}
+		mv = max(mv, cnt)
+	}
+	mv = max(mv, cnt)
+	edge := min(mv, mh) + 1
+	return edge * edge
+}
