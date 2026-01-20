9127536c7a02f0bcdaf46fb34b9a442f3bf5f336
0xff-dev <stevenshuang521@gmail.com>
2026-01-20 22:10:39 +0800

    Construct the Minimum Bitwise Array I

---
+++ leetcode/3314ConstructTheMinimumBitwiseArrayI.go
@@ -0,0 +1,17 @@
+package leetcode
+
+func minBitwiseArray(nums []int) []int {
+	ret := make([]int, len(nums))
+	for i, n := range nums {
+		if n == 2 {
+			ret[i] = -1
+			continue
+		}
+		k, t := 0, n
+		for ; t&1 == 1; t, k = t>>1, k+1 {
+		}
+
+		ret[i] = n ^ (1 << (k - 1))
+	}
+	return ret
+}
