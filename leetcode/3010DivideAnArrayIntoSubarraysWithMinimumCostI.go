c9d6dcf49a7e3fb267220e293639dc908baf0cde
0xff-dev <stevenshuang521@gmail.com>
2026-02-01 21:18:31 +0800

    Divide an Array Into Subarrays With Minimum Cost I

---
+++ leetcode/3010DivideAnArrayIntoSubarraysWithMinimumCostI.go
@@ -0,0 +1,13 @@
+package leetcode
+
+func minimumCost3010(nums []int) int {
+	l := len(nums)
+	for i := 1; i < 3; i++ {
+		for j := 1; j < l-i; j++ {
+			if nums[j] < nums[j+1] {
+				nums[j], nums[j+1] = nums[j+1], nums[j]
+			}
+		}
+	}
+	return nums[0] + nums[l-1] + nums[l-2]
+}
