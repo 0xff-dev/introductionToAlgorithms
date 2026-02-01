f54a61f6531522e2e44abef9811c8897130de434
0xff-dev <stevenshuang521@gmail.com>
2026-02-01 23:11:44 +0800

    fix check length >= 4

--- leetcode/1559DetectCyclesIn2DGrid.go
+++ leetcode/1559DetectCyclesIn2DGrid.go
@@ -7,12 +7,12 @@ func containsCycle(grid [][]byte) bool {
 		visited[i] = make([]uint8, cols)
 	}
 
-	var dfs func(x, y, fromX, fromY int) bool
+	var dfs func(x, y, fromX, fromY, length int) bool
 	dirs := [4][2]int{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}
 
-	dfs = func(x, y, fromX, fromY int) bool {
+	dfs = func(x, y, fromX, fromY, length int) bool {
 		if visited[x][y] == 1 {
-			return true
+			return length >= 4
 		}
 		if visited[x][y] == 2 {
 			return false
@@ -22,7 +22,7 @@ func containsCycle(grid [][]byte) bool {
 			nx, ny := x+dir[0], y+dir[1]
 			if nx >= 0 && nx < rows && ny >= 0 && ny < cols && grid[nx][ny] == grid[x][y] {
 				if nx != fromX || ny != fromY {
-					if dfs(nx, ny, x, y) {
+					if dfs(nx, ny, x, y, length+1) {
 						return true
 					}
 				}
@@ -35,7 +35,7 @@ func containsCycle(grid [][]byte) bool {
 	for i := 0; i < rows; i++ {
 		for j := 0; j < cols; j++ {
 			if visited[i][j] == 0 {
-				if dfs(i, j, -1, -1) {
+				if dfs(i, j, -1, -1, 1) {
 					return true
 				}
 			}
