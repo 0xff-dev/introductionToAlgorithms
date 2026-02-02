d3f4e8c7eb9d38b2539c3f4c78aa5d7838636bbc
0xff-dev <stevenshuang521@gmail.com>
2026-02-02 22:53:53 +0800

    Minimum Distance Between Three Equal Elements II

---
+++ leetcode/3714MinimumDistanceBetweenThreeEqualElementsII.go
@@ -0,0 +1,23 @@
+package leetcode
+
+func minimumDistance(nums []int) int {
+	found := false
+	ret := 0x7fffffff
+	group := make(map[int][]int)
+	for i := range nums {
+		group[nums[i]] = append(group[nums[i]], i)
+	}
+	for _, v := range group {
+		if len(v) < 3 {
+			continue
+		}
+		found = true
+		for i := 2; i < len(v); i++ {
+			ret = min(ret, 2*(v[i]-v[i-2]))
+		}
+	}
+	if !found {
+		return -1
+	}
+	return ret
+}
