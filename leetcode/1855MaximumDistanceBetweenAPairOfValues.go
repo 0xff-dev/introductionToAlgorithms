ee1524ffcdfd690147f221870b2e86d598ecafaa
0xff-dev <stevenshuang521@gmail.com>
2026-02-02 23:26:01 +0800

    Maximum Distance Between Pair of Values

---
+++ leetcode/1855MaximumDistanceBetweenAPairOfValues.go
@@ -0,0 +1,16 @@
+package leetcode
+
+func maxDistance1855(nums1 []int, nums2 []int) int {
+	var ret int
+	l := len(nums2)
+	index := 0
+	for idx := range nums1 {
+		for ; index < l && nums2[index] >= nums1[idx]; index++ {
+		}
+		ret = max(ret, index-1-idx)
+	}
+	if index == 0 {
+		return 0
+	}
+	return ret
+}
