42d15d74dfb6c24dcd3e1ab83fd6d42dc455a3f2
0xff-dev <stevenshuang521@gmail.com>
2026-02-05 22:28:58 +0800

    Transformed Array

---
+++ leetcode/3379TransformedArray.go
@@ -0,0 +1,20 @@
+package leetcode
+
+func constructTransformedArray(nums []int) []int {
+	l := len(nums)
+	ret := make([]int, l)
+	steps := 0
+	for i := range nums {
+		if nums[i] == 0 {
+			continue
+		}
+		if nums[i] > 0 {
+			steps = nums[i] % l
+			ret[i] = nums[(i+steps)%l]
+			continue
+		}
+		steps = (-nums[i]) % l
+		ret[i] = nums[(i-steps+l)%l]
+	}
+	return ret
+}
