d70a80b2c177b6e1a2d162b3c89499935982290c
0xff-dev <stevenshuang521@gmail.com>
2026-02-06 22:29:53 +0800

    Minimum Removals to Balance Array

---
+++ leetcode/3634MinimumRemovalsToBalanceArray.go
@@ -0,0 +1,23 @@
+package leetcode
+
+import "sort"
+
+func minRemoval(nums []int, k int) int {
+	n := len(nums)
+	sort.Ints(nums)
+
+	ans := n
+	right := 0
+
+	for left := 0; left < n; left++ {
+		for right < n && int64(nums[right]) <= int64(nums[left])*int64(k) {
+			right++
+		}
+		current := n - (right - left)
+		if current < ans {
+			ans = current
+		}
+	}
+
+	return ans
+}
