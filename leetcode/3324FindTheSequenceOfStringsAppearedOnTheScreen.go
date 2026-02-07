b80a7ff8cbe999303279de137a2988c47af162cc
0xff-dev <stevenshuang521@gmail.com>
2026-02-07 10:44:50 +0800

    Find The Sequence Of strings Appeared On The Screen

---
+++ leetcode/3324FindTheSequenceOfStringsAppearedOnTheScreen.go
@@ -0,0 +1,25 @@
+package leetcode
+
+func stringSequence(target string) []string {
+	var (
+		ret []string
+		cur string
+	)
+	index := 0
+	letters := [26]string{}
+	chars := [26]byte{}
+	for i := range 26 {
+		letters[i] = string('a' + i)
+		chars[i] = byte('a' + i)
+	}
+	bs := []byte(target)
+	for i := range bs {
+		for ; chars[index] != bs[i]; index = (index + 1) % 26 {
+			ret = append(ret, cur+letters[index])
+		}
+		cur += letters[bs[i]-'a']
+		ret = append(ret, cur)
+		index = 0
+	}
+	return ret
+}
