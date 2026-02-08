c8e80846bec29e60754a7b4a5c1b12f15175881d
0xff-dev <stevenshuang521@gmail.com>
2026-02-08 20:49:49 +0800

    Longest Turbulent Subarray

---
+++ leetcode/978LongestTurbulentSubarray.go
@@ -0,0 +1,39 @@
+package leetcode
+
+type fn func(int) bool
+
+func maxTurbulenceSize(arr []int) int {
+	cnt, l := 1, len(arr)
+
+	var (
+		ret int
+		gt  = func(i int) bool {
+			return arr[i] > arr[i+1]
+		}
+		lt = func(i int) bool {
+			return arr[i] < arr[i+1]
+		}
+	)
+
+	funcs := []fn{lt, gt}
+	for i := 0; i < l-1; i++ {
+		if funcs[i&1](i) {
+			cnt++
+			continue
+		}
+		ret = max(ret, cnt)
+		cnt = 1
+	}
+	ret = max(ret, cnt)
+
+	cnt = 1
+	for i := 0; i < l-1; i++ {
+		if funcs[(i+1)&1](i) {
+			cnt++
+			continue
+		}
+		ret = max(ret, cnt)
+		cnt = 1
+	}
+	return max(ret, cnt)
+}
