baea52eb1be016ab88691f3590f91b2c4d420512
0xff-dev <stevenshuang521@gmail.com>
2026-02-08 19:56:11 +0800

    Optimize 2526

--- leetcode/2526FindConsecutiveIntegersFromADataStream.go
+++ leetcode/2526FindConsecutiveIntegersFromADataStream.go
@@ -1,45 +1,31 @@
 package leetcode
 
 type DataStream struct {
-	value, k, index int
-
-	top []int
-	cnt map[int]int
+	value, k, cnt int
 }
 
 func Constructor2526(value int, k int) DataStream {
-	ds := DataStream{
+	return DataStream{
 		value: value,
 		k:     k,
-		top:   make([]int, k),
-		cnt:   make(map[int]int),
-		index: 0,
+		cnt:   0,
 	}
-	for i := range ds.top {
-		ds.top[i] = -1
-	}
-	return ds
 }
+
+// value = 1, k = 2
+// 1, 2, 1, 1, 1
 
 func (this *DataStream) Consec(num int) bool {
-	if this.top[this.index] == -1 {
-		this.top[this.index] = num
-		this.cnt[num]++
-		this.index = (this.index + 1) % this.k
-		if this.index != 0 {
-			return false
-		}
-	} else {
-		remove := this.top[this.index]
-		this.cnt[remove]--
-		if this.cnt[remove] == 0 {
-			delete(this.cnt, remove)
-		}
-		this.top[this.index] = num
-		this.cnt[num]++
-		this.index = (this.index + 1) % this.k
+	if num == this.value {
+		this.cnt++
+		return this.cnt >= this.k
 	}
-
-	_, ok := this.cnt[this.value]
-	return ok && len(this.cnt) == 1
+	this.cnt = 0
+	return false
 }
+
+/**
+ * Your DataStream object will be instantiated and called as such:
+ * obj := Constructor(value, k);
+ * param_1 := obj.Consec(num);
+ */
