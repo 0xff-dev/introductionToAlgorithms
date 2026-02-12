8d46de4d44653025187025e3e60e19753d84a0b5
0xff-dev <stevenshuang521@gmail.com>
2026-02-12 21:18:17 +0800

    Longest Balanced Substring I

---
+++ leetcode/3713LongestBalancedSubstringI.go
@@ -0,0 +1,47 @@
+package leetcode
+
+func longestBalanced3713(s string) int {
+	ret := 1
+	n := len(s)
+	count := make([][26]int, n)
+	pre := [26]int{}
+	for i := range s {
+		pre[s[i]-'a']++
+		count[i] = pre
+	}
+
+	check := func(end, start int) bool {
+		test := count[end]
+		if start != -1 {
+			for i := range 26 {
+				test[i] -= count[start][i]
+			}
+		}
+		cur := 0
+		for i := range 26 {
+			if test[i] == 0 {
+				continue
+			}
+			if cur == 0 {
+				cur = test[i]
+			}
+			if cur != test[i] {
+				return false
+			}
+		}
+		return true
+	}
+	for end := 1; end < n; end++ {
+		if check(end, -1) {
+			ret = max(ret, end+1)
+			continue
+		}
+		for i := 0; i < end; i++ {
+			if check(end, i) {
+				ret = max(ret, end-i)
+				break
+			}
+		}
+	}
+	return ret
+}
