45fc230245d2b8feee44e7c12dae93e356252251
0xff-dev <stevenshuang521@gmail.com>
2026-02-15 17:03:59 +0800

    Maximum Nesting Depth of Two Valid Parentheses Strings

---
+++ leetcode/1111MaximumNestingDepthOfTwoValidParenthesesStrings.go
@@ -0,0 +1,19 @@
+package leetcode
+
+func maxDepthAfterSplit(seq string) []int {
+	l := len(seq)
+	ret := make([]int, l)
+
+	depth := 0
+	bs := []byte(seq)
+	for i := range bs {
+		if bs[i] == '(' {
+			depth++
+			ret[i] = 1 - depth&1
+			continue
+		}
+		ret[i] = 1 - depth&1
+		depth--
+	}
+	return ret
+}
