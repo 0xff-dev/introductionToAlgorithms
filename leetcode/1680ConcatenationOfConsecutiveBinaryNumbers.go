700e9eb7d0dba87abbf3725890106848182aa4ef
0xff-dev <stevenshuang521@gmail.com>
2026-02-28 16:06:49 +0800

    concatenation of Consecutive Binary Numbers

---
+++ leetcode/1680ConcatenationOfConsecutiveBinaryNumbers.go
@@ -0,0 +1,23 @@
+package leetcode
+
+const mod1680 = 1000000007
+
+func concatenatedBinary(n int) int {
+	bits := func(x int) int {
+		mask := 1 << 31
+		cnt := 0
+		for mask&x == 0 {
+			mask >>= 1
+			cnt++
+		}
+		return 32 - cnt
+	}
+	ret := 0
+	for i := 1; i <= n; i++ {
+		shift := bits(i)
+		ret <<= shift
+		ret %= mod1680
+		ret = (ret + i) % mod1680
+	}
+	return ret
+}
