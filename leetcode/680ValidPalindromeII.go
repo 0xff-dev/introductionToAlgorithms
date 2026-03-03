567ea60c6411f4537fa06bc6f54f23b29f174186
0xff-dev <stevenshuang521@gmail.com>
2026-03-03 22:21:14 +0800

    Valid Palindrome II

---
+++ leetcode/680ValidPalindromeII.go
@@ -0,0 +1,23 @@
+package leetcode
+
+func validPalindrome(s string) bool {
+	size := len(s)
+	l, r := 0, size-1
+	// a b a
+	for ; l < r && s[l] == s[r]; l, r = l+1, r-1 {
+	}
+	if l >= r {
+		return true
+	}
+
+	sl, sr := l+1, r
+	for ; sl < sr && s[sl] == s[sr]; sl, sr = sl+1, sr-1 {
+	}
+	if sl >= sr {
+		return true
+	}
+	sl, sr = l, r-1
+	for ; sl < sr && s[sl] == s[sr]; sl, sr = sl+1, sr-1 {
+	}
+	return sl >= sr
+}
