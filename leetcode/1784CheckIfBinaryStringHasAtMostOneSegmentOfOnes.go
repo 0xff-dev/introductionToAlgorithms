c8af48d1174d447feda48d22822f7673388aed4f
0xff-dev <stevenshuang521@gmail.com>
2026-03-06 22:36:46 +0800

    Check if Binary String Has At Most One Segment of Ones

---
+++ leetcode/1784CheckIfBinaryStringHasAtMostOneSegmentOfOnes.go
@@ -0,0 +1,24 @@
+package leetcode
+
+func checkOnesSegment(s string) bool {
+
+	seg := 0
+	prev := s[0]
+	if prev == '1' {
+		seg++
+	}
+
+	for i := 1; i < len(s); i++ {
+		if s[i] == prev {
+			continue
+		}
+		prev = s[i]
+		if prev == '1' {
+			seg++
+		}
+		if seg > 1 {
+			return false
+		}
+	}
+	return true
+}
