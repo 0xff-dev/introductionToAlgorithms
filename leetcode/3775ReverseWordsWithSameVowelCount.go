c63f426f991f0af8bcfbe984419433ac870618f8
0xff-dev <stevenshuang521@gmail.com>
2026-03-18 22:06:59 +0800

    Reverse Words With Same Vowel Count

---
+++ leetcode/3775ReverseWordsWithSameVowelCount.go
@@ -0,0 +1,35 @@
+package leetcode
+
+import (
+	"bytes"
+)
+
+func reverseWords3775(s string) string {
+	parts := bytes.Split([]byte(s), []byte{' '})
+
+	cmp := 0
+	for idx := range parts[0] {
+		if parts[0][idx] == 'a' || parts[0][idx] == 'e' || parts[0][idx] == 'i' || parts[0][idx] == 'o' || parts[0][idx] == 'u' {
+			cmp++
+		}
+	}
+	buf := make([][]byte, len(parts))
+	buf[0] = parts[0]
+
+	cnt := 0
+	for i := 1; i < len(parts); i++ {
+		cnt = 0
+		for idx := range parts[i] {
+			if parts[i][idx] == 'a' || parts[i][idx] == 'e' || parts[i][idx] == 'i' || parts[i][idx] == 'o' || parts[i][idx] == 'u' {
+				cnt++
+			}
+		}
+		if cnt == cmp {
+			for s, e := 0, len(parts[i])-1; s < e; s, e = s+1, e-1 {
+				parts[i][s], parts[i][e] = parts[i][e], parts[i][s]
+			}
+		}
+		buf[i] = parts[i]
+	}
+	return string(bytes.Join(buf, []byte{' '}))
+}
