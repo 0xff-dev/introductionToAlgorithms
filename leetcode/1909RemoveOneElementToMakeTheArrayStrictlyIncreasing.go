4f20c67212483f60aaacea74c9c0ad72486809bd
0xff-dev <stevenshuang521@gmail.com>
2026-03-22 20:50:51 +0800

    Remove One Element To Make The Array Strictly Increasing

---
+++ leetcode/1909RemoveOneElementToMakeTheArrayStrictlyIncreasing.go
@@ -0,0 +1,28 @@
+package leetcode
+
+func canBeIncreasing(nums []int) bool {
+	pivot := -1
+	for i := 1; i < len(nums); i++ {
+		if nums[i] <= nums[i-1] {
+			if pivot == -1 {
+				pivot = i
+				continue
+			}
+			return false
+		}
+	}
+	if pivot == -1 {
+		return true
+	}
+
+	// remove nums[pivot-1]
+	if pivot-2 >= 0 && nums[pivot] > nums[pivot-2] {
+		return true
+	}
+	// remove nums[pivot]
+	if pivot < len(nums)-1 && nums[pivot+1] > nums[pivot-1] {
+		return true
+	}
+
+	return pivot == len(nums)-1 || pivot == 1
+}
