a7ac198f833c3f8c7291931b3aaeb637a44b218c
0xff-dev <stevenshuang521@gmail.com>
2026-04-04 15:27:41 +0800

    Decode The slanted Ciphertext

---
+++ leetcode/2075DecodeTheSlantedCiphertext.go
@@ -0,0 +1,24 @@
+package leetcode
+
+import "bytes"
+
+func decodeCiphertext(encodedText string, rows int) string {
+	if rows == 1 || encodedText == "" {
+		return encodedText
+	}
+	size := len(encodedText)
+	cols := size / rows
+	var buf bytes.Buffer
+	for i := 0; i <= cols-rows+1; i++ {
+		for j := i; j < size; j += cols + 1 {
+			buf.WriteByte(encodedText[j])
+		}
+	}
+
+	ret := buf.String()
+	end := len(ret) - 1
+	for ; ret[end] == ' '; end-- {
+
+	}
+	return ret[:end+1]
+}
