9aca4ead5e15b518e5e21304d36f04756c117120
0xff-dev <stevenshuang521@gmail.com>
2026-04-13 22:05:46 +0800

    Minimum Distance to the Target Element

---
+++ leetcode/1848MinimumDistanceToTheTargetElement.go
@@ -0,0 +1,18 @@
+package leetcode
+
+func getMinDistance(nums []int, target int, start int) int {
+	ret := -1
+	diff := 0
+	for i := range nums {
+		if nums[i] == target {
+			diff = i - start
+			if diff < 0 {
+				diff = -diff
+			}
+			if ret == -1 || ret > diff {
+				ret = diff
+			}
+		}
+	}
+	return ret
+}
