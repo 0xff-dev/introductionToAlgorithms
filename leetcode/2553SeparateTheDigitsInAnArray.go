cd01d7b113aff42a26b84edf3ebfccfc1e87ffe0
0xff-dev <stevenshuang521@gmail.com>
2026-05-11 22:07:50 +0800

    Separate the Digits in an Array

---
+++ leetcode/2553SeparateTheDigitsInAnArray.go
@@ -0,0 +1,19 @@
+package leetcode
+
+func separateDigits(nums []int) []int {
+	ret := make([]int, 0)
+	var n, tmp int
+	for i := range nums {
+		n = nums[i]
+		var bits []int
+		for n > 0 {
+			tmp = n % 10
+			bits = append(bits, tmp)
+			n /= 10
+		}
+		for j := len(bits) - 1; j >= 0; j-- {
+			ret = append(ret, bits[j])
+		}
+	}
+	return ret
+}
