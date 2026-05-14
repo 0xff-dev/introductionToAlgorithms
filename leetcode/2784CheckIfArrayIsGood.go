471d97d637710d4df071c94c6216c4a7fd21e6f4
0xff-dev <stevenshuang521@gmail.com>
2026-05-14 22:03:09 +0800

    Check If Array is Good

---
+++ leetcode/2784CheckIfArrayIsGood.go
@@ -0,0 +1,18 @@
+package leetcode
+
+import "sort"
+
+func isGood(nums []int) bool {
+	size := len(nums)
+	if size < 2 {
+		return false
+	}
+
+	sort.Ints(nums)
+	for i := 0; i < size-1; i++ {
+		if nums[i] != i+1 {
+			return false
+		}
+	}
+	return nums[size-1] == nums[size-2]
+}
