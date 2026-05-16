92363192e7437cd1ce4e636d1d3250c61f3e60df
0xff-dev <stevenshuang521@gmail.com>
2026-05-16 16:58:24 +0800

    Find Minimum In Rotated Sorted Array II

---
+++ leetcode/154FindMinimumInRotatedSortedArrayII.go
@@ -0,0 +1,16 @@
+package leetcode
+
+func findMin(nums []int) int {
+	low, high := 0, len(nums)-1
+	for low < high {
+		mid := low + (high-low)/2
+		if nums[mid] > nums[high] {
+			low = mid + 1
+		} else if nums[mid] < nums[high] {
+			high = mid
+		} else {
+			high--
+		}
+	}
+	return nums[low]
+}
