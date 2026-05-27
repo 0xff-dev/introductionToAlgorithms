4f617f70b672a63a2f4cc8ac5aac4e0b9f4668cc
0xff-dev <stevenshuang521@gmail.com>
2026-05-27 08:41:00 +0800

    Count the Number Of Special Characters II

---
+++ leetcode/3121CountTheNumberOfSpecialCharactersII.go
@@ -0,0 +1,27 @@
+package leetcode
+
+func numberOfSpecialChars3121(word string) int {
+	lower, upper := [26]int{}, [26]int{}
+	for i := range 26 {
+		lower[i] = -1
+		upper[i] = -1
+	}
+
+	for i := range word {
+		if word[i] >= 'a' && word[i] <= 'z' {
+			lower[word[i]-'a'] = i
+			continue
+		}
+		if upper[word[i]-'A'] == -1 {
+			upper[word[i]-'A'] = i
+		}
+	}
+
+	var ret int
+	for i := range 26 {
+		if lower[i] != -1 && upper[i] != -1 && lower[i] < upper[i] {
+			ret++
+		}
+	}
+	return ret
+}
