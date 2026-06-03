b7e90d78ba22a5ddd9da1a926af291db46a7dfaf
0xff-dev <stevenshuang521@gmail.com>
2026-06-03 22:01:22 +0800

    Earliest Finish Time For Land And Water Rides II

---
+++ leetcode/3635EarliestFinishTimeForLandAndWaterRidesII.go
@@ -0,0 +1,23 @@
+package leetcode
+
+func earliestFinishTime3635(landStartTime []int, landDuration []int, waterStartTime []int, waterDuration []int) int {
+	ans := 0x7fffffff
+
+	minLandEnd := 0x7fffffff
+	for i := range landStartTime {
+		minLandEnd = min(minLandEnd, landStartTime[i]+landDuration[i])
+	}
+	for i := range waterStartTime {
+		ans = min(ans, max(minLandEnd, waterStartTime[i])+waterDuration[i])
+	}
+
+	minWaterEnd := 0x7fffffff
+	for i := range waterStartTime {
+		minWaterEnd = min(minWaterEnd, waterStartTime[i]+waterDuration[i])
+	}
+	for i := range landStartTime {
+		ans = min(ans, max(minWaterEnd, landStartTime[i])+landDuration[i])
+	}
+
+	return ans
+}
