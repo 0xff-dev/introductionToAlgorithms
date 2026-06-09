810a93ce8ddade931b1663aceaa44013b68b4950
0xff-dev <stevenshuang521@gmail.com>
2026-06-09 22:56:17 +0800

    Maximum Total Subarray Value I

---
+++ leetcode/3689MaximumTotalSubarrayValueI.go
@@ -0,0 +1,11 @@
+package leetcode
+
+func maxTotalValue(nums []int, k int) int64 {
+	a, b := nums[0], nums[0]
+	for i := 1; i < len(nums); i++ {
+		a = max(a, nums[i])
+		b = min(b, nums[i])
+	}
+
+	return int64(k) * int64(a-b)
+}
