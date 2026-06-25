ff51dbe2b2ac8112de991e2c7fa60c4e5a33f6f8
0xff-dev <stevenshuang521@gmail.com>
2026-06-25 23:23:23 +0800

    Count Subarrays With Majority Element I

---
+++ leetcode/3737CountSubarraysWithMajorityElementI.go
@@ -0,0 +1,33 @@
+package leetcode
+
+func countMajoritySubarrays(nums []int, target int) int {
+	n := len(nums)
+	if n == 0 {
+		return 0
+	}
+
+	offset := n
+	count := make([]int, 2*n+1)
+
+	count[0+offset] = 1
+
+	var ans int = 0
+	curSum := 0
+	lessThanCount := 0
+
+	for _, num := range nums {
+		if num == target {
+			lessThanCount += count[curSum+offset]
+			curSum++
+		} else {
+			curSum--
+			lessThanCount -= count[curSum+offset]
+		}
+
+		ans += lessThanCount
+
+		count[curSum+offset]++
+	}
+
+	return ans
+}
