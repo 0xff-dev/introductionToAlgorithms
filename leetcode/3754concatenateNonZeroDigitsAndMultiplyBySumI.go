adf31b86f2012d219722d9e16d93529ba5d603bf
0xff-dev <stevenshuang521@gmail.com>
2026-07-07 09:17:03 +0800

    Concatenate Non-Zero Digits and Multiply by Sum I

---
+++ leetcode/3754concatenateNonZeroDigitsAndMultiplyBySumI.go
@@ -0,0 +1,20 @@
+package leetcode
+
+func sumAndMultiply(n int) int64 {
+	var (
+		base        int64 = 1
+		sum, x, tmp int64
+	)
+	i64n := int64(n)
+	for ; i64n > 0; i64n /= 10 {
+		tmp = i64n % 10
+		if tmp == 0 {
+			continue
+		}
+
+		sum += tmp
+		x = base*int64(tmp) + x
+		base *= 10
+	}
+	return sum * x
+}
