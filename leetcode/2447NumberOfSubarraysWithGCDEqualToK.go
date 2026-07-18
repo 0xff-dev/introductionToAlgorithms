67ad73d0b1c6f731a2b4c4dd81ae990477fbe340
0xff-dev <stevenshuang521@gmail.com>
2026-07-18 15:24:59 +0800

    Number of Subarrays With GCD Equal to K

---
+++ leetcode/2447NumberOfSubarraysWithGCDEqualToK.go
@@ -0,0 +1,29 @@
+package leetcode
+
+func gcd2447(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
+}
+
+func subarrayGCD(nums []int, k int) int {
+	size := len(nums)
+	var ret int
+	for i := 0; i < size; i++ {
+		cur := nums[i]
+		if cur == k {
+			ret++
+		}
+		for j := i + 1; j < size; j++ {
+			cur = gcd2447(cur, nums[j])
+			if cur < k {
+				break
+			}
+			if cur == k {
+				ret++
+			}
+		}
+	}
+	return ret
+}
