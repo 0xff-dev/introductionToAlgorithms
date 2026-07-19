8a4fce84a24766d97971eb7644dbd3a8dc121c33
0xff-dev <stevenshuang521@gmail.com>
2026-07-19 20:05:40 +0800

    Shortest Distance to a Character

---
+++ leetcode/821ShortestDistanceToACharacter.go
@@ -0,0 +1,29 @@
+package leetcode
+
+import "sort"
+
+func shortestToChar(s string, c byte) []int {
+	ret := make([]int, len(s))
+	indies := make([]int, 0)
+	for i := range s {
+		if s[i] == c {
+			indies = append(indies, i)
+		}
+	}
+	n := len(indies)
+	for i := range s {
+		idx := sort.Search(n, func(ii int) bool {
+			return indies[ii] >= i
+		})
+		if idx == n {
+			ret[i] = i - indies[n-1]
+			continue
+		}
+		distance := indies[idx] - i
+		if idx > 0 {
+			distance = min(distance, i-indies[idx-1])
+		}
+		ret[i] = distance
+	}
+	return ret
+}
