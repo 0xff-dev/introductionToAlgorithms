108b2c935c53d0fcc8a6ccd736965c499a722fd8
0xff-dev <stevenshuang521@gmail.com>
2026-07-21 22:58:43 +0800

    Maximize Active Section with Trade I

---
+++ leetcode/3499MaximizeActiveSectionWithTradeI.go
@@ -0,0 +1,35 @@
+package leetcode
+
+func maxActiveSectionsAfterTrade(s string) int {
+	n := len(s)
+	cnt1 := 0
+	for _, c := range s {
+		if c == '1' {
+			cnt1++
+		}
+	}
+
+	zeroBlocks := []int{}
+	i := 0
+	for i < n {
+		start := i
+		for i < n && s[i] == s[start] {
+			i++
+		}
+		if s[start] == '0' {
+			zeroBlocks = append(zeroBlocks, i-start)
+		}
+	}
+
+	m := len(zeroBlocks)
+	if m < 2 {
+		return cnt1
+	}
+
+	bestGain := 0 // Optimal Increment
+	for j := 0; j < m-1; j++ {
+		bestGain = max(bestGain, zeroBlocks[j]+zeroBlocks[j+1])
+	}
+
+	return cnt1 + bestGain
+}
