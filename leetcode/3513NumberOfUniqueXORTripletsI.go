d6ebde3e0dd022eb4d864e32a80a4b79aa3b151b
0xff-dev <stevenshuang521@gmail.com>
2026-07-23 22:23:28 +0800

    Number of Unique XOR Triplets I

---
+++ leetcode/3513NumberOfUniqueXORTripletsI.go
@@ -0,0 +1,15 @@
+package leetcode
+
+func uniqueXorTriplets(nums []int) int {
+	n := len(nums)
+	if n < 3 {
+		return n
+	}
+
+	mask := 1
+	for mask < n {
+		mask = (mask << 1) | 1
+	}
+
+	return mask + 1
+}
