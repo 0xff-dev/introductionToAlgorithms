9f34153467918288266886a567f2352bdc9286d2
0xff-dev <stevenshuang521@gmail.com>
2026-08-04 21:28:45 +0800

    Find Missing Elements

---
+++ leetcode/3731FindMissingElements.go
@@ -0,0 +1,19 @@
+package leetcode
+
+import (
+	"sort"
+)
+
+func findMissingElements(nums []int) []int {
+	var ret []int
+	sort.Ints(nums)
+	index, cur := 1, nums[0]+1
+	for ; index != len(nums); cur++ {
+		if cur == nums[index] {
+			index++
+			continue
+		}
+		ret = append(ret, cur)
+	}
+	return ret
+}
