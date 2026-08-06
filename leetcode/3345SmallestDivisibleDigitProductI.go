52c7d5a7021b0e5ac2535972102d41aa006a945e
0xff-dev <stevenshuang521@gmail.com>
2026-08-06 21:40:18 +0800

    Smallest Divisible Digit Product I

---
+++ leetcode/3345SmallestDivisibleDigitProductI.go
@@ -0,0 +1,22 @@
+package leetcode
+
+func smallestNumber3345(n int, t int) int {
+	p, c := 1, n
+	for ; c > 0; c /= 10 {
+		p *= c % 10
+	}
+	if p%t == 0 {
+		return n
+	}
+
+	digit := n % 10
+	p /= digit
+
+	for i := digit + 1; i <= 9; i++ {
+		x := p * i
+		if x%t == 0 {
+			return n + i - digit
+		}
+	}
+	return n + 10 - digit
+}
