4628f2bb9bfe7d244631026a12e8a0f910a340f8
0xff-dev <stevenshuang521@gmail.com>
2026-08-11 22:48:57 +0800

    Smallest Missing Integer Greater Than Sequential Prefix Sum

---
+++ leetcode/2996SmallestMissingIntegerGreaterThenSequentialPrefixSum.go
@@ -0,0 +1,23 @@
+package leetcode
+
+func missingInteger(nums []int) int {
+	sum := nums[0]
+	exists := map[int]struct{}{}
+	for i := 0; i < len(nums); i++ {
+		exists[nums[i]] = struct{}{}
+	}
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[i-1]+1 {
+			break
+		}
+		sum += nums[i]
+	}
+
+	for ; ; sum++ {
+		_, ok := exists[sum]
+		if !ok {
+			break
+		}
+	}
+	return sum
+}
