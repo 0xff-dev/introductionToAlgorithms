e9f535516f52a521f47901fc8f465d39cdace4dd
0xff-dev <stevenshuang521@gmail.com>
2026-08-14 22:07:05 +0800

    Maximum Length Substring With Two Occurences

---
+++ leetcode/3090MaximumLengthSubstringWithTwoOccurrences.go
@@ -0,0 +1,22 @@
+package leetcode
+
+func maximumLengthSubstring(s string) int {
+	var ret int
+	cnt := [26]int{}
+	start, end := 0, 0
+	for ; end < len(s); end++ {
+		i := s[end] - 'a'
+		if cnt[i] < 2 {
+			cnt[i]++
+			continue
+		}
+		ret = max(ret, end-start)
+		for ; s[start] != s[end]; start++ {
+			cnt[s[start]-'a']--
+		}
+		start++
+	}
+	ret = max(ret, end-start)
+
+	return ret
+}
