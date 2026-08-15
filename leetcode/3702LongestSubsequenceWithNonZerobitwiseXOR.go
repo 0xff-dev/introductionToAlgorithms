78fddcb8d8abd92ddf8e1520c8565055463f48dc
0xff-dev <stevenshuang521@gmail.com>
2026-08-15 13:18:32 +0800

    Longest Subsequence With Non-Zero bitwise XOR

---
+++ leetcode/3702LongestSubsequenceWithNonZerobitwiseXOR.go
@@ -0,0 +1,19 @@
+package leetcode
+
+func longestSubsequence3702(nums []int) int {
+	xor, size := 0, len(nums)
+	allZero := true
+	for i := range nums {
+		xor ^= nums[i]
+		if nums[i] > 0 {
+			allZero = false
+		}
+	}
+	if xor > 0 {
+		return size
+	}
+	if allZero {
+		return 0
+	}
+	return size - 1
+}
