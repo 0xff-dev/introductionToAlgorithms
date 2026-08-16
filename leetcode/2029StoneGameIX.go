fdb9c4f2bd3715fcc9d167e04742d6504dcd8644
0xff-dev <stevenshuang521@gmail.com>
2026-08-16 10:44:29 +0800

    Stone Game IX

---
+++ leetcode/2029StoneGameIX.go
@@ -0,0 +1,12 @@
+package leetcode
+
+func stoneGameIX(stones []int) bool {
+	score := [3]int{}
+	for i := range stones {
+		score[stones[i]%3]++
+	}
+	if score[0]%2 == 0 {
+		return score[1] >= 1 && score[2] >= 1
+	}
+	return score[1]-score[2] > 2 || score[2]-score[1] > 2
+}
