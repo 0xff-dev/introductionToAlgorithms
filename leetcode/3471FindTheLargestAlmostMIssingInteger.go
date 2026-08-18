c3de710241f365e18be45e1b55f911a9ee78c4e2
0xff-dev <stevenshuang521@gmail.com>
2026-08-18 22:43:52 +0800

    Find the largest Almost Missing Integer

---
+++ leetcode/3471FindTheLargestAlmostMIssingInteger.go
@@ -0,0 +1,24 @@
+package leetcode
+
+func largestInteger3471(nums []int, k int) int {
+	cnt := map[int]int{}
+	for i := range nums {
+		if k == len(nums) {
+			cnt[nums[i]] = 1
+			continue
+		}
+
+		cnt[nums[i]]++
+		if k == 1 || i == 0 || i == len(nums)-1 {
+			continue
+		}
+		cnt[nums[i]] += 1
+	}
+	ret := -1
+	for i := range nums {
+		if cnt[nums[i]] == 1 {
+			ret = max(ret, nums[i])
+		}
+	}
+	return ret
+}
