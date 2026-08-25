a7b27a75d3678b2399a01dcd1bf3af9f1ea23fd9
0xff-dev <stevenshuang521@gmail.com>
2026-08-25 09:41:04 +0800

    Smallest Missing Multiple of K

---
+++ leetcode/3718SmallestMissingMultipleOfK.go
@@ -0,0 +1,18 @@
+package leetcode
+
+func missingMultiple(nums []int, k int) int {
+	m := 0
+	in := make(map[int]struct{})
+	for i := range nums {
+		if nums[i]%k == 0 {
+			in[nums[i]] = struct{}{}
+			m = max(m, nums[i])
+		}
+	}
+	for i := k; i <= m; i += k {
+		if _, ok := in[i]; !ok {
+			return i
+		}
+	}
+	return m + k
+}
