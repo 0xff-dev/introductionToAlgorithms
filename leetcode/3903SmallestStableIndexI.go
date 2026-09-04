c4881c6b340afb3617b1d98ee54001ca868187a7
0xff-dev <stevenshuang521@gmail.com>
2026-09-04 21:48:13 +0800

    Smallest Stalbe Index I

---
+++ leetcode/3903SmallestStableIndexI.go
@@ -0,0 +1,19 @@
+package leetcode
+
+func firstStableIndex(nums []int, k int) int {
+	n := len(nums)
+	minValues := make([]int, n)
+	minValues[n-1] = nums[n-1]
+	for i := n - 2; i >= 0; i-- {
+		minValues[i] = min(nums[i], minValues[i+1])
+	}
+	maxValue := 0
+	for i := 0; i < n; i++ {
+		maxValue = max(maxValue, nums[i])
+		right := minValues[i]
+		if maxValue-right <= k {
+			return i
+		}
+	}
+	return -1
+}
