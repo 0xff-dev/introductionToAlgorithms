be0d86c55eeca6e254780e433d5294fa6349c70e
0xff-dev <stevenshuang521@gmail.com>
2026-09-06 16:22:23 +0800

    optimize 115

--- leetcode/115DistinctSubsequences_test.go
+++ leetcode/115DistinctSubsequences_test.go
@@ -7,14 +7,23 @@ func TestNumDistinct(t *testing.T) {
 	if r := numDistinct(s, t1); r != 3 {
 		t.Fatalf("expect 3 get %d", r)
 	}
+	if r := numDistinct1(s, t1); r != 3 {
+		t.Fatalf("expect 3 get %d", r)
+	}
 
 	s, t1 = "babgbag", "bag"
 	if r := numDistinct(s, t1); r != 5 {
 		t.Fatalf("expect 5 get %d", r)
 	}
+	if r := numDistinct1(s, t1); r != 5 {
+		t.Fatalf("expect 5 get %d", r)
+	}
 
 	s, t1 = "", ""
 	if r := numDistinct(s, t1); r != 1 {
+		t.Fatalf("expcet 1 get %d", r)
+	}
+	if r := numDistinct1(s, t1); r != 1 {
 		t.Fatalf("expcet 1 get %d", r)
 	}
 }
