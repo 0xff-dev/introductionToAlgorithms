3cfc76c411ffd38bf9c321b226ae465e1d74534d
0xff-dev <stevenshuang521@gmail.com>
2026-09-08 08:32:16 +0800

    optimize 3870

--- leetcode/3870CountCommasInRange.go
+++ leetcode/3870CountCommasInRange.go
@@ -5,14 +5,5 @@ func countCommas(n int) int {
 	if n < 1000 {
 		return 0
 	}
-	// 9999-1000+1 = 9000个
-	bits, s := 0, n
-	for ; n > 0; n /= 10 {
-		bits++
-	}
-	base := 9000
-	if bits == 4 {
-		return s - 1000 + 1
-	}
-	return base + s - 10000 + 1
+	return n - 999
 }
