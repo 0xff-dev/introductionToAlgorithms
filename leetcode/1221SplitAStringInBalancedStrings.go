8715137c365b605d5719bfd9e7e3001d29c7ffa2
0xff-dev <stevenshuang521@gmail.com>
2026-09-10 08:55:31 +0800

    Split a String In Balanced Strings

---
+++ leetcode/1221SplitAStringInBalancedStrings.go
@@ -0,0 +1,18 @@
+package leetcode
+
+func balancedStringSplit(s string) int {
+	var ret int
+	r, l := 0, 0
+	for i := range s {
+		if s[i] == 'R' {
+			r++
+		} else {
+			l++
+		}
+		if l == r {
+			ret++
+			l, r = 0, 0
+		}
+	}
+	return ret
+}
