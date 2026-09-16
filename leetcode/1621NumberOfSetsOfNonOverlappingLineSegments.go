dadaf2d425ff4ab540d5b4e9633bf00e7f25dbe9
0xff-dev <stevenshuang521@gmail.com>
2026-09-16 08:36:21 +0800

    Number Of Sets of Non-Overlapping Line Segments

---
+++ leetcode/1621NumberOfSetsOfNonOverlappingLineSegments.go
@@ -0,0 +1,24 @@
+package leetcode
+
+func numberOfSets(n int, k int) int {
+	const mod = 1000000007
+
+	m := n + k - 1
+	r := 2 * k
+
+	if r > m || r < 0 {
+		return 0
+	}
+
+	c := make([][]int, m+1)
+	for i := range c {
+		c[i] = make([]int, i+1)
+		c[i][0] = 1
+		c[i][i] = 1
+		for j := 1; j < i; j++ {
+			c[i][j] = (c[i-1][j-1] + c[i-1][j]) % mod
+		}
+	}
+
+	return c[m][r]
+}
